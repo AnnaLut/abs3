99e2cd1ec6985f5671ef119bcb3adf0779438475
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2017-12-07 19:20:36 +0000

    Коментар: #COBUNEWNBS-84# Коментар:

--- ABS/master/sql/ru/bars/View/proc_dr.sql
+++ ABS/master/sql/ru/bars/View/proc_dr.sql
@@ -1,29 +1,37 @@
+prompt -- ======================================================
+prompt -- create view PROC_DR
+prompt -- ======================================================
 
+create or replace force view PROC_DR
+( NBS, G67, V67, SOUR, NBSN, G67N, V67N, NBSZ, REZID, TT, TTV, IO, BRANCH
+, KF
+) as
+select NBS
+     , G67
+     , V67
+     , SOUR
+     , NBSN
+     , G67N
+     , V67N
+     , NBSZ
+     , REZID
+     , TT
+     , TTV
+     , IO
+     , BRANCH
+     , KF
+  from PROC_DR$BASE
+ where BRANCH = sys_context('bars_context','user_branch');
 
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/View/PROC_DR.sql =========*** Run *** ======
-PROMPT ===================================================================================== 
+show errors;
 
+prompt -- ======================================================
+prompt -- Grants
+prompt -- ======================================================
 
-PROMPT *** Create  view PROC_DR ***
-
-  CREATE OR REPLACE FORCE VIEW BARS.PROC_DR ("NBS", "G67", "V67", "SOUR", "NBSN", "G67N", "V67N", "NBSZ", "REZID", "TT", "TTV", "IO", "BRANCH") AS 
-  select NBS,G67,V67,SOUR,NBSN,G67N,V67N,NBSZ,REZID,TT,TTV,IO, BRANCH
-from proc_dr$BASE where BRANCH=sys_context('bars_context','user_branch') ;
-
-PROMPT *** Create  grants  PROC_DR ***
-grant SELECT                                                                 on PROC_DR         to BARS009;
-grant SELECT                                                                 on PROC_DR         to BARS010;
-grant DELETE,FLASHBACK,INSERT,SELECT,UPDATE                                  on PROC_DR         to BARS_ACCESS_DEFROLE;
-grant DELETE,INSERT,SELECT,UPDATE                                            on PROC_DR         to FOREX;
-grant SELECT                                                                 on PROC_DR         to RCC_DEAL;
-grant SELECT                                                                 on PROC_DR         to START1;
-grant DELETE,INSERT,SELECT,UPDATE                                            on PROC_DR         to TECH005;
-grant DELETE,FLASHBACK,INSERT,SELECT,UPDATE                                  on PROC_DR         to WR_ALL_RIGHTS;
-grant FLASHBACK,SELECT                                                       on PROC_DR         to WR_REFREAD;
-
-
-
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/View/PROC_DR.sql =========*** End *** ======
-PROMPT ===================================================================================== 
+grant SELECT on PROC_DR to BARS_ACCESS_DEFROLE;
+grant SELECT on PROC_DR to FOREX;
+grant SELECT on PROC_DR to RCC_DEAL;
+grant SELECT on PROC_DR to START1;
+grant SELECT on PROC_DR to WR_ALL_RIGHTS;
+grant SELECT on PROC_DR to WR_REFREAD;
