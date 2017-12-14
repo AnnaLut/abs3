3a1f8087a40328978fa346668aeb47c9586c3ec0
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2017-12-14 15:45:26 +0000

    #COBUNEWNBS-84# Коментар:

--- ABS/master/sql/ru/bars/Table/tmp_all_enabled_triggers.sql
+++ ABS/master/sql/ru/bars/Table/tmp_all_enabled_triggers.sql
@@ -1,5 +1,5 @@
 begin 
-    excute immediate 'create table tmp_all_enabled_triggers(owner varcjar2(20), trigger_name varchar2(200) )';
+    execute immediate 'create table tmp_all_enabled_triggers(owner varchar2(20), trigger_name varchar2(200) )';
 exception when others then       
   if sqlcode=-955 then null; else raise; end if; 
 end; 
