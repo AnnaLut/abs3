a43a0d60f80fe6520e676ddfd4e2df1701da837b
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2017-12-15 18:00:16 +0000

    #COBUNEWNBS-84# Коментар: MErge

--- sql/ru/bars/View/dpu_vidd_ob22.sql
+++ sql/ru/bars/View/dpu_vidd_ob22.sql
@@ -1,29 +1,30 @@
-
-
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/View/DPU_VIDD_OB22.sql =========*** Run *** 
-PROMPT ===================================================================================== 
-
-
-PROMPT *** Create  view DPU_VIDD_OB22 ***
-
-  CREATE OR REPLACE FORCE VIEW BARS.DPU_VIDD_OB22 ("VIDD", "NAME", "KV", "NBS_DEP", "OB22_DEP", "NBS_INT", "OB22_INT", "NBS_EXP", "OB22_EXP", "NBS_RED", "OB22_RED") AS 
-  SELECT distinct
-       v.vidd, v.name, v.kv,
+create or replace force view DPU_VIDD_OB22
+( VIDD
+, NAME
+, KV
+, S181
+, NBS_DEP
+, OB22_DEP
+, NBS_INT
+, OB22_INT
+, NBS_EXP
+, OB22_EXP
+, NBS_RED
+, OB22_RED
+) as
+select distinct
+       v.VIDD, v.NAME, v.KV, o.S181,
        o.nbs_dep, o.ob22_dep,
        o.nbs_int, o.ob22_int,
        o.nbs_exp, o.ob22_exp,
        o.nbs_red, o.ob22_red
-  FROM BARS.DPU_VIDD v
-  LEFT JOIN BARS.DPU_TYPES_OB22 o ON (v.type_id = o.type_id AND v.bsd = o.nbs_dep AND decode(v.KV,980,1,2) = o.R034)
- WHERE v.flag = 1;
+  FROM DPU_VIDD v
+  LEFT 
+  JOIN DPU_TYPES_OB22 o
+    ON ( v.TYPE_ID = o.TYPE_ID AND v.BSD = o.NBS_DEP AND decode(v.KV,980,1,2) = o.R034 and v.DPU_TYPE = o.S181 )
+ WHERE v.FLAG = 1;
 
-PROMPT *** Create  grants  DPU_VIDD_OB22 ***
-grant DELETE,INSERT,SELECT,UPDATE                                            on DPU_VIDD_OB22   to BARS_ACCESS_DEFROLE;
-grant DELETE,INSERT,SELECT,UPDATE                                            on DPU_VIDD_OB22   to DPT_ADMIN;
+show errors;
 
-
-
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/View/DPU_VIDD_OB22.sql =========*** End *** 
-PROMPT ===================================================================================== 
+grant SELECT on DPU_VIDD_OB22 to BARS_ACCESS_DEFROLE;
+grant SELECT on DPU_VIDD_OB22 to DPT_ADMIN;
