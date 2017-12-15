431070e77d86fa59cc7dfd5e6bc14d97bc9e68a3
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2017-12-15 21:02:51 +0000

    #COBUMMFO-81# Коментар: скрипты на отключение джобов

--- sql/ru/sys/Script/disable_all_worked_jobs.sql
+++ sql/ru/sys/Script/disable_all_worked_jobs.sql
@@ -1,18 +1,18 @@
 delete from bars.tmp_all_enabled_jobs;
 commit;
 insert into bars.tmp_all_enabled_jobs(owner, job_type, job_name)
-select owner, 'S', job_name from dba_scheduler_jobs where owner in ('BARS','BARSUPL','BARS_DM','DM', 'PFU') and state = 'SHEDULED'
+select owner, 'S', job_name from dba_scheduler_jobs where owner in ('BARS','BARSUPL','BARS_DM','DM', 'PFU') and state = 'SHEDULED'  and  job_name in ('IMPORT_DAY', 'IMPORT_MONTH') 
+              or job_name like 'CIG%'
+              or job_name like 'EAD%'
+              or job_name like 'CRM_UPLOAD%'
 union all
 select  schema_user, 'J', to_char(job) from dba_jobs where broken = 'N';
 
 
 begin
-   for c in( select * from bars.tmp_all_enabled_jobs where job_name in ('IMPORT_DAY', 'IMPORT_MONTH') 
-              or job_name like 'CIG%'
-              or job_name like 'EAD%'
-              or job_name like 'CRM_UPLOAD%') loop
+   for c in( select * from bars.tmp_all_enabled_jobs ) loop
        if c.job_type = 'S' then
-          dbms_scheduler.disable(name => c.job_name, force => true);
+          dbms_scheduler.disable(name =>  c.owner||'.'||c.job_name, force => true);
        else
           dbms_ijob.broken( to_number(c.job_name), true);
        end if;
