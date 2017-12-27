14eef5e58a94e58efa589fe02e2b26901510e15f
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2017-12-27 06:06:28 +0000

    #COBUMMFO-6121# Коментар:

---
+++ ABS/master/sql/mmfo/bars/Script/rez_R013.sql
@@ -0,0 +1,19 @@
+begin
+   for m in (select * from mv_kf)
+   LOOP 
+      bc.go(m.kf);
+      for k in (select a.acc,t.r013 r013_new from accounts a,TMP_NBS_OB22_R013 t
+                where a.nbs||a.ob22 = t.nbs||t.ob22 and ostc <>0 )
+      LOOP
+         update specparam  set r013 = k.r013_new where acc=k.acc;
+         IF SQL%ROWCOUNT=0 then
+            insert into specparam (acc, R013) values (k.acc, k.R013_new);
+         end if;
+      end loop;
+      commit;
+   end loop;
+   commit; 
+   bc.go('/'); 
+end;
+/
+
