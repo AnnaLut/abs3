a54d982539d531bcfcbbaf808e991513a883457e
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-01-05 14:23:40 +0000

    #COBUMMFO-81# Коментар:

--- sql/mmfo/bars/View/ea_customer.sql
+++
@@ -1,5 +0,0 @@
-create or replace view ea_customer as
-select c.rnk, c.name_last||' '||c.name_first||' '||c.name_middle nmk, c.okpo, p.psp_issued   pdate, c.birthday bday
-from bars.fsc_CUSTOMER c, bars.fsc_customer_passport p
-where c.custype    = 3  -- Фізична особа
-  and p.rnk (+)    = c.rnk;
