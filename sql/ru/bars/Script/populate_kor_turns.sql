ad4769ee0d328e1de3efb06925c63d60962abfad
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-01-12 14:55:41 +0000

    Коментар: #COBUSUPABS-6896# Коментар:

--- sql/ru/bars/Script/populate_kor_turns.sql
+++ sql/ru/bars/Script/populate_kor_turns.sql
@@ -2,11 +2,8 @@ begin
     for k in (select kf from mv_kf)
     loop
         bc.subst_mfo(k.kf);
-		
-		ddraps(bankdate);
-		commit;
 
-        bars.nbur_prepare_turns (k.kf, bankdate);
+        bars.nbur_prepare_turns (k.kf, to_date('18122017','ddmmyyyy'));
         commit;
     end loop;
 end;
