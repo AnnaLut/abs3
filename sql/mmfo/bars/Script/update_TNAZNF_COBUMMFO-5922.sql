5ddfd675844dfcb71ff12b7f9f1617a51297668e
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-01-16 13:59:00 +0000

    #COBUMMFO-5922# Коментар: MErge

--- sql/mmfo/bars/Script/update_TNAZNF_COBUMMFO-5922.sql
+++ sql/mmfo/bars/Script/update_TNAZNF_COBUMMFO-5922.sql
@@ -2,6 +2,17 @@ begin
 update TNAZNF t
 set t.txt = '#(''Погашення простроченої заборгованості за користування системою <Клієнт-Банк>'')'
 where t.n = 5;
+update E_TARIF t
+set t.npk_3579 = replace(t.npk_3579,'за :R_DATE');
+update E_TARIF t
+set t.npk_3579 = replace(t.npk_3579,'за:R_DATE');
+update E_TARIF t
+set t.npk_3579 = replace(t.npk_3579,' :R_DATE');
+update E_TARIF t
+set t.npk_3579 = replace(t.npk_3579,':R_DATE');
   commit;
 end;
 /
+
+
+
