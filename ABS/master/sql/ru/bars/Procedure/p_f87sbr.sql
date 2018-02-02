20f644ea7d61cb9d3b62b3788b4137d41ad80c8b
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-02 14:20:14 +0000

    #COBUSUPABS-6979# Коментар: Merge.

--- ABS/master/sql/ru/bars/Procedure/p_f87sbr.sql
+++ ABS/master/sql/ru/bars/Procedure/p_f87sbr.sql
@@ -7,35 +7,35 @@ PROMPT =========================================================================
 
 PROMPT *** Create  procedure P_F87SBR ***
 
-  CREATE OR REPLACE PROCEDURE BARS.P_F87SBR (Dat_ DATE)  IS
+CREATE OR REPLACE PROCEDURE BARS.P_F87SBR (Dat_ DATE)  IS
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % DESCRIPTION :	Процедура формирование файла @87 для КБ
 % COPYRIGHT   :	Copyright UNITY-BARS Limited, 2009.All Rights Reserved.
-% VERSION     : 14.11.2017 (09/02/2016, 04/12/2013) 
+% VERSION     : 02/02/2018 (14.11.2017)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 параметры: Dat_ - отчетная дата
            sheme_ - схема формирования
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 14.11.2017 - удалил ненужные строки и изменил некоторые блоки формирования 
+% 14.11.2017 - удалил ненужные строки и изменил некоторые блоки формирования
 % 09.02.2016 - изменил блок наполнения таблицы REF_KOR
-% 04.12.2013 - не включались счета по которым за месяц отсутсвовали 
-%              Дт и Кт обороты. Если были только коректирующие обороты 
+% 04.12.2013 - не включались счета по которым за месяц отсутсвовали
+%              Дт и Кт обороты. Если были только коректирующие обороты
 %              то они не включались в файл (замечание Луганска)
 % 15.09.2012 - формируем в разрезе кодов территорий
 % 30.04.2011 - добавил acc,tobo в протокол
 % 15.03.2011 - в поле комментарий вносим код TOBO и название счета
-% 19.01.2011 до изменений выполненных Квас Л.Т. 18.05.2010 добавлены 
-%            изменения выполненные ОАВ также 18.05.2010 
-%            (убран блок заполнения спецпараметров в SPECPARAM_INT) 
+% 19.01.2011 до изменений выполненных Квас Л.Т. 18.05.2010 добавлены
+%            изменения выполненные ОАВ также 18.05.2010
+%            (убран блок заполнения спецпараметров в SPECPARAM_INT)
 % 18.05.2010 qwa отбираем только счета, по которым установлены правильно
 %            спецпараметры для отбора в 87 файл
 %            убрала "старые" хинты
-%            OAB убрал блок заполнения спецпараметров в SPECPARAM_INT 
+%            OAB убрал блок заполнения спецпараметров в SPECPARAM_INT
 % 26.01.2010 убрал присвоение Dat1_='31122008' и в условии отбора оборотов
-%            теперь будет условие Dat1_ >= (было Dat1_ > ) 
+%            теперь будет условие Dat1_ >= (было Dat1_ > )
 % 12.07.2009 убрал ORDER BY для табл. RNBU_TRACE
-% 07.07.2009 изменил условие substr(nbs,1,1)='8%' на nbs LIKE '8%'  
-% 02.07.2009 формируем показатели если p080<>'0000' и 'FFFF'  
+% 07.07.2009 изменил условие substr(nbs,1,1)='8%' на nbs LIKE '8%'
+% 02.07.2009 формируем показатели если p080<>'0000' и 'FFFF'
 % 25.03.2009 вместо VIEW V_OB22NU будем использовать V_OB22nu_N
 % 19.03.2009 исключил проверку на различие знаков для счетов 8 класса
 %            и для счетов 6,7 классов
@@ -116,8 +116,10 @@ f87k_    Number;
 userid_  Number;
 tobo_    accounts.tobo%TYPE;
 nms_     accounts.nms%TYPE;
+
+
 comm_    rnbu_trace.comm%TYPE;
-typ_    Number; 
+typ_    Number;
 nbuc1_  VARCHAR2(12);
 nbuc_   VARCHAR2(12);
 
@@ -131,30 +133,30 @@ CURSOR SCHETA IS
 ---Остатки на отчетную дату грн.
 CURSOR SaldoASeekOstf IS
    SELECT  a.acc, a.nls, a.kv, a.fdat, a.nbs, a.ostf-a.dos+a.kos,
-           a.tobo, a.nms           
+           a.tobo, a.nms
    FROM  (SELECT s.acc, s.nls, s.kv, aa.fdat, s.nbs, aa.ostf,
-         aa.dos, aa.kos, s.tobo, s.nms  
+         aa.dos, aa.kos, s.tobo, s.nms
          FROM saldoa aa, accounts s
          WHERE aa.acc=s.acc     AND
-              (s.acc,aa.fdat) in 
+              (s.acc,aa.fdat) in
                (select c.acc,max(c.fdat)
                 from saldoa c
-                where c.fdat <= dat_  
-                group by c.acc)) a   
-   WHERE a.kv = 980        
-     and a.nbs LIKE '8%' 
+                where c.fdat <= dat_
+                group by c.acc)) a
+   WHERE a.kv = 980
+     and a.nbs LIKE '8%'
      and a.nbs not in ('8605','8625','8999')
      and (exists (select 1 from v_ob22nu_n where accn=a.acc)
         or exists (select 1 from v_ob22nu80 where accn=a.acc));
 
 ---Обороты грн.
 CURSOR SaldoASeekOs IS
-   SELECT  a.acc, a.nls, a.kv, a.nbs, NVL(SUM(s.dos),0), NVL(SUM(s.kos),0), a.tobo, a.nms 
-   FROM saldoa s, accounts a 
-   WHERE s.fdat(+) >= Dat1_                
-     and s.fdat(+) <= dat_                   
-     and a.acc = s.acc(+)                   
-     and a.kv = 980                      
+   SELECT  a.acc, a.nls, a.kv, a.nbs, NVL(SUM(s.dos),0), NVL(SUM(s.kos),0), a.tobo, a.nms
+   FROM saldoa s, accounts a
+   WHERE s.fdat(+) >= Dat1_
+     and s.fdat(+) <= dat_
+     and a.acc = s.acc(+)
+     and a.kv = 980
      and a.nbs LIKE '8%'
      and a.nbs not in ('8605','8625','8999')
      and (exists (select 1 from v_ob22nu_n where accn = a.acc)
@@ -182,17 +184,17 @@ P_Proc_Set_Int(kodf_,sheme_,nbuc1_,typ_);
 ---корректирующие проводки
 DELETE FROM ref_kor ;
 
-IF to_char(Dat_,'MM') = '12' 
+IF to_char(Dat_,'MM') = '12'
 THEN
    INSERT INTO REF_KOR (REF, VOB, VDAT)
    SELECT /*+ PARALLEl(o) */
         REF, VOB, VDAT
    FROM OPER o
-   WHERE VDAT = any(select /*+ PARALLEL(f) */ max(fdat) 
-                         from fdat f 
-                         where fdat BETWEEN ADD_MONTHS (dat_, -2) AND ADD_MONTHS (dat_, 1) 
+   WHERE VDAT = any(select /*+ PARALLEL(f) */ max(fdat)
+                         from fdat f
+                         where fdat BETWEEN ADD_MONTHS (dat_, -2) AND ADD_MONTHS (dat_, 1)
                          group by trunc(fdat, 'mm'))
-          AND VOB IN (96, 99) 
+          AND VOB IN (96, 99)
           and not (((nlsa LIKE '6%' or nlsa LIKE '7%') and
                    (nlsb LIKE '5040%' or nlsb LIKE '5041%')) or
                   ((nlsa LIKE '5040%' or nlsa LIKE '5041%') and
@@ -202,9 +204,9 @@ ELSE
    SELECT /*+ PARALLEl(o) */
         REF, VOB, VDAT
    FROM OPER o
-   WHERE VDAT = any(select /*+ PARALLEL(f) */ max(fdat) 
-                         from fdat f 
-                         where fdat BETWEEN ADD_MONTHS (dat_, -2) AND ADD_MONTHS (dat_, 1) 
+   WHERE VDAT = any(select /*+ PARALLEL(f) */ max(fdat)
+                         from fdat f
+                         where fdat BETWEEN ADD_MONTHS (dat_, -2) AND ADD_MONTHS (dat_, 1)
                          group by trunc(fdat, 'mm'))
               AND VOB IN (96, 99);
 END IF ;
@@ -212,9 +214,9 @@ END IF ;
 DELETE FROM kor_prov ;
 INSERT INTO KOR_PROV (REF,  DK,  ACC , S,  FDAT , VDAT, SOS,  VOB)
 SELECT o.ref, o.dk, o.acc, o.s, o.fdat, p.vdat, o.sos, p.vob
-FROM opldok o, ref_kor p     
-WHERE o.fdat between Dat1_ AND Dat2_   
-  AND o.ref = p.ref      
+FROM opldok o, ref_kor p
+WHERE o.fdat between Dat1_ AND Dat2_
+  AND o.ref = p.ref
   AND o.sos = 5 ;
 
 -- Остатки грн. --
@@ -224,11 +226,11 @@ LOOP
    EXIT WHEN SaldoASeekOstf%NOTFOUND;
 
    SELECT count(*) INTO f87_ FROM sb_p0853 WHERE r020 = nbs_ ;
-   
-   IF f87_ > 0 and Ostn8_ <> 0 
+
+   IF f87_ > 0 and Ostn8_ <> 0
    THEN
 
-      IF typ_ > 0 
+      IF typ_ > 0
       THEN
          nbuc_ := NVL(F_Codobl_Tobo(acc_,typ_),nbuc1_);
       ELSE
@@ -253,8 +255,8 @@ LOOP
                        SUM(DECODE(d.dk, 1, d.s, 0))
          INTO acc1_, Dosnk_, Kosnk_
          FROM  kor_prov d
-         WHERE d.acc = acc_     
-           AND d.fdat between Dat_ AND Dat2_  
+         WHERE d.acc = acc_
+           AND d.fdat between Dat_+1 AND Dat2_
            AND d.vob = 96
          GROUP BY d.acc ;
       EXCEPTION WHEN NO_DATA_FOUND THEN
@@ -269,9 +271,9 @@ LOOP
 
       comm_ := substr(comm_ || tobo_ || '  ' || nms_, 1, 200);
 
-      IF kol6_7 = 0 
+      IF kol6_7 = 0
       THEN
-         IF pp_ not in ('0000','FFFF') and Ostn8_ <> 0 
+         IF pp_ not in ('0000','FFFF') and Ostn8_ <> 0
          THEN
             kodp_ := dk_ || pp_ || r020_fa_ || zz_;  --'000000' ;  --- nbs1_='0000',zz_='00'
             znap_ := TO_CHAR(ABS(Ostn8_));
@@ -292,10 +294,10 @@ LOOP
 
    SELECT count(*) INTO f87_ FROM sb_p0853 WHERE r020 = nbs_ ;
 
-   IF f87_ > 0 
-   THEN    
+   IF f87_ > 0
+   THEN
 
-      IF typ_ > 0 
+      IF typ_ > 0
       THEN
          nbuc_ := NVL(F_Codobl_Tobo(acc_,typ_),nbuc1_);
       ELSE
@@ -323,7 +325,7 @@ LOOP
          INTO acc1_, Dosnk_, Kosnk_
          FROM  kor_prov d
          WHERE d.acc = acc_
-           AND d.fdat between Dat_ AND Dat2_
+           AND d.fdat between Dat_+1 AND Dat2_
            AND d.vob = 96
          GROUP BY d.acc ;
       EXCEPTION WHEN NO_DATA_FOUND THEN
@@ -336,20 +338,20 @@ LOOP
       kol6_7 := 0;
       comm_ := substr(comm_ || tobo_ || '  ' || nms_, 1, 200);
 
-      IF kol6_7 = 0 
+      IF kol6_7 = 0
       THEN
-         IF pp_ not in ('0000','FFFF') and Kosn8_ > 0 
+         IF pp_ not in ('0000','FFFF') and Kosn8_ > 0
          THEN
-            kodp_ := '6' || pp_ || r020_fa_ || zz_;  --'000000' ;   --- nbs1_='0000',zz_='00'
+            kodp_ := '6' || pp_ || r020_fa_ || zz_;  
             znap_ := TO_CHAR(Kosn8_) ;
             INSERT INTO rnbu_trace     -- Дт. обороты в номинале валюты (грн.+вал.)
                     (nls, kv, odate, kodp, znap, acc, comm, tobo, nbuc)
             VALUES  (nls_, kv_, dat_, kodp_, znap_, acc_, comm_, tobo_, nbuc_) ;
          END IF;
 
-         IF pp_ not in ('0000','FFFF') and Dosn8_ > 0 
+         IF pp_ not in ('0000','FFFF') and Dosn8_ > 0
          THEN
-            kodp_ := '5' || pp_ || r020_fa_ || zz_;  --'000000' ;   --- nbs1_='0000',zz_='00'
+            kodp_ := '5' || pp_ || r020_fa_ || zz_;  
             znap_ := TO_CHAR(Dosn8_);
             INSERT INTO rnbu_trace     -- Кр. обороты в номинале валюты (грн.+вал.)
                     (nls, kv, odate, kodp, znap, acc, comm, tobo, nbuc)
