41d0109ecb0d4ba0706126144058dc84eaa4f2e4
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-02 14:22:23 +0000

    #COBUSFUND-40# Коментар: Merge.

---
+++ ABS/master/sql/mmfo/msp/Job/get_check_state.sql
@@ -0,0 +1,11 @@
+begin
+  sys.dbms_scheduler.create_job(job_name            => 'MSP.GET_CHECK_STATE',
+                                job_type            => 'PLSQL_BLOCK',
+                                job_action          => 'begin msp_utl.prepare_check_state; end;',
+                                start_date          => to_date('15-01-2018 00:00:00', 'dd-mm-yyyy hh24:mi:ss'),
+                                repeat_interval     => 'Freq=Hourly;ByHour=09, 12, 15, 18, 21',
+                                end_date            => to_date(null),
+                                job_class           => 'DBMS_JOB$',
+                                comments            => '');
+end;
+/
--- ABS/master/sql/mmfo/msp/Job/process_transport.sql
+++ ABS/master/sql/mmfo/msp/Job/process_transport.sql
@@ -10,10 +10,7 @@ end;
 begin
   sys.dbms_scheduler.create_job(job_name            => 'MSP.PROCESS_TRANSPORT',
                                 job_type            => 'PLSQL_BLOCK',
-                                job_action          => 'begin
-    msp_utl.prepare_check_state;
-    msp_utl.process_receipt;
-  end;',
+                                job_action          => 'begin msp_utl.process_receipt; end;',
                                 start_date          => to_date('27-12-2017 00:00:00', 'dd-mm-yyyy hh24:mi:ss'),
                                 repeat_interval     => 'Freq=Minutely;Interval=2',
                                 end_date            => to_date(null),
