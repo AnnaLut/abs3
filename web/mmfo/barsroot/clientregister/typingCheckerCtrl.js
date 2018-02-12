d963fecafddc347e64badf99c1f3eebe6dc0bf01
serhii.karchavets <serhii.karchavets@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-12 11:08:39 +0000

    #COBUMMFO-6638# Коментар: merge:COBUMMFO-6638 Владислав Петрусь

--- web/mmfo/barsroot/clientregister/typingCheckerCtrl.js
+++ web/mmfo/barsroot/clientregister/typingCheckerCtrl.js
@@ -1,6 +1,6 @@
 ﻿var TranslateDict = {
-    dictCode: [192, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191],
-    dictChar: "'ЙЦУКЕНГШЩЗХЇФІВАПРОЛДЖЄЯЧСМИТЬБЮ."
+	dictCode: [189, 32, 192, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, -85],
+	dictChar: "- 'ЙЦУКЕНГШЩЗХЇФІВАПРОЛДЖЄЯЧСМИТЬБЮ.Ґ"
 };
 var NMKCodes = {
 	codes: {
@@ -12,7 +12,7 @@ var NMKCodes = {
 			"ed_NMKV",		//destination international name
 			"ed_NMKK"],		//destination short international name
 		data: function () {
-			return (this.nameSource +","+ this.nameDestination).split(",");
+			return (this.nameSource + "," + this.nameDestination).split(",");
 		},
 		indexOf: function (token) {
 			for (var item in this.nameSource) {
@@ -26,46 +26,77 @@ var NMKCodes = {
 		return (this.codes.indexOf(token) > -1);
 	}
 };
+var charStack = {
+	comandChars: {
+		rAlt: 18,
+		space: 32
+	},
+	stack: [],
+	setEmpty: function () {
+		this.stack = [];
+	},
+	validateChar: function (item) {
+		if (this.stack.length > 0) {
+			if (this.stack[this.stack.length - 1] === this.comandChars["rAlt"]) {
+				this.setEmpty();
+				if (item === 85) {
+					return -item;
+				} else {
+					return item;
+				}
+			} if (this.stack[this.stack.length - 1] === this.comandChars["space"]) {
+				if (item === this.comandChars["space"]) {
+					return 0;
+				} else {
+					if (jQuery.inArray(item, TranslateDict.dictCode))
+						this.setEmpty();
+					return item;
+				}
+
+			}
+		} else {
+			if (item === this.comandChars["rAlt"] || item === this.comandChars["space"])
+				this.stack.push(item);
+			return item;
+		}
+	}
+};
 function translateCode(code) {
-    var loc_d = TranslateDict;
-    return loc_d.dictChar.charAt(jQuery.inArray(code, loc_d.dictCode));
+	var loc_d = TranslateDict;
+	return loc_d.dictChar.charAt(jQuery.inArray(code, loc_d.dictCode));
 }
-
 function isSpd() {
-    return getParamFromUrl('spd', document.URL) === '1';
+	return getParamFromUrl("spd", document.URL) === "1";
 }
-
 function translateLetters(e) {
-    var k = e.keyCode;
-
-    if (jQuery.inArray(k, TranslateDict.dictCode) > -1) {
-        e.stopImmediatePropagation();
-        e.preventDefault();
-        e.target.value += translateCode(k);
-    }
+	var k = charStack.validateChar(e.keyCode);
+	if (e.target.id == "ed_FIO_LN" && k == charStack.comandChars["space"])
+		return;
+	if (jQuery.inArray(k, TranslateDict.dictCode) > -1) {
+		e.stopImmediatePropagation();
+		e.preventDefault();
+		e.target.value += translateCode(k);
+	}
 
 	//при изменении фио, синхронизируем зависимые поля.
-	if (NMKCodes.containsSourceCode(e.target.id) ) {
-        fieldSyncLock.events += 1;
-    }
+	if (NMKCodes.containsSourceCode(e.target.id)) {
+		fieldSyncLock.events += 1;
+	}
 }
-
 function isCustomer() {
-    return (parent.obj_Parameters['CUSTTYPE'] == "person" && !isSpd())
+	return (parent.obj_Parameters["CUSTTYPE"] == "person" && !isSpd())
 }
-
 function initTranslate(e) {
-    if (isCustomer()) {
-        translateLetters(e);
-    }
+	if (isCustomer()) {
+		translateLetters(e);
+	}
 }
 var fieldSyncLock = {
-    events: 0
+	events: 0
 };
-
 setInterval(function () {
-    if (fieldSyncLock.events > 0) {
-        fieldSyncLock.events = 0;
-        EditNMKtext();
-    }
+	if (fieldSyncLock.events > 0) {
+		fieldSyncLock.events = 0;
+		EditNMKtext();
+	}
 }, 1200);
