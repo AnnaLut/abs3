76297a130b076e910a3b02b0f95a62c07bf8c8d2
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-12 09:30:57 +0000

    #COBUMMFO-6370# Коментар:

--- sql/mmfo/bars_dm/Data/dm_obj.sql
+++ sql/mmfo/bars_dm/Data/dm_obj.sql
@@ -2,79 +2,57 @@ prompt перезаливаем dm_obj - объекты для выгрузок
 
 delete from dm_obj;
 
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CREDITS_D', 'DAY', 'dm_import.credits_dyn_imp', 1, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('DEPOSITS', 'DAY', 'dm_import.deposits_imp', 3, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('ACCOUNTS', 'DAY', 'dm_import.accounts_imp', 4, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('BPK', 'DAY', 'dm_import.bpk_imp', 5, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CLIENTFO', 'DAY', 'dm_import.customers_imp', 6, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CLIENTUR', 'DAY', 'dm_import.custur_imp', 7, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('ZASTAVA', 'DAY', 'dm_import.zastava_imp', 8, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CUSTREL', 'DAY', 'dm_import.custur_rel_imp', 9, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('DEPOSITS_PLT', 'DAY', 'dm_import.deposits_plt_imp', 10, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('BPK_PLT', 'DAY', 'dm_import.bpk_plt_imp', 11, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CUSTOMERS_PLT', 'DAY', 'dm_import.customers_plt_imp', 12, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CUSTSEGM', 'DAY', 'dm_import.cust_segm_imp', 13, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CREDITS_S', 'DAY', 'dm_import.credits_stat_imp', 14, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CLIENTFO', 'MONTH', 'dm_import.customers_imp', 1, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('DEPOSITS', 'MONTH', 'dm_import.deposits_imp', 2, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('ACCOUNTS', 'MONTH', 'dm_import.accounts_imp', 3, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('BPK', 'MONTH', 'dm_import.bpk_imp', 4, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CLIENTUR', 'MONTH', 'dm_import.custur_imp', 5, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CUSTREL', 'MONTH', 'dm_import.custur_rel_imp', 6, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CUSTSEGM', 'MONTH', 'dm_import.cust_segm_imp', 7, 0);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('DEPOSITS_PLT', 'MONTH', 'dm_import.deposits_plt_imp', 8, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('BPK_PLT', 'MONTH', 'dm_import.bpk_plt_imp', 9, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CUSTOMERS_PLT', 'MONTH', 'dm_import.customers_plt_imp', 10, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CREDITS_ZAL', 'MONTH', 'dm_import.credits_zal_imp', 11, 1);
-
-insert into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE)
-values ('CREDITS_ZAL', 'DAY', 'dm_import.credits_zal_imp', 15, 1);
+insert all
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CREDITS_D', 'DAY', 'dm_import.credits_dyn_imp', 1, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('DEPOSITS', 'DAY', 'dm_import.deposits_imp', 3, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('ACCOUNTS', 'DAY', 'dm_import.accounts_imp', 4, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('BPK', 'DAY', 'dm_import.bpk_imp', 5, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CLIENTFO', 'DAY', 'dm_import.customers_imp', 6, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CLIENTUR', 'DAY', 'dm_import.custur_imp', 7, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('ZASTAVA', 'DAY', 'dm_import.zastava_imp', 8, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CUSTREL', 'DAY', 'dm_import.custur_rel_imp', 9, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('DEPOSITS_PLT', 'DAY', 'dm_import.deposits_plt_imp', 10, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('BPK_PLT', 'DAY', 'dm_import.bpk_plt_imp', 11, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CUSTOMERS_PLT', 'DAY', 'dm_import.customers_plt_imp', 12, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CUSTSEGM', 'DAY', 'dm_import.cust_segm_imp', 13, 1, 'N')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CREDITS_S', 'DAY', 'dm_import.credits_stat_imp', 14, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CLIENTFO', 'MONTH', 'dm_import.customers_imp', 1, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('DEPOSITS', 'MONTH', 'dm_import.deposits_imp', 2, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('ACCOUNTS', 'MONTH', 'dm_import.accounts_imp', 3, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('BPK', 'MONTH', 'dm_import.bpk_imp', 4, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CLIENTUR', 'MONTH', 'dm_import.custur_imp', 5, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CUSTREL', 'MONTH', 'dm_import.custur_rel_imp', 6, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CUSTSEGM', 'MONTH', 'dm_import.cust_segm_imp', 7, 1, 'N')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('DEPOSITS_PLT', 'MONTH', 'dm_import.deposits_plt_imp', 8, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('BPK_PLT', 'MONTH', 'dm_import.bpk_plt_imp', 9, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CUSTOMERS_PLT', 'MONTH', 'dm_import.customers_plt_imp', 10, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CREDITS_ZAL', 'MONTH', 'dm_import.credits_zal_imp', 11, 1, 'Y')
+into dm_obj (OBJ_NAME, IMP_TYPE, OBJ_PROC, IMP_ORDER, ACTIVE, PARALLEL_FLAG)
+values ('CREDITS_ZAL', 'DAY', 'dm_import.credits_zal_imp', 15, 1, 'Y')
+select 1 from dual;
 
 commit;
---
+++ sql/mmfo/sys/User/BARS_DM.sql
@@ -0,0 +1,3 @@
+prompt sys\user\bars_dm
+prompt grant create type
+grant create type to bars_dm;
