2a9ff78fdd07319a18b04410e3b98fb930512b83
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-16 09:22:33 +0000

    Коментар: #COBUMMFO-5345# Коментар:

--- sql/ru/bars/Data/fill_table/customer_field.sql
+++ sql/ru/bars/Data/fill_table/customer_field.sql
@@ -130,3 +130,13 @@ end;
 /
 
 COMMIT;
+
+prompt add FMPOS reqv
+begin
+insert into customer_field (TAG, NAME, B, U, F, TABNAME, BYISP, TYPE, OPT, TABCOLUMN_CHECK, SQLVAL, CODE, NOT_TO_EDIT, HIST, PARID, U_NREZ, F_NREZ, F_SPD)
+values ('FMPOS', 'Висн. щодо наявн.у кл-та потенц. та реал. фін.можл.для провед. опер.', 1, 1, 1, 'FM_POSS', null, null, null, 'NAME', null, 'FM', 0, null, null, 0, 1, 1);
+commit;
+exception
+when dup_val_on_index then null;
+end;
+/
