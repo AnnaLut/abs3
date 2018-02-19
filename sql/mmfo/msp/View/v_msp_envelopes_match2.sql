bdebbb4fdf2432bde51649fee814fa605d14e10e
artem.iurchenko <artem.iurchenko@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-19 14:53:30 +0000

    #COBUSFUND-42# Коментар: доопрацювання квитанції 2 та новий алгоритм обробки файлу

--- sql/mmfo/msp/View/v_msp_envelopes_match2.sql
+++ sql/mmfo/msp/View/v_msp_envelopes_match2.sql
@@ -6,26 +6,27 @@ PROMPT =========================================================================
 PROMPT *** Create view v_msp_envelopes_match2 ***
 
 create or replace view v_msp_envelopes_match2 as
-select e.id, 
-       e.id_msp_env, 
-       e.code, 
-       e.sender, 
-       e.recipient, 
-       e.partnumber, 
-       e.parttotal, 
-       e.comm, 
+select e.id,
+       e.id_msp_env,
+       e.code,
+       e.sender,
+       e.recipient,
+       e.partnumber,
+       e.parttotal,
+       e.comm,
        cast(e.create_date as date) create_date,
        -- (select sum(f.pay_sum)*0.01 from msp_file_records fr where fr.file_id = f.id and fr.state_id in (0)) sum_to_pay, --total_sum
        null total_sum,
        null total_sum_to_pay,
-       e.state state_id, 
+       e.state state_id,
        s.name  state_name,
        s.state state_code
 from msp_envelopes e
      inner join msp_envelope_state s on s.id = e.state
 where e.code in ('payment_data')
-      and 0 = (select sum(case when f.state_id in (9) then 0 else 1 end) from msp_files f where f.envelope_file_id = e.id);
-
+      and e.state in (0,9,11,12,13,14,15)
+      and 0 = (select sum(case when f.state_id in (9) then 0 else 1 end) from msp_files f where f.envelope_file_id = e.id)
+;
 PROMPT *** Create comments on v_msp_envelopes_match2 ***
 comment on column v_msp_envelopes_match2.total_sum is 'Загальна сума конверту';
 comment on column v_msp_envelopes_match2.total_sum_to_pay is 'Загальна сума для оплати';
