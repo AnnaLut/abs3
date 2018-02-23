8c7910d5a8fd620941f38f01d23d76871079b1ea
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-23 10:58:23 +0000

    #COBUMMFO-5459# Коментар: MERGE: COBUMMFO-5459 Олександр Кузяков

--- ABS/master/web/mmfo/barsroot/customerlist/Scripts/CustAcc.js
+++ ABS/master/web/mmfo/barsroot/customerlist/Scripts/CustAcc.js
@@ -147,6 +147,7 @@ function InitCustAcc() {
         || (type == 3 && getParamFromUrl("nd", location.href))
         || (type == 5 && getParamFromUrl("bpkw4nd", location.href))
         || type == 8
+        || type == 9 /* перегляд рахунків угоди з АРМ Цінні Папери ->ЦП Потрфель Загальний */
     ) {
         obj.v_notFill = false;
     }
