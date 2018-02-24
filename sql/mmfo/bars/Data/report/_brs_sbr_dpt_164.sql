f802410430405c24f79dc6fcb95808ba270de9d3
oleksandr.lypskykh <oleksandr.lypskykh@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-24 10:10:54 +0000

    #COBUMMFO-5135# merge

---
+++ sql/mmfo/bars/Data/report/_brs_sbr_dpt_164.sql
@@ -0,0 +1,213 @@
+prompt =====================================
+prompt == БПК рахунки,  контроль зарах. 35 та 90 днів
+prompt =====================================
+
+set serveroutput on
+set feed off
+declare
+
+   nlchr       char(2):=chr(13)||chr(10);
+   l_zpr       zapros%rowtype;
+   l_zprr      zapros%rowtype;
+   l_rep       reports%rowtype;
+   l_repfolder number;
+   l_isnew     smallint:=0;
+   l_isnewr    smallint:=0;
+   l_message   varchar2(1000);
+
+begin
+   l_zpr.name := 'БПК рахунки,  контроль зарах. 35 та 90 днів';
+   l_zpr.pkey := '\BRS\SBR\DPT\164';
+
+   l_message  := 'Ключ запроса: '||l_zpr.pkey||'  '||nlchr;
+
+   begin
+      select kodz, kodr into l_zpr.kodz, l_zpr.kodr
+      from zapros where pkey=l_zpr.pkey;
+   exception when no_data_found then
+      l_isnew:=1;
+      select s_zapros.nextval into l_zpr.kodz from dual;
+      if (0>0) then
+         select s_zapros.nextval into l_zpr.kodr from dual;
+         l_zprr.kodz:=l_zpr.kodr;
+      end if;
+   end;
+
+
+    ------------------------
+    --  main query        --
+    ------------------------
+
+    l_zpr.id           := 1;
+    l_zpr.name         := 'БПК рахунки,  контроль зарах. 35 та 90 днів';
+    l_zpr.namef        := '';  
+    l_zpr.bindvars     := ':zdate=''Звітна дата (DD.MM.YYYY) '',:Nbpk=''Вкл. нац. карту? (1-так / 0,пусто - ні) ''';
+    l_zpr.create_stmt  := '';
+    l_zpr.rpt_template := 'rep_5520.frx';
+    l_zpr.form_proc    := '';
+    l_zpr.default_vars := '';
+    l_zpr.bind_sql     := '';
+    l_zpr.xml_encoding := 'CL8MSWIN1251';
+    l_zpr.txt          := ' select x.branch, x.nls, x.kv
+     , to_char(x.daos,''dd.mm.yyyy'') as daos
+     , x.card_code, x.grp_prod, x.rnk, x.nmk, x.okpo, x.bday, x.v
+     , x.mtel, x.dtel
+     , x.lim_bpk
+     , case when nvl(z2.SZ2,0) <> 0 then ''так'' else ''ні'' end as z_35
+     , case when nvl(z3.SZ2,0) <> 0 then ''так'' else ''ні'' end as z_90
+     , x.zp_id, x.zp_okpo, x.zp_name
+     , x.id as zp_id
+--     , (select nvl(trim(okpo),'')||''-''||nvl(trim(name),'') from bpk_proect where TRIM(TO_CHAR(ID))=TRIM(x.id)) as zp_name
+from (
+select a.acc, a.branch, a.nls, a.kv, a.daos
+     , w.card_code, wg.name as grp_prod
+     , c.rnk, c.nmk, c.okpo, cw.value as mtel, p.TELD as dtel
+     , aw.value as id
+     , bp.id as zp_id, bp.okpo as zp_okpo, bp.name as zp_name
+     , p.BDAY, case when p.bday is NULL then NULL else trunc((sysdate-p.bday)/(365.25)+0.001) end as v
+     , case when (nvl(abs(fost(w.acc_ovr,to_date(:zdate,''dd.mm.yyyy''))),0)+nvl(abs(fost(w.acc_9129,to_date(:zdate,''dd.mm.yyyy''))),0)) > 0 then ''так'' else ''ні'' end as Lim_BPK
+from accounts a left join w4_acc w   on a.acc=w.acc_pk and a.kf = w.kf
+                left join customer c on a.rnk=c.rnk
+                left join customerw cw on c.rnk=cw.rnk and trim(cw.tag)=''MPNO''
+                left join person p   on c.rnk=p.rnk
+                left join w4_card wc on wc.CODE=w.CARD_CODE and wc.kf = w.kf
+                left join w4_product wp on wp.code=wc.product_code and wp.kf = wc.kf
+                left join w4_product_groups wg on wg.code=wp.GRP_CODE
+                LEFT JOIN accountsw aw ON (a.acc = aw.acc AND trim(aw.tag) = ''PK_PRCT'')
+                LEFT JOIN bpk_proect bp ON TRIM(aw.VALUE) = TRIM(TO_CHAR (bp.ID))
+where a.nbs = ''2625'' 
+  and (a.dazs is NULL or a.dazs>to_date(:zdate,''dd.mm.yyyy''))
+  and a.daos <= to_date(:zdate,''dd.mm.yyyy'')
+  and w.dat_close is NULL
+  and c.k050=''000'' and c.CUSTTYPE=3
+  and w.acc_pk is not NULL  
+  and instr(a.branch, sys_context(''bars_context'',''user_mfo'')) > 0           
+  and w.card_code not like (case when nvl(:Nbpk,''0'')<>''0'' then ''0'' else ''LOC%'' end)
+--  and instr((select min(bb.obl) from branch bb where bb.branch=a.branch and bb.date_closed is NULL), case when :KO is NULL then (select min(bb.obl) from branch bb where bb.branch=a.branch and bb.date_closed is NULL) else :KO end)>0  
+--  and instr(upper(w.card_code), upper(nvl(:PROD,w.card_code)))>0
+) x 
+left join   
+( select a2.acc
+        , round(sum(nvl(o2.s,0))/100,2) as SZ2
+   from accounts a2, opldok o2
+   where a2.nbs = ''2625''
+     and (a2.dazs is NULL or a2.dazs > to_date(:zdate,''dd.mm.yyyy''))  
+     and a2.daos <= to_date(:zdate,''dd.mm.yyyy'') 
+     and o2.acc=a2.acc
+     AND o2.dk = 1
+     AND o2.fdat >= TO_DATE (:zdate, ''dd.mm.yyyy'')-35+1
+     AND o2.fdat <= TO_DATE (:zdate, ''dd.mm.yyyy'')
+     AND o2.tt not in (''OW1'',''OW2'',''OWK'')
+--     and instr((select min(bb.obl) from branch bb where bb.branch=a2.branch and bb.date_closed is NULL), case when :KO is NULL then (select min(bb.obl) from branch bb where bb.branch=a2.branch and bb.date_closed is NULL) else :KO end)>0
+   group by a2.acc
+ ) z2 on x.acc=z2.acc
+left join   
+( select a2.acc
+        , round(sum(nvl(o2.s,0))/100,2) as SZ2
+   from accounts a2, opldok o2
+   where a2.nbs = ''2625''
+     and (a2.dazs is NULL or a2.dazs > to_date(:zdate,''dd.mm.yyyy''))  
+     and a2.daos <= to_date(:zdate,''dd.mm.yyyy'') 
+     and o2.acc=a2.acc
+     AND o2.dk = 1
+     AND o2.fdat >= TO_DATE (:zdate, ''dd.mm.yyyy'')-90+1
+     AND o2.fdat <= TO_DATE (:zdate, ''dd.mm.yyyy'')
+     AND o2.tt not in (''OW1'',''OW2'',''OWK'')
+--     and instr((select min(bb.obl) from branch bb where bb.branch=a2.branch and bb.date_closed is NULL), case when :KO is NULL then (select min(bb.obl) from branch bb where bb.branch=a2.branch and bb.date_closed is NULL) else :KO end)>0
+   group by a2.acc
+ ) z3 on x.acc=z3.acc';
+    l_zpr.xsl_data     := '';
+    l_zpr.xsd_data     := '';
+
+    if l_isnew = 1 then
+       insert into zapros values l_zpr;
+       l_message:=l_message||'Добавлен новый кат.запрос №'||l_zpr.kodz||'.';
+    else
+       update zapros set name         = l_zpr.name,
+                         namef        = l_zpr.namef,
+                         bindvars     = l_zpr.bindvars,
+                         create_stmt  = l_zpr.create_stmt,
+                         rpt_template = l_zpr.rpt_template,
+                         form_proc    = l_zpr.form_proc,
+                         default_vars = l_zpr.default_vars,
+                         bind_sql     = l_zpr.bind_sql,
+                         xml_encoding = l_zpr.xml_encoding,
+                         txt          = l_zpr.txt,
+                         xsl_data     = l_zpr.xsl_data,
+                         xsd_data     = l_zpr.xsd_data
+       where pkey=l_zpr.pkey;
+       l_message:=l_message||'Кат.запрос c таким ключем уже существует под №'||l_zpr.kodz||', его параметры изменены.';
+
+    end if;
+    ------------------------    
+    --  report            --    
+    ------------------------    
+                                
+
+    l_rep.name        :='Empty';
+    l_rep.description :='БПК рахунки,  контроль зарах. 35 та 90 днів';
+    l_rep.form        :='frm_FastReport';
+    l_rep.param       :=l_zpr.kodz||',3,sFdat,sFdat2,"",FALSE,FALSE';
+    l_rep.ndat        :=2;
+    l_rep.mask        :='';
+    l_rep.usearc      :=0;
+    l_rep.idf         :=null;    
+
+    -- Фиксированный № печатного отчета   
+    l_rep.id          := 5520;
+
+
+    if l_isnew = 1 then                     
+       begin                                
+          insert into reports values l_rep;        
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then  
+           bars_error.raise_error('REP',14, to_char(l_rep.id));
+       end;                                    
+    else                                            
+       begin                                        
+          insert into reports values l_rep;         
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then         
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' изменен.';
+          update reports set                
+             name        = l_rep.name,       
+             description = l_rep.description,
+             form        = l_rep.form,       
+             param       = l_rep.param,      
+             ndat        = l_rep.ndat,       
+             mask        = l_rep.mask,       
+             usearc      = l_rep.usearc,     
+             idf         = l_rep.idf         
+          where id=l_rep.id;                 
+       end;                                  
+    end if;                                  
+
+    begin
+       Insert into BARS.APP_REP
+               (CODEAPP, CODEREP, APPROVE, GRANTOR)
+       Values
+               ('$RM_NBUR', l_rep.id, 1, 1);
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' добавлен в АРМ Звітність (новий)';
+    exception when dup_val_on_index
+          then 
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' существует в АРМ Звітність (новий)';
+    end;
+
+    begin
+       Insert into BARS.APP_REP
+               (CODEAPP, CODEREP, APPROVE, GRANTOR)
+       Values
+               ('$RM_DRU1', l_rep.id, 1, 1);
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' добавлен в АРМ Друк звітів';
+    exception when dup_val_on_index
+          then 
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' существует в АРМ Друк звітів';
+    end;
+
+    bars_report.print_message(l_message);
+end;
+/
+
+commit;
+  
--- sql/mmfo/bars/Data/report/_brs_sbr_dpt_202.sql
+++ sql/mmfo/bars/Data/report/_brs_sbr_dpt_202.sql
@@ -40,30 +40,30 @@ begin
 
     l_zpr.id           := 1;
     l_zpr.name         := 'Звіт про кількість нових депозитів (по бранчу)';
-    l_zpr.namef        := 'my.txt';
-    l_zpr.bindvars     := ':Param1=''Дата з:'',:Param0=''Дата по:'',:S=''Сума(грн)''';
+    l_zpr.namef        := '';
+    l_zpr.bindvars     := ':Param1=''Дата з:'',:Param0=''Дата по:''';
     l_zpr.create_stmt  := '';
-    l_zpr.rpt_template := '';
+    l_zpr.rpt_template := 'rep_5515.frx';
     l_zpr.form_proc    := '';
     l_zpr.default_vars := '';
     l_zpr.bind_sql     := '';
     l_zpr.xml_encoding := 'CL8MSWIN1251';
     l_zpr.txt          := 'select substr(b_all,1,15)b_1tvbv,b_all,s_all,c_all,s_new,c_new,s_renew,c_renew,s_new_dep,c_new_dep,s_all20k,c_all20k
     from ( select sum(s) s_all,b b_all ,sum(col) c_all
-            from(select round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
+            from(select /*+use_nl(a)*/ round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
                 from dpt_deposit d inner join accounts a on d.acc=a.acc and a.nbs in (2630,2635)
                                    inner join customer c on a.rnk=c.rnk                  
-                where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0
+                where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0 and d.kv <> 980
                 group by substr(d.branch,1,22),d.kv,dat_begin,a.acc,a.ostc,c.rnk,c.okpo,d.deposit_id
                 order by substr(d.branch,1,22),d.kv,a.acc,a.ostc)
             group by b
             order by b) s1 
         left join 
         (select sum(s) s_new,b b_new,sum(col) c_new
-            from(select round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
+            from(select /*+use_nl(a)*/ round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
                 from dpt_deposit d inner join accounts a on d.acc=a.acc and a.nbs in (2630,2635)
                                    inner join customer c on a.rnk=c.rnk                  
-                where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0
+                where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0 and d.kv <> 980
                         and d.rnk in (select rnk from customer where date_on between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') )  
                         and c.okpo not in (select okpo  from customer where date_on < to_date(:Param1,''DD.MM.YYYY'') and (k050 in (''000'') and custtype=3) and okpo=c.okpo )                      
                 group by substr(d.branch,1,22),d.kv,dat_begin,a.acc,a.ostc,c.rnk,c.okpo,d.deposit_id
@@ -72,10 +72,10 @@ begin
         order by b)s2 ON b_all=b_new
         left join 
         (select sum(s) s_renew,b b_renew,sum(col) c_renew
-            from(select c.rnk,c.okpo,round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col,d.deposit_id
+            from(select /*+use_nl(a)*/ c.rnk,c.okpo,round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col,d.deposit_id
                 from dpt_deposit d inner join accounts a on d.acc=a.acc and a.nbs in (2630,2635)
                                    inner join customer c on a.rnk=c.rnk                 
-                where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0
+                where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0 and d.kv <> 980
                         and c.okpo in (select okpo  from customer where date_on < to_date(:Param1,''DD.MM.YYYY'') and (k050 in (''000'') and custtype=3) and okpo=c.okpo )                         
                 group by substr(d.branch,1,22),d.kv,dat_begin,a.acc,a.ostc,c.rnk,c.okpo,d.deposit_id
                 order by c.date_on,c.rnk,c.okpo,substr(d.branch,1,22),d.kv,a.acc,a.ostc)
@@ -83,9 +83,9 @@ begin
         order by b)s2 ON b_all=b_renew
         left join 
         (select sum(s) s_new_dep,b b_new_dep,sum(col) c_new_dep
-            from(select round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
+            from(select /*+use_nl(a)*/ round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
                  from dpt_deposit d inner join accounts a on d.acc=a.acc and a.nbs in (2630,2635)
-                 where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0
+                 where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0 and d.kv <> 980
                          and d.rnk not in (select rnk from dpt_deposit where dat_begin <= to_date(:Param1,''DD.MM.YYYY'') and acc in (select acc from accounts where nbs<>2620 group by acc)
                                            union all
                                            select rnk from dpt_deposit_clos where dat_end <= add_months(to_date(:Param1,''DD.MM.YYYY''),-12) and acc in (select acc from accounts where nbs<>2620 group by acc)  ) 
@@ -95,12 +95,11 @@ begin
         order by b)s2 ON b_all=b_new_dep                   
         left join 
         (select sum(s) s_all20k,b b_all20k,sum(col) c_all20k
-        from(select round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
+        from(select /*+use_nl(a)*/ round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2) s,d.kv,substr(d.branch,1,22) b,d.DAT_BEGIN,count(*) col
             from dpt_deposit d inner join accounts a on d.acc=a.acc and a.nbs in (2630,2635)
                                inner join customer c on a.rnk=c.rnk              
-            where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0
+            where d.dat_begin between to_date(:Param1,''DD.MM.YYYY'')  and to_date(:Param0,''DD.MM.YYYY'') and a.ostc<>0 and d.kv <> 980
             group by substr(d.branch,1,22),d.kv,dat_begin,a.acc,a.ostc,c.rnk,c.okpo,d.deposit_id
-            having round(a.ostc*f_ret_rate(d.kv,d.dat_begin)/100,2)>=:S
             order by substr(d.branch,1,22),d.kv,a.acc,a.ostc)
         group by b
         order by b)s3 ON b_all=b_all20k';
@@ -127,6 +126,71 @@ begin
        l_message:=l_message||'Кат.запрос c таким ключем уже существует под №'||l_zpr.kodz||', его параметры изменены.';
 
     end if;
+    ------------------------    
+    --  report            --    
+    ------------------------    
+                                
+
+    l_rep.name        :='Empty';
+    l_rep.description :='Звіт про кількість нових депозитів (по бранчу)';
+    l_rep.form        :='frm_FastReport';
+    l_rep.param       :=l_zpr.kodz||',3,sFdat,sFdat2,"",FALSE,FALSE';
+    l_rep.ndat        :=2;
+    l_rep.mask        :='';
+    l_rep.usearc      :=0;
+    l_rep.idf         :=null;    
+
+    -- Фиксированный № печатного отчета   
+    l_rep.id          := 5515;
+
+
+    if l_isnew = 1 then                     
+       begin                                
+          insert into reports values l_rep;        
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then  
+           bars_error.raise_error('REP',14, to_char(l_rep.id));
+       end;                                    
+    else                                            
+       begin                                        
+          insert into reports values l_rep;         
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then         
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' изменен.';
+          update reports set                
+             name        = l_rep.name,       
+             description = l_rep.description,
+             form        = l_rep.form,       
+             param       = l_rep.param,      
+             ndat        = l_rep.ndat,       
+             mask        = l_rep.mask,       
+             usearc      = l_rep.usearc,     
+             idf         = l_rep.idf         
+          where id=l_rep.id;                 
+       end;                                  
+    end if;                                  
+
+    begin
+       Insert into BARS.APP_REP
+               (CODEAPP, CODEREP, APPROVE, GRANTOR)
+       Values
+               ('$RM_NBUR', l_rep.id, 1, 1);
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' добавлен в АРМ Звітність (новий)';
+    exception when dup_val_on_index
+          then 
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' существует в АРМ Звітність (новий)';
+    end;
+
+    begin
+       Insert into BARS.APP_REP
+               (CODEAPP, CODEREP, APPROVE, GRANTOR)
+       Values
+               ('$RM_DRU1', l_rep.id, 1, 1);
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' добавлен в АРМ Друк звітів';
+    exception when dup_val_on_index
+          then 
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' существует в АРМ Друк звітів';
+    end;
 
     bars_report.print_message(l_message);
 end;
