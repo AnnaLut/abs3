36788e05aa24129871b6509aaf2f57f2a92b2da4
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-02-26 12:21:08 +0000

    #COBUMMFO-6804# Коментар:

--- sql/ru/bars/Script/tts191992.sql
+++ sql/ru/bars/Script/tts191992.sql
@@ -2,8 +2,42 @@ BEGIN
    bc.home;
 
    UPDATE tts
-      SET nlsk = '37391192', nlsb = '37391192'
+      SET nlsk = '37391192'
     WHERE nlsk LIKE '191992%';
+END;
+/
+COMMIT
+/
+
+
+BEGIN
+   bc.home;
+
+   UPDATE tts
+      SET  nlsb = '37391192'
+    WHERE nlsb LIKE '191992%';
+END;
+/
+COMMIT
+/
+
+BEGIN
+   bc.home;
+
+   UPDATE tts
+      SET  nlsm = '37391192'
+    WHERE nlsm LIKE '191992%';
+END;
+/
+COMMIT
+/
+
+BEGIN
+   bc.home;
+
+   UPDATE tts
+      SET  nlsa = '37391192'
+    WHERE nlsa LIKE '191992%';
 END;
 /
 COMMIT
