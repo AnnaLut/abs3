fa75ed0550034c7a1371b40d474e0660e72e487d
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-05 10:44:25 +0000

    #COBUPRVNIX-92# Коментар:

--- ABS/master/sql/ru/bars/Table/accountsw.sql
+++ ABS/master/sql/ru/bars/Table/accountsw.sql
@@ -115,16 +115,6 @@ exception when others then
 /
 
 
-PROMPT *** Create  constraint CC_ACCOUNTSW_TAG ***
-begin
-  execute immediate 'alter table accountsw
-  add constraint accountsw_tag
-  check ((tag IN (''INTRT'',''ND_REST'') and (txt between 0 and 100 or tag is null)) or value not IN (''INTRT'',''ND_REST''))';
- exception when others then 
-    if sqlcode = -2264 or sqlcode = -2261 then null; else raise; 
-    end if; 
-end;
-/
 
 PROMPT *** Create  index PK_ACCOUNTSW ***
 begin   
