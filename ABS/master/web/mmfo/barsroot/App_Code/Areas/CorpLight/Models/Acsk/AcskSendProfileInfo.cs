26bc8d2d986b38bd922154f3d364f6e6e3782b26
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-07 15:13:46 +0000

    #COBUMMFO-81# Коментар: откат обновлений по корплайту

---
+++ ABS/master/web/mmfo/barsroot/App_Code/Areas/CorpLight/Models/Acsk/AcskSendProfileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace BarsWeb.Areas.CorpLight.Models.Acsk
+{
+    public class AcskSendProfileInfo
+    {
+        public AcskSendProfileInfo()
+        {
+        }
+
+        public int? RegistrationId { get; set; }
+        public string UserId { get; set; }
+    }
+}
