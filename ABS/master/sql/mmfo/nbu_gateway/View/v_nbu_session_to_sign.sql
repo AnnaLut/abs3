f15d8df95f096cdf9848f02f746f4889a45ac082
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-15 00:59:47 +0000

    Коментар: #COBUMMFO-5266# Коментар:

--- ABS/master/sql/mmfo/nbu_gateway/View/v_nbu_session_to_sign.sql
+++ ABS/master/sql/mmfo/nbu_gateway/View/v_nbu_session_to_sign.sql
@@ -1,6 +1,6 @@
 create or replace view v_nbu_session_to_sign as
 select s.id, nbu_object_utl.get_object_json(s.object_id, s.report_id) data_to_sign
 from   nbu_session s
-where  s.state_id = 1 /*SESSION_STATE_NEW*/;
+where  s.state_id = 2 /*SESSION_STATE_TO_SIGN*/;
 
 grant all on v_nbu_session_to_sign to bars_access_defrole;
