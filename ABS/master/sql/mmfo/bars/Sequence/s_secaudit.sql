9fbf7d5109b30b7a5d4e45984004845fac3b4ec1
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-15 13:54:59 +0000

    #COBUMMFO-6455# Коментар: Merge. NOORDER -> ORDER = правильний порядок подій в SEC_AUDIT

--- ABS/master/sql/mmfo/bars/Sequence/s_secaudit.sql
+++ ABS/master/sql/mmfo/bars/Sequence/s_secaudit.sql
@@ -1,15 +1,13 @@
-
-
 PROMPT ===================================================================================== 
 PROMPT *** Run *** ========== Scripts /Sql/BARS/Sequence/S_SECAUDIT.sql =========*** Run ***
 PROMPT ===================================================================================== 
 
-
 PROMPT *** Create  sequence S_SECAUDIT ***
 
-   CREATE SEQUENCE  BARS.S_SECAUDIT  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 4475268694 CACHE 100 NOORDER  NOCYCLE ;
+CREATE SEQUENCE S_SECAUDIT MINVALUE 1 INCREMENT BY 1 START WITH 1 CACHE 100 ORDER NOCYCLE;
 
 
+alter sequence S_SECAUDIT ORDER;
 
 PROMPT ===================================================================================== 
 PROMPT *** End *** ========== Scripts /Sql/BARS/Sequence/S_SECAUDIT.sql =========*** End ***
