4324d7015b821094e30dfef3247c7253813557e6
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-16 14:42:45 +0000

    #COBUSUPMMFO-661# Коментар:

---
+++ sql/mmfo/bars/Data/report/BRS_SBM_REP_684.sql
@@ -0,0 +1,153 @@
+
+
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/Bars/Data/_BRS_SBM_REP_684.sql =========*** Run *
+PROMPT ===================================================================================== 
+prompt ===================================== 
+prompt == Реєстр платіжних документів 
+prompt ===================================== 
+
+set serveroutput on
+set feed off       
+declare                               
+
+   nlchr       char(2):=chr(13)||chr(10);
+   l_zpr       zapros%rowtype;    
+   l_zprr      zapros%rowtype;    
+   l_rep       reports%rowtype;   
+   l_repfolder number;            
+   l_isnew     smallint:=0;       
+   l_isnewr    smallint:=0;       
+   l_message   varchar2(1000);    
+
+begin     
+   l_zpr.name := 'Реєстр платіжних документів ';
+   l_zpr.pkey := '\BRS\SBM\REP\684';
+
+   l_message  := 'Ключ запроса: '||l_zpr.pkey||'  '||nlchr;
+
+   begin                                                   
+      select kodz, kodr into l_zpr.kodz, l_zpr.kodr        
+      from zapros where pkey=l_zpr.pkey;                   
+   exception when no_data_found then                       
+      l_isnew:=1;                                          
+      select s_zapros.nextval into l_zpr.kodz from dual;   
+      if (0>0) then                  
+         select s_zapros.nextval into l_zpr.kodr from dual;
+         l_zprr.kodz:=l_zpr.kodr;           
+      end if;                               
+   end;                                     
+                                            
+
+    ------------------------    
+    --  main query        --    
+    ------------------------    
+                                
+    l_zpr.id           := 1;
+    l_zpr.name         := 'Реєстр платіжних документів ';
+    l_zpr.namef        := '';
+    l_zpr.bindvars     := ':sFdat1=''На дату'',:Param4=''Виконавець'',:Param5=''Код операції'',:NLSMASK=''Рахунок-А''';
+    l_zpr.create_stmt  := '';
+    l_zpr.rpt_template := 'prov5.qrp';
+    l_zpr.form_proc    := '';
+    l_zpr.default_vars := ':Param4=''%'',:Param5=''%'',:NLSMASK=''%''';
+    l_zpr.bind_sql     := '';
+    l_zpr.xml_encoding := 'CL8MSWIN1251';
+    l_zpr.txt          := 'SELECT :sFdat1 DAT1,  p.userid ISP,    
+       p.ref REF, p.kv KV, p.id_a OKPOA, p.nlsa NLSA, 
+       p.MFOB MFOB, p.id_b OKPOB, p.nlsb NLSB, p.s/100 S, 
+       p.sq/100  SQ,
+       p.nazn NAZN, p.vdat PDAT,
+       p.tt TT,        
+      f.fio FIO
+FROM   oper p , staff f   
+WHERE  p.userid=decode(:Param4,''%'',p.userid,:Param4)   
+       AND  p.nlsa like :NLSMASK 
+       AND p.tt like :Param5
+       AND  p.vdat between to_date(:sFdat1||'' 00:00:00'',''dd.mm.yyyy hh24:mi:ss'') and to_date(:sFdat1||'' 23:59:59'',''dd.mm.yyyy hh24:mi:ss'')
+       AND  p.sos=5 
+       AND p.userid=f.id
+ORDER BY p.vdat,  p.userid, p.s';
+    l_zpr.xsl_data     := '';
+    l_zpr.xsd_data     := '';
+
+    if l_isnew = 1 then           
+       insert into zapros values l_zpr;  
+       l_message:=l_message||'Добавлен новый кат.запрос №'||l_zpr.kodz||'.'; 
+    else                           
+       update zapros set name         = l_zpr.name,        
+                         namef        = l_zpr.namef,       
+                         bindvars     = l_zpr.bindvars,    
+                         create_stmt  = l_zpr.create_stmt, 
+                         rpt_template = l_zpr.rpt_template,
+                         form_proc    = l_zpr.form_proc,   
+                         default_vars = l_zpr.default_vars,
+                         bind_sql     = l_zpr.bind_sql,    
+                         xml_encoding = l_zpr.xml_encoding,
+                         txt          = l_zpr.txt,         
+                         xsl_data     = l_zpr.xsl_data,    
+                         xsd_data     = l_zpr.xsd_data     
+       where pkey=l_zpr.pkey;                              
+       l_message:=l_message||'Кат.запрос c таким ключем уже существует под №'||l_zpr.kodz||', его параметры изменены.'; 
+                                                           
+    end if;                                                
+
+    ------------------------    
+    --  report            --    
+    ------------------------    
+                                
+
+    l_rep.name        :='Empty';
+    l_rep.description :='Реєстр платіжних документів ';
+    l_rep.form        :='frm_UniReport';
+    l_rep.param       :=l_zpr.kodz||',3,sFdat,sFdat,"",TRUE,FALSE';
+    l_rep.ndat        :=1;
+    l_rep.mask        :='';
+    l_rep.usearc      :=0;
+    begin                                                                        
+        select idf into l_repfolder from reportsf where idf = 50; 
+    exception when no_data_found then                                            
+        l_repfolder := null;                                                     
+    end;                         
+    l_rep.idf := l_repfolder;    
+
+    -- Фиксированный № печатного отчета   
+    l_rep.id          := 684;
+
+
+    if l_isnew = 1 then                     
+       begin                                
+          insert into reports values l_rep;        
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then  
+           bars_error.raise_error('REP',14, to_char(l_rep.id));
+       end;                                    
+    else                                            
+       begin                                        
+          insert into reports values l_rep;         
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then         
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' изменен.';
+          update reports set                
+             name        = l_rep.name,       
+             description = l_rep.description,
+             form        = l_rep.form,       
+             param       = l_rep.param,      
+             ndat        = l_rep.ndat,       
+             mask        = l_rep.mask,       
+             usearc      = l_rep.usearc,     
+             idf         = l_rep.idf         
+          where id=l_rep.id;                 
+       end;                                  
+    end if;                                  
+    bars_report.print_message(l_message);   
+end;                                        
+/                                           
+                                            
+commit;                                     
+
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/Bars/Data/_BRS_SBM_REP_684.sql =========*** End *
+PROMPT ===================================================================================== 
---
+++ sql/mmfo/bars/Data/report/_BRS_SBER_ZVT_7.sql
@@ -0,0 +1,149 @@
+
+
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/Bars/Data/_BRS_SBER_ZVT_7.sql =========*** Run **
+PROMPT ===================================================================================== 
+prompt ===================================== 
+prompt == 7.МФО: Зведення документiв дня - РЕЄСТР проводок  (Папки,Бранчi+)
+prompt ===================================== 
+
+set serveroutput on
+set feed off       
+declare                               
+
+   nlchr       char(2):=chr(13)||chr(10);
+   l_zpr       zapros%rowtype;    
+   l_zprr      zapros%rowtype;    
+   l_rep       reports%rowtype;   
+   l_repfolder number;            
+   l_isnew     smallint:=0;       
+   l_isnewr    smallint:=0;       
+   l_message   varchar2(1000);    
+
+begin     
+   l_zpr.name := '7.МФО: Зведення документiв дня - РЕЄСТР проводок  (Папки,Бранчi+)';
+   l_zpr.pkey := '\BRS\SBER\ZVT\7';
+
+   l_message  := 'Ключ запроса: '||l_zpr.pkey||'  '||nlchr;
+
+   begin                                                   
+      select kodz, kodr into l_zpr.kodz, l_zpr.kodr        
+      from zapros where pkey=l_zpr.pkey;                   
+   exception when no_data_found then                       
+      l_isnew:=1;                                          
+      select s_zapros.nextval into l_zpr.kodz from dual;   
+      if (0>0) then                  
+         select s_zapros.nextval into l_zpr.kodr from dual;
+         l_zprr.kodz:=l_zpr.kodr;           
+      end if;                               
+   end;                                     
+                                            
+
+    ------------------------    
+    --  main query        --    
+    ------------------------    
+                                
+    l_zpr.id           := 1;
+    l_zpr.name         := '7.МФО: Зведення документiв дня - РЕЄСТР проводок  (Папки,Бранчi+)';
+    l_zpr.namef        := '';
+    l_zpr.bindvars     := ':sFdat1=''За дату'',:TEMA=''Код папки (0-всі)'',:BRANCH=''Відділення (%-всі)'',:PRN=''Перегляд-0,Папки бух_облік-1,Папка(26,27)-2,Папка(38,39)-3,Папки бек-офіс-4'',:Param3=''Виконавець (0-всі)'' ';
+    l_zpr.create_stmt  := '';
+    l_zpr.rpt_template := 'ZVT7n.qrp';
+    l_zpr.form_proc    := '';
+    l_zpr.default_vars := ':TEMA=''0'',:BRANCH=''%'',:PRN=''0'',:Param3=''0''';
+    l_zpr.bind_sql     := ':TEMA=''TEST_ZVT|TEMA|NAME|ORDER BY TEMA'',:BRANCH=''OUR_BRANCH|BRANCH|NAME|WHERE length(branch)<16 and length(branch)>7 ORDER BY 1''';
+    l_zpr.xml_encoding := 'CL8MSWIN1251';
+    l_zpr.txt          := 'select t.TEMA, b.BRANCH, d.isp USERID, d.KV, d.TT, d.REF, d.NLSD, d.NLSK,
+     d.S S, d.SQ, b.NAME,t.name NAMET
+from  branch b, TEST_ZVT t, zvt_doc d
+where d.fdat = to_date( :sFdat1,''dd-mm-yyyy'')
+and ( nvl(:PRN,''0'') = ''0'' or t.PRN =:PRN )
+and b.branch = substr( d.branch,1,15)
+and t.tema =abs(d.tema)
+and t.tema = decode(:TEMA,''0'', t.tema, to_number(:TEMA))
+and b.branch like :BRANCH || decode ( length(:BRANCH), 8, '''',''%'' )
+and d.ISP =  decode(:Param3,''0'', d.ISP, to_number(:Param3))
+order by 1, 2, 3,  sign(tema),   4, 5, 6';
+    l_zpr.xsl_data     := '';
+    l_zpr.xsd_data     := '';
+
+    if l_isnew = 1 then           
+       insert into zapros values l_zpr;  
+       l_message:=l_message||'Добавлен новый кат.запрос №'||l_zpr.kodz||'.'; 
+    else                           
+       update zapros set name         = l_zpr.name,        
+                         namef        = l_zpr.namef,       
+                         bindvars     = l_zpr.bindvars,    
+                         create_stmt  = l_zpr.create_stmt, 
+                         rpt_template = l_zpr.rpt_template,
+                         form_proc    = l_zpr.form_proc,   
+                         default_vars = l_zpr.default_vars,
+                         bind_sql     = l_zpr.bind_sql,    
+                         xml_encoding = l_zpr.xml_encoding,
+                         txt          = l_zpr.txt,         
+                         xsl_data     = l_zpr.xsl_data,    
+                         xsd_data     = l_zpr.xsd_data     
+       where pkey=l_zpr.pkey;                              
+       l_message:=l_message||'Кат.запрос c таким ключем уже существует под №'||l_zpr.kodz||', его параметры изменены.'; 
+                                                           
+    end if;                                                
+
+    ------------------------    
+    --  report            --    
+    ------------------------    
+                                
+
+    l_rep.name        :='Empty';
+    l_rep.description :='7.МФО: Зведення документiв дня - РЕЄСТР проводок  (Папки,Бранчi+)';
+    l_rep.form        :='frm_UniReport';
+    l_rep.param       :=l_zpr.kodz||',3,sFdat,sFdat,"",TRUE,FALSE';
+    l_rep.ndat        :=1;
+    l_rep.mask        :='';
+    l_rep.usearc      :=0;
+    begin                                                                        
+        select idf into l_repfolder from reportsf where idf = 80; 
+    exception when no_data_found then                                            
+        l_repfolder := null;                                                     
+    end;                         
+    l_rep.idf := l_repfolder;    
+
+    -- Фиксированный № печатного отчета   
+    l_rep.id          := 7;
+
+
+    if l_isnew = 1 then                     
+       begin                                
+          insert into reports values l_rep;        
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then  
+           bars_error.raise_error('REP',14, to_char(l_rep.id));
+       end;                                    
+    else                                            
+       begin                                        
+          insert into reports values l_rep;         
+          l_message:=l_message||nlchr||'Добавлен новый печ. отчет под №'||l_rep.id;
+       exception when dup_val_on_index then         
+          l_message:=l_message||nlchr||'Печатный отчет под №'||l_rep.id||' изменен.';
+          update reports set                
+             name        = l_rep.name,       
+             description = l_rep.description,
+             form        = l_rep.form,       
+             param       = l_rep.param,      
+             ndat        = l_rep.ndat,       
+             mask        = l_rep.mask,       
+             usearc      = l_rep.usearc,     
+             idf         = l_rep.idf         
+          where id=l_rep.id;                 
+       end;                                  
+    end if;                                  
+    bars_report.print_message(l_message);   
+end;                                        
+/                                           
+                                            
+commit;                                     
+
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/Bars/Data/_BRS_SBER_ZVT_7.sql =========*** End **
+PROMPT ===================================================================================== 
