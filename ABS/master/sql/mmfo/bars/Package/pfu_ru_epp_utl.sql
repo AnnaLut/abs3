5dce7dfe397e25d093457473b24d31e46719c245
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-16 15:58:00 +0000

    #COBUSUPABS-7093# Коментар: Merge.

--- ABS/master/sql/mmfo/bars/Package/pfu_ru_epp_utl.sql
+++ ABS/master/sql/mmfo/bars/Package/pfu_ru_epp_utl.sql
@@ -1713,8 +1713,7 @@ CREATE OR REPLACE PACKAGE BODY BARS.PFU_RU_EPP_UTL is
     for i in (select *
                 from pfu_ca_files t
                where t.state = 0
-               order by t.kf
-                 for update skip locked) loop
+               order by t.kf) loop
       begin
       if (l_kf is not null and l_kf != i.kf) or (l_kf is null) then
         l_kf := i.kf;
