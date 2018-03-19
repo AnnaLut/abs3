9083fd63a62ad8ee11ed9944af1bc4a0abf62b6f
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-19 14:24:47 +0000

    #COBUMMFO-81# Коментар:

---
+++ ABS/master/web/ru/barsroot/App_Code/Areas/CorpLight/Models/Acsk/AcskDke.cs
@@ -0,0 +1,14 @@
+﻿namespace BarsWeb.Areas.CorpLight.Models
+{
+    public class AcskDke
+    {
+        public AcskDke()
+        {
+        }
+
+        public int? Id { get; set; }
+        public string Item { get; set; }
+        public string Value { get; set; }
+        public string Tag { get; set; }
+    }
+}
