6ab0e1c5de78ad4dfeb8e51e5af7eced0c05eeff
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-20 16:13:48 +0000

    #COBUMMFO-7033# Коментар: Merge.

---
+++ ABS/master/sql/ru/bars/Script/patch_person.sql
@@ -0,0 +1,31 @@
+-- ======================================================================================
+-- Module : ALL
+-- Author : BAA
+-- Date   : 
+-- ======================================================================================
+-- modify table PERSON
+-- ======================================================
+
+SET SERVEROUTPUT ON SIZE UNLIMITED FORMAT WRAPPED
+SET FEEDBACK     OFF
+SET TIMING       OFF
+SET DEFINE       OFF
+SET LINES        200
+SET PAGES        200
+SET TERMOUT      ON
+SET TRIMSPOOL    ON
+
+prompt -- ======================================================
+prompt --  modify table PERSON
+prompt -- ======================================================
+
+begin
+  
+  execute immediate 'alter table PERSON modify ORGAN varchar2(150)';
+  
+  dbms_output.put_line( 'Table PERSON altered.' );
+  
+end;
+/
+
+SET FEEDBACK ON
