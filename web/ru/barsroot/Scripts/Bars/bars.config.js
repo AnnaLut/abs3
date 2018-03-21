fc191744d622b483bdd415861ec506e5e8066744
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-21 14:25:44 +0000

    #COBUBB-1586# Коментар: MERGE: COBUBB-1586 Станіслав Зуйко

--- web/ru/barsroot/Scripts/Bars/bars.config.js
+++ web/ru/barsroot/Scripts/Bars/bars.config.js
@@ -2,6 +2,24 @@
 bars.config = bars.config || {};
 
 bars.config.appName = 'barsroot';
-bars.config.urlContent = function (url) {
-    return '/' + this.appName + url;
+bars.config.baseUrl = '/' + bars.config.appName;
+bars.config.urlContent = function (url, params) {
+    var result = this.baseUrl;
+    if (url) {
+        if (result.lastIndexOf('/') !== (result.length - 1) && url.slice(0, 1) !== '/') {
+            result += '/';
+        } else if ((result.lastIndexOf('/') === (result.length - 1) && url.slice(0, 1) === '/')) {
+            result = result.slice(0, result.length - 1);
+        }
+        result += url;
+    }
+    if (params) {
+        if (result.indexOf('?') === -1) {
+            result += '?';
+        }
+        if (window['$']) {
+            result += $.param(params);
+        }
+    }
+    return result;
 };
