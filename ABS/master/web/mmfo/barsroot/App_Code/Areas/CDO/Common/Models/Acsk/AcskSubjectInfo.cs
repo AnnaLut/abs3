0383bd465cd8e44485850e248b73fd5071774460
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-21 14:25:44 +0000

    #COBUBB-1586# Коментар: MERGE: COBUBB-1586 Станіслав Зуйко

---
+++ ABS/master/web/mmfo/barsroot/App_Code/Areas/CDO/Common/Models/Acsk/AcskSubjectInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace BarsWeb.Areas.CDO.Common.Models.Acsk
+{
+    public class AcskSubjectInfo
+    {
+        public AcskSubjectInfo()
+        {
+        }
+
+        public string TemplateId { get; set; }
+        public string RegistrationId { get; set; }
+        public string Subject { get; set; }
+    }
+}
---
+++ ABS/master/web/mmfo/barsroot/App_Code/Areas/CDO/Corp2/Models/FunctionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BarsWeb.Areas.CDO.Corp2.Models
+{
+    /// <summary>
+    /// Summary description for FunctionViewModel
+    /// </summary>
+    public class FunctionViewModel
+    {
+        public decimal Id { get; set; }
+        public string Name { get; set; }
+        public string ModuleId { get; set; }
+    }
+}
