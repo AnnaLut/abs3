5cfda7b84d748d086880c19b05fff491e567d149
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-21 11:56:59 +0000

    #COBUSUPABS-6872# Коментар: Merge.

--- ABS/master/sql/ru/bars/View/v_reserved_acc.sql
+++ ABS/master/sql/ru/bars/View/v_reserved_acc.sql
@@ -1,27 +1,45 @@
+create or replace force view V_RESERVED_ACC
+( NLS
+, KV
+, NMS
+, BRANCH
+, ISP
+, RNK
+, OKPO
+, NMK
+, AGRM_NUM
+, RSRV_ID
+) as
+select a.NLS, a.KV, a.NMS, a.BRANCH, a.ISP, a.RNK
+     , c.OKPO, c.NMK
+     , a.AGRM_NUM, a.RSRV_ID
+  from ACCOUNTS_RSRV a
+  join CUSTOMER c
+    on ( c.RNK = a.RNK )
+ where c.DATE_OFF is null;
 
+show errors;
 
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/View/V_RESERVED_ACC.sql =========*** Run ***
-PROMPT ===================================================================================== 
+prompt -- ======================================================
+prompt -- Comments
+prompt -- ======================================================
 
+comment on table  V_RESERVED_ACC          is 'Зарезервовані номера рахунків';
 
-PROMPT *** Create  view V_RESERVED_ACC ***
+comment on column V_RESERVED_ACC.NLS      is 'Номер рахунку';
+comment on column V_RESERVED_ACC.KV       is 'Валюта рахунку';
+comment on column V_RESERVED_ACC.NMS      is 'Назва рахунку';
+comment on column V_RESERVED_ACC.BRANCH   is 'Код ТВБВ';
+comment on column V_RESERVED_ACC.ISP      is 'Ід. відповіднального виконавця';
+comment on column V_RESERVED_ACC.RNK      is 'Реєстраційний Номер Клієнта (РНК)';
+comment on column V_RESERVED_ACC.AGRM_NUM is 'Номер договору банківського обслуговування (SPECPARAM.NKD)';
+comment on column V_RESERVED_ACC.OKPO     is 'ЗКПО';
+comment on column V_RESERVED_ACC.NMK      is 'Назва клієнта';
+comment on column V_RESERVED_ACC.RSRV_ID  is 'Ід. запису про резерв номера рахунку';
 
-  CREATE OR REPLACE FORCE VIEW BARS.V_RESERVED_ACC ("ACC", "NLS", "KV", "NMS", "FLAG_OPEN", "RNK", "OKPO", "NMK") AS 
-  select a.acc, a.nls, a.kv, a.nms, null flag_open, c.rnk, c.okpo, c.nmk
-  from accounts a, accountsw w, customer c
- where a.acc = w.acc
-   and w.tag = 'RESERVED' and w.value = '1'
-   and a.rnk = c.rnk
-   and c.date_off is null;
+prompt -- ======================================================
+prompt -- Grants
+prompt -- ======================================================
 
-PROMPT *** Create  grants  V_RESERVED_ACC ***
-grant SELECT,UPDATE                                                          on V_RESERVED_ACC  to BARS_ACCESS_DEFROLE;
-grant DELETE,INSERT,SELECT,UPDATE                                            on V_RESERVED_ACC  to CUST001;
-grant FLASHBACK,SELECT                                                       on V_RESERVED_ACC  to WR_REFREAD;
-
-
-
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/View/V_RESERVED_ACC.sql =========*** End ***
-PROMPT ===================================================================================== 
+grant SELECT on V_RESERVED_ACC to BARS_ACCESS_DEFROLE;
+grant SELECT on V_RESERVED_ACC to CUST001;
