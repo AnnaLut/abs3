ec46dcaeb0dfd8faff1d6102c3735c12916d2376
oleksandr.kuziakov <oleksandr.kuziakov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-22 10:42:38 +0000

    Коментар: #COBUSUPABS-6322# Коментар: MERGE: COBUSUPABS-6322 Ростислав Кузьмович

---
+++ ABS/master/web/ru/barsroot/App_Code/webservices/DataExchange/Models/ResultResponseFromRu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bars.WebServices.DataExchangeService
+{
+    public class ResultResponseFromRu
+    {
+        public Decimal? id { get; set; }
+        public String clob { get; set; }
+        public String message { get; set; }
+    }
+}
