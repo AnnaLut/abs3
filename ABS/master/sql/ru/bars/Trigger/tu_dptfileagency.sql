21c3a7940139730e780014000ba5845217d9d96d
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-23 17:48:51 +0000

    #COBUSUPABS-6180# Коментар: Merge.

--- ABS/master/sql/ru/bars/Trigger/tu_dptfileagency.sql
+++ ABS/master/sql/ru/bars/Trigger/tu_dptfileagency.sql
@@ -1,42 +1,29 @@
-
-
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/Trigger/TU_DPTFILEAGENCY.sql =========*** Ru
-PROMPT ===================================================================================== 
-
-
 PROMPT *** Create  trigger TU_DPTFILEAGENCY ***
 
-  CREATE OR REPLACE TRIGGER BARS.TU_DPTFILEAGENCY 
-before update of agency_id ON BARS.DPT_FILE_AGENCY for each row
+CREATE OR REPLACE TRIGGER BARS.TU_DPTFILEAGENCY 
+before update of agency_id ON BARS.DPT_FILE_AGENCY
+for each row
 declare
-	l_cnt number(38);
+  l_cnt number(38);
 begin
-  -- Репликация: Если изменения пришли из удаленной БД, то ничего не делаем
-  if (dbms_mview.i_am_a_refresh = true or dbms_reputil.from_remote = true) then
-      return;
-  end if;
 
   -- Якщо є оплачені документи - орган соц. захисту змінювати не можна
   select count(*)
-  into l_cnt
-  from dpt_file_row
-  where header_id = :old.header_id and branch = :old.branch and ref is not null;
+    into l_cnt
+    from dpt_file_row
+   where header_id = :old.header_id and branch = :old.branch and ref is not null;
 
-  if l_cnt > 0 then
-	bars_error.raise_nerror('SOC', 'BF_IS_PAID',to_char(:old.header_id),to_char(:old.branch));
+  if ( l_cnt > 0 )
+  then
+    bars_error.raise_nerror('SOC', 'BF_IS_PAID',to_char(:old.header_id),to_char(:old.branch));
   end if;
 
-  update dpt_file_row
-  set agency_id = :new.agency_id,
-   agency_name = (select name from social_agency where agency_id = :new.agency_id )
-  where header_id = :old.header_id and branch = :old.branch;
+  update DPT_FILE_ROW
+     set AGENCY_ID = :new.agency_id
+       , AGENCY_NAME = (select name from social_agency where agency_id = :new.agency_id )
+   where HEADER_ID = :old.header_id;
 
-end; 
+end TU_DPTFILEAGENCY;
 /
-ALTER TRIGGER BARS.TU_DPTFILEAGENCY ENABLE;
 
-
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/Trigger/TU_DPTFILEAGENCY.sql =========*** En
-PROMPT ===================================================================================== 
+show errors;
