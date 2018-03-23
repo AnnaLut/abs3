a9dd98b99025c52aa80a0112d4a2d41ee9193bfb
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-23 18:17:52 +0000

    #COBUMMFO-6618# Коментар: Merge.

--- ABS/master/sql/mmfo/bars/Procedure/p_bpk_rep.sql
+++ ABS/master/sql/mmfo/bars/Procedure/p_bpk_rep.sql
@@ -8,6 +8,7 @@ PROMPT =========================================================================
 PROMPT *** Create  procedure P_BPK_REP ***
 
   CREATE OR REPLACE PROCEDURE BARS.P_BPK_REP (
+-- 23.03.2018 LIA
   p_mode number,
   p_dat1 date,
   p_dat2 date,
@@ -25,9 +26,9 @@ begin
      for z in ( select b.acc, c.nmk, c.rnk, to_date(w.value, 'dd/mm/yyyy') dat, a.nls, o.kf
                   from w4_acc o, accounts b, customer c, accountsw w, accounts a
                  where o.acc_ovr = b.acc
-                   and b.nbs = '2203' 
+                   and b.nbs = '2203'
                    and b.dazs is null
-                   and b.acc = w.acc 
+                   and b.acc = w.acc
                    and w.tag = 'PK_DDAT'
                    and b.rnk = c.rnk
                    and (p_kf = '%' or o.kf = p_kf)
@@ -35,8 +36,8 @@ begin
      loop
         l_ost := abs(fost(z.acc, p_dat1));
         acrn.p_int(z.acc, 0, z.dat, p_dat1, l_int);
-        insert into tmp_bpk_rep (nmk, rnk, open_date, nls, kf, sum_borg, sum_int)
-        values (z.nmk,z.rnk,z.dat,z.nls,z.kf,abs(l_ost),abs(l_int));
+        insert into tmp_bpk_rep (acc, nmk, rnk, open_date, nls, kf, sum_borg, sum_int)
+        values (z.acc,z.nmk,z.rnk,z.dat,z.nls,z.kf,abs(l_ost/100),abs(round(l_int/100)));
      end loop;
   end if;
 end p_bpk_rep;
