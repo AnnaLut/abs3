05e7dd63459eed41f0e619dc631ab5a5ec1a688f
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-03-27 14:24:44 +0000

    #COBUMMFO-5136# Коментар: Merge.

--- sql/ru/bars/Procedure/p_nbur_get_sk_data.sql
+++ sql/ru/bars/Procedure/p_nbur_get_sk_data.sql
@@ -1,16 +1,16 @@
-create or replace procedure p_nbur_get_sk_data(p_ReportDateFrom in date, p_ReportDateTo in date, p_kf in varchar2)
+CREATE OR REPLACE procedure BARS.p_nbur_get_sk_data(p_ReportDateFrom in date, p_ReportDateTo in date, p_kf in varchar2)
 is
   title            VARCHAR2(100) := 'P_NBUR_GET_SK_DATA.';
-  
+
   l_request        soap_rpc.t_request;
   l_response       soap_rpc.t_response;
   l_url            varchar2(4000);
   l_xml_resp       xmltype;
-  l_errtxt         varchar2(4000);
+  l_errtxt         clob;
   l_params         varchar2(4000);
-  
+
   l_clob           clob;
-  
+
   function get_param_webconfig(par varchar2) return web_barsconfig.val%type is
       l_res web_barsconfig.val%type;
   begin
@@ -22,15 +22,17 @@ is
   end;
 begin
   begin
-      select substr(val, 1, instr(val, '/', 1, 4)-1) || '/webservices/QuickMoneyService.asmx' into l_url from params where par = 'REPORT_SERVER_URL';
+      select substr(val, 1, instr(val, '/', 1, 4)-1) || '/webservices/QuickMoneyService.asmx' into l_url from web_barsconfig  where key = 'EWA.URL_SEND_REF_STATUS';
   exception when no_data_found then
       raise_application_error(-20000, 'Параметр REPORT_SERVER_URL не задано');
   end;
   
+--  l_url:= replace(l_url, 'https', 'http');
+
 --  l_url:= 'http://10.10.10.35:1212/barsroot/webservices/QuickMoneyService.asmx'; --jeka потом убрать
 
-  logger.info(title||'profix_service: begin call service ' || l_url || ' with method [ TransactionShortReport ]');
-  
+  logger.info(title||'profix_service: begin call service ' || l_url || ' with method [ Te9xreport ]');
+
   --подготовить реквест
   l_request := soap_rpc.new_request(p_url         => l_url,
                                     p_namespace   => 'http://ws.unity-bars.com.ua/',
@@ -41,7 +43,7 @@ begin
   l_params:='<' || 'DATEFROM' || '>'||to_char(p_ReportDateFrom, 'DD.MM.YYYY')||'</' || 'DATEFROM' || '>';
 
   l_params:=l_params||'<' || 'REPORTDATE' || '>'||to_char(p_ReportDateTo, 'DD.MM.YYYY')||'</' || 'REPORTDATE' || '>';
-  
+
   soap_rpc.add_parameter(l_request, 'Parameters', l_params);
   soap_rpc.add_parameter(l_request, 'ServiceMethod', '1');
 
@@ -60,15 +62,15 @@ begin
   exception
     when others then null;
   end;
-  
+
   if instr(l_errtxt,'Exception') >0 then
      raise_application_error(-20100, l_errtxt);
   end if;
-  
+
   l_clob := dbms_xmlgen.convert(l_clob, 1);
-  
+
   delete from NBUR_TMP_E9_CLOB where report_date = p_ReportDateTo and kf = p_kf;
-  
+
   insert into NBUR_TMP_E9_CLOB(report_date, kf, xml_file)
   values (p_ReportDateTo, p_kf, l_clob);
 
