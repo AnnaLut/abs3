57bef2fd8c272507c518e6e9e042f33f25401407
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-04-05 07:30:08 +0000

    #COBUMMFO-7348# Коментар:

--- sql/ru/bars/Procedure/p_bv_balans.sql
+++ sql/ru/bars/Procedure/p_bv_balans.sql
@@ -1,3 +1,5 @@
+
+
 PROMPT ===================================================================================== 
 PROMPT *** Run *** ========== Scripts /Sql/BARS/Procedure/P_BV_BALANS.sql =========*** Run *
 PROMPT ===================================================================================== 
@@ -7,11 +9,12 @@ PROMPT *** Create  procedure P_BV_BALANS ***
 
   CREATE OR REPLACE PROCEDURE BARS.P_BV_BALANS (p_dat01 date) IS
 
-/* Версия 2.0  31-07-2017
+/* Версия 2.1  14-09-2017  31-07-2017
    BV по 351 постанове = БАЛАНСУ
    -------------------------------------
+ 2) 14-09-2017  - Округление CR
  1) 31-07-2017  - Выравнивание по хоз дебиторке в первую очередь fin=1
-                                 
+
 */
 
  l_bv    rez_cr.bv%type; l_bvq  rez_cr.bvq%type;
@@ -24,16 +27,18 @@ begin
                    from rez_cr where fdat = p_dat01 group by acc) a
              where bv<>ostc)
    LOOP
-      for r in (select rowid rw, bv, kv, ead, eadq, cr, crq, nvl(pd,0) pd, decode(tipa,17,1,21,1,nvl(lgd,0)) lgd, tipa from rez_cr 
+      for r in (select rowid rw, bv, kv, ead, eadq, cr, crq, nvl(pd,0) pd, decode(tipa,17,1,21,1,nvl(lgd,0)) lgd, tipa, ccf from rez_cr
                 where  fdat = p_dat01  and acc = k.acc order by fin,bv)
       LOOP
          l_bv  := greatest(r.bv + k.del,0);
          l_bvq := p_icurval(r.kv,l_bv*100,l_dat31)/100;
          k.del := k.del + (r.bv - l_bv);
-         if r.ead > l_bv or r.tipa in (17,21) THEN 
-            r.ead  := l_bv;                  r.eadq := l_bvq; 
-            r.cr   := r.ead * r.pd *r.LGD;   r.crq  := p_icurval(r.kv,r.cr*100,l_dat31)/100;
+         if r.ccf is null THEN
+            r.ead  := l_bv ;  r.eadq := l_bvq;
+         else
+            r.ead  := round(l_bv * nvl(r.ccf,100)/100,2);  r.eadq := p_icurval(r.kv,r.ead*100,l_dat31)/100;
          end if;
+         r.cr   := round(r.ead * r.pd *r.LGD,2); r.crq  := p_icurval(r.kv,r.cr*100,l_dat31)/100;
          update rez_cr set bv = l_bv, bvq = l_bvq, ead = r.ead, eadq = r.eadq, cr = r.cr, crq = r.crq where rowid = r.rw;
          if k.del = 0 THEN   exit; end if;
       end loop;
