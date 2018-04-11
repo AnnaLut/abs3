24f29c077e58b812e1cfa407109f5f8c176924fa
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-04-11 14:37:25 +0000

    #COBUMMFO-6808# Коментар: Merge.

---
+++ ABS/master/sql/mmfo/bars/Data/fill_table/META_CALL_SETTINGS.sql
@@ -0,0 +1,14 @@
+begin
+    execute immediate 'Insert into BARS.META_CALL_SETTINGS
+   (ID, CODE, CALL_FROM, WEB_FORM_NAME, INSERT_AFTER, 
+    EDIT_MODE, SUMM_VISIBLE, ADD_WITH_WINDOW, SWITCH_OF_DEPS, SHOW_COUNT)
+ Values
+   (6, ''CALL_FOREX'', ''FREE'', ''V_FX_SWAP[NSIFUNCTION][PROC=>forex.proc_swap(0, :nDealTag,:BASEY_A,:RATE_A,:BASEY_B,:RATE_B)][PAR=>:BASEY_A(SEM=Прц_база по вал-A,TYPE=N,REF=BASEY),:RATE_A(SEM=Прц_ставка по вал-A,TYPE=N),:BASEY_B(SEM=Прц_база по вал-B,TYPE=N,REF=BASEY),:RATE_B(SEM=Прц_ставка по вал-B,TYPE=N)][EXEC=>BEFORE][ACCESSCODE=>1]'', 0, 
+    ''ROW_EDIT'', 0, 0, 0, 0)';
+ exception when others then 
+    if sqlcode = -1 then null; else raise; 
+    end if; 
+end;
+/ 
+
+COMMIT;
