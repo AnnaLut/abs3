167df9740aafdeaa0a876d1bd69990a2196cb5d7
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-04-12 14:15:44 +0000

    #COBUMMFO-7464# Коментар: Merge.

--- ABS/master/sql/mmfo/bars/Data/tts/et_G02.sql
+++ ABS/master/sql/mmfo/bars/Data/tts/et_G02.sql
@@ -12,11 +12,11 @@ begin
   --------------------------------
   begin
     insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
-    values ('G02', 'G02 √≈–÷ «Ó‚Ì≥¯Ì≥ ', 1, null, 980, '#(get_proc_nls(''T00'',#(KVA)))', 980, null, null, null, null, 0, 0, 0, 0, null, null, null, null, null, null, '0001100000000000000000000000000000010100000000000000000000000000', null);
+    values ('G02', 'G02 √≈–÷ «Ó‚Ì≥¯Ì≥ ', 1, null, 980, '#(get_proc_nls(''T00'',#(KVA)))', 980, null, null, null, null, 0, 1, 0, 0, null, null, null, null, null, null, '0201100000000000000000000000000000010100000000000000000000000000', null);
   exception
     when dup_val_on_index then 
       update tts
-         set tt='G02', name='G02 √≈–÷ «Ó‚Ì≥¯Ì≥ ', dk=1, nlsm=null, kv=980, nlsk='#(get_proc_nls(''T00'',#(KVA)))', kvk=980, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0001100000000000000000000000000000010100000000000000000000000000', nazn=null
+         set tt='G02', name='G02 √≈–÷ «Ó‚Ì≥¯Ì≥ ', dk=1, nlsm=null, kv=980, nlsk='#(get_proc_nls(''T00'',#(KVA)))', kvk=980, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=1, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0201100000000000000000000000000000010100000000000000000000000000', nazn=null
        where tt='G02';
   end;
   --------------------------------
