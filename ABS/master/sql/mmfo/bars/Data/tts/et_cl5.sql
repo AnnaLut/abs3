0a2c76c93c94b0f06d38b99938315fdfad9d662a
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-04-18 14:54:22 +0000

    #COBUBB-1525# Коментар:

--- ABS/master/sql/mmfo/bars/Data/tts/et_cl5.sql
+++ ABS/master/sql/mmfo/bars/Data/tts/et_cl5.sql
@@ -3,7 +3,7 @@ set trimspool on
 set serveroutput on size 1000000
 
 prompt Создание / Обновление операции CL5
-prompt Наименование операции: CL5 Internet-Banking CorpLight: БПК
+prompt Наименование операции: Internet-Banking CorpLight: БПК
 declare
   cnt_  number;
 begin
@@ -12,11 +12,11 @@ begin
   --------------------------------
   begin
     insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
-    values ('CL5', 'CL5 Internet-Banking CorpLight: БПК', 1, null, null, '#(bpk_get_transit(''19'',#(NLSA),#(NLSB),#(KVA)))', null, null, null, null, null, 0, 0, 0, 0, null, null, null, null, null, null, '1101000000000000000000000000000000010000000000000000000000000000', null);
+    values ('CL5', 'Internet-Banking CorpLight: БПК', 1, null, null, '#(bpk_get_transit(''19'',#(NLSA),#(NLSB),#(KVA)))', null, null, null, null, null, 0, 0, 0, 0, null, null, null, null, null, null, '0101000000000000000000000000000000010000000000000000000000000000', null);
   exception
     when dup_val_on_index then 
       update tts
-         set tt='CL5', name='CL5 Internet-Banking CorpLight: БПК', dk=1, nlsm=null, kv=null, nlsk='#(bpk_get_transit(''19'',#(NLSA),#(NLSB),#(KVA)))', kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=null, flags='1101000000000000000000000000000000010000000000000000000000000000', nazn=null
+         set tt='CL5', name='Internet-Banking CorpLight: БПК', dk=1, nlsm=null, kv=null, nlsk='#(bpk_get_transit(''19'',#(NLSA),#(NLSB),#(KVA)))', kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0101000000000000000000000000000000010000000000000000000000000000', nazn=null
        where tt='CL5';
   end;
   --------------------------------
@@ -152,6 +152,72 @@ begin
   ------- Балансовые счета -------
   --------------------------------
   delete from ps_tts where tt='CL5';
+  begin
+    insert into ps_tts(nbs, tt, dk)
+    values ('2600', 'CL5', 1);
+  exception
+    when dup_val_on_index then null;
+    when others then
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ps_tts: ''2600'', ''CL5'', 1) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ps_tts(nbs, tt, dk)
+    values ('2605', 'CL5', 1);
+  exception
+    when dup_val_on_index then null;
+    when others then
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ps_tts: ''2605'', ''CL5'', 1) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ps_tts(nbs, tt, dk)
+    values ('2620', 'CL5', 1);
+  exception
+    when dup_val_on_index then null;
+    when others then
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ps_tts: ''2620'', ''CL5'', 1) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ps_tts(nbs, tt, dk)
+    values ('2625', 'CL5', 1);
+  exception
+    when dup_val_on_index then null;
+    when others then
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ps_tts: ''2625'', ''CL5'', 1) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ps_tts(nbs, tt, dk)
+    values ('2650', 'CL5', 1);
+  exception
+    when dup_val_on_index then null;
+    when others then
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ps_tts: ''2650'', ''CL5'', 1) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ps_tts(nbs, tt, dk)
+    values ('2655', 'CL5', 1);
+  exception
+    when dup_val_on_index then null;
+    when others then
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ps_tts: ''2655'', ''CL5'', 1) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
   --------------------------------
   -------- Виды документов -------
   --------------------------------
@@ -233,3 +299,4 @@ begin
 end;
 /
 commit;
+
