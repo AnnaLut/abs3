1b34c88bbf8e1601fb680eeabd2213d59416ec9a
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-03 15:35:31 +0000

    #COBUMMFO-6291# Коментар: Merge.

--- sql/mmfo/bars/Table/mway_match.sql
+++ sql/mmfo/bars/Table/mway_match.sql
@@ -22,33 +22,27 @@ END;
 
 PROMPT *** Create  table MWAY_MATCH ***
 begin 
-  execute immediate '
-  CREATE TABLE BARS.MWAY_MATCH 
-   (	ID NUMBER, 
-	DATE_TR DATE, 
-	SUM_TR NUMBER(*,0), 
-	LCV_TR VARCHAR2(3), 
-	NLS_TR VARCHAR2(15), 
-	RRN_TR VARCHAR2(100), 
-	DRN_TR VARCHAR2(100), 
-	STATE NUMBER, 
-	REF_TR NUMBER(38,0), 
-	REF_FEE_TR NUMBER(38,0)
-   ) SEGMENT CREATION IMMEDIATE 
-  PCTFREE 10 PCTUSED 0 INITRANS 1 MAXTRANS 255 
- NOCOMPRESS LOGGING
-  TABLESPACE BRSDYNI ';
+  execute immediate 'create table MWAY_MATCH 
+( ID         NUMBER(38)
+, DATE_TR    DATE
+, SUM_TR     NUMBER(24)
+, LCV_TR     VARCHAR2(3)
+, NLS_TR     VARCHAR2(15)
+, RRN_TR     VARCHAR2(100)
+, DRN_TR     VARCHAR2(100)
+, STATE      NUMBER
+, REF_TR     NUMBER(38)
+, REF_FEE_TR NUMBER(38)
+, constraint PK_MWAYMATCH PRIMARY KEY (ID) using index tablespace BRSMDLI
+, constraint UK_MWAYMATCH UNIQUE (REF_TR)  using index tablespace BRSMDLI
+) tablespace BRSMDLD';
 exception when others then       
   if sqlcode=-955 then null; else raise; end if; 
 end; 
 /
 
-
-
-
 PROMPT *** ALTER_POLICIES to MWAY_MATCH ***
- exec bpa.alter_policies('MWAY_MATCH');
-
+exec bpa.alter_policies('MWAY_MATCH');
 
 COMMENT ON TABLE BARS.MWAY_MATCH IS 'Таблиця операцій з кліент-банка WAY4';
 COMMENT ON COLUMN BARS.MWAY_MATCH.SUM_TR IS 'Сума транзакції';
@@ -62,41 +56,9 @@ COMMENT ON COLUMN BARS.MWAY_MATCH.REF_FEE_TR IS '';
 COMMENT ON COLUMN BARS.MWAY_MATCH.ID IS 'Ід. номер';
 COMMENT ON COLUMN BARS.MWAY_MATCH.DATE_TR IS 'Дата транзакції';
 
-
-
-
-PROMPT *** Create  constraint SYS_C0035373 ***
-begin   
- execute immediate '
-  ALTER TABLE BARS.MWAY_MATCH ADD PRIMARY KEY (ID)
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSDYND  ENABLE NOVALIDATE';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
- end;
-/
-
-
-
-
-PROMPT *** Create  constraint UK_MWAYMATCH ***
-begin   
- execute immediate '
-  ALTER TABLE BARS.MWAY_MATCH ADD CONSTRAINT UK_MWAYMATCH UNIQUE (REF_TR)
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSSMLI  ENABLE NOVALIDATE';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
- end;
-/
-
-
-
-
 PROMPT *** Create  index UK_MWAYMATCH ***
 begin   
- execute immediate '
-  CREATE UNIQUE INDEX BARS.UK_MWAYMATCH ON BARS.MWAY_MATCH (REF_TR) 
+ execute immediate 'CREATE UNIQUE INDEX BARS.UK_MWAYMATCH ON BARS.MWAY_MATCH (REF_TR) 
   PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
   TABLESPACE BRSSMLI ';
 exception when others then
@@ -104,26 +66,28 @@ exception when others then
  end;
 /
 
+prompt -- ======================================================
+prompt -- create index UK_MWAYMATCH_RRNTR
+prompt -- ======================================================
 
-
-
-PROMPT *** Create  index SYS_C0035373 ***
-begin   
- execute immediate '
-  CREATE UNIQUE INDEX BARS.SYS_C0035373 ON BARS.MWAY_MATCH (ID) 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSDYND ';
-exception when others then
-  if  sqlcode=-955  then null; else raise; end if;
- end;
+declare
+  e_idx_exists           exception;
+  pragma exception_init( e_idx_exists,      -00955 );
+  e_col_already_idx      exception;
+  pragma exception_init( e_col_already_idx, -01408 );
+begin
+  execute immediate 'create index UK_MWAYMATCH_RRNTR on MWAY_MATCH ( RRN_TR )';
+  dbms_output.put_line( 'Index created.' );
+exception
+  when e_idx_exists 
+  then dbms_output.put_line( 'Name is already used by an existing object.' );
+  when e_col_already_idx 
+  then dbms_output.put_line( 'Such column list already indexed.' );
+end;
 /
 
-
-
 PROMPT *** Create  grants  MWAY_MATCH ***
-grant SELECT                                                                 on MWAY_MATCH      to BARSREADER_ROLE;
 grant SELECT                                                                 on MWAY_MATCH      to BARS_ACCESS_DEFROLE;
-grant SELECT                                                                 on MWAY_MATCH      to UPLD;
 
 
 
