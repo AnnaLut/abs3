23b0d439a69785b325d3d7ff38e4279b3f92ef84
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-03 11:45:24 +0000

    #COBUMMFO-7182# Коментар: Merge.

--- ABS/master/sql/mmfo/bars/View/v_cp_accounts.sql
+++ ABS/master/sql/mmfo/bars/View/v_cp_accounts.sql
@@ -1,70 +1,68 @@
-
-
 PROMPT ===================================================================================== 
 PROMPT *** Run *** ========== Scripts /Sql/BARS/View/V_CP_ACCOUNTS.sql =========*** Run *** 
 PROMPT ===================================================================================== 
 
-
 PROMPT *** Create  view V_CP_ACCOUNTS ***
 
-  CREATE OR REPLACE FORCE VIEW BARS.V_CP_ACCOUNTS ("ACC", "NLS", "NLSALT", "KV", "LCV", "DIG", "DENOM", "KF", "BRANCH", "TOBO", "NBS", "NBS2", "DAOS", "DAPP", "ISP", "RNK", "NMS", "LIM", "OST", "OSTB", "OSTC", "OSTF", "OSTQ", "OSTX", "DOS", "KOS", "DOSQ", "KOSQ", "PAP", "TIP", "VID", "TRCN", "MDATE", "DAZS", "SEC", "ACCC", "BLKD", "BLKK", "POS", "SECI", "SECO", "GRP", "OB22", "NOTIFIER_REF", "BDATE", "OPT", "DAPPQ", "INTACCN", "FIO") AS 
-  SELECT "ACC",
-          "NLS",
-          "NLSALT",
-          "KV",
-          "LCV",
-          "DIG",
-          "DENOM",
-          "KF",
-          "BRANCH",
-          "TOBO",
-          '1111',
-          "NBS2",
-          "DAOS",
-          "DAPP",
-          "ISP",
-          "RNK",
-          "NMS",
-          "LIM",
-          "OST",
-          "OSTB",
-          "OSTC",
-          "OSTF",
-          "OSTQ",
-          "OSTX",
-          "DOS",
-          "KOS",
-          "DOSQ",
-          "KOSQ",
-          "PAP",
-          "TIP",
-          "VID",
-          "TRCN",
-          "MDATE",
-          "DAZS",
-          "SEC",
-          "ACCC",
-          "BLKD",
-          "BLKK",
-          "POS",
-          "SECI",
-          "SECO",
-          "GRP",
-          "OB22",
-          "NOTIFIER_REF",
-          "BDATE",
-          "OPT",
-          "DAPPQ",
-          "INTACCN",
-          "FIO"
-     FROM V_CP_ACCOUNTS_LITE;
+create or replace force view V_CP_ACCOUNTS
+( ACC, NLS, NLSALT, KV, LCV, DIG, DENOM, KF, BRANCH, TOBO, NBS, NBS2, DAOS, DAPP, ISP, RNK, NMS, LIM, OST, OSTB, OSTC, OSTF, OSTQ, OSTX, DOS, KOS, DOSQ, KOSQ, PAP, TIP, VID, TRCN, MDATE, DAZS, SEC, ACCC, BLKD, BLKK, POS, SECI, SECO, GRP, OB22, NOTIFIER_REF, BDATE, OPT, DAPPQ, INTACCN, FIO
+) AS 
+SELECT ACC,
+       NLS,
+       NLSALT,
+       KV,
+       LCV,
+       DIG,
+       DENOM,
+       KF,
+       BRANCH,
+       TOBO,
+       NBS,
+       NBS2,
+       DAOS,
+       DAPP,
+       ISP,
+       RNK,
+       NMS,
+       LIM,
+       OST,
+       OSTB,
+       OSTC,
+       OSTF,
+       OSTQ,
+       OSTX,
+       DOS,
+       KOS,
+       DOSQ,
+       KOSQ,
+       PAP,
+       TIP,
+       VID,
+       TRCN,
+       MDATE,
+       DAZS,
+       SEC,
+       ACCC,
+       BLKD,
+       BLKK,
+       POS,
+       SECI,
+       SECO,
+       GRP,
+       OB22,
+       NOTIFIER_REF,
+       BDATE,
+       OPT,
+       DAPPQ,
+       INTACCN,
+       FIO
+  FROM V_CP_ACCOUNTS_LITE;
 
 PROMPT *** Create  grants  V_CP_ACCOUNTS ***
-grant SELECT                                                                 on V_CP_ACCOUNTS   to BARSREADER_ROLE;
-grant SELECT                                                                 on V_CP_ACCOUNTS   to BARS_ACCESS_DEFROLE;
-grant SELECT                                                                 on V_CP_ACCOUNTS   to UPLD;
 
-
+grant SELECT on V_CP_ACCOUNTS to BARSREADER_ROLE;
+grant SELECT on V_CP_ACCOUNTS to BARS_ACCESS_DEFROLE;
+grant SELECT on V_CP_ACCOUNTS to UPLD;
 
 PROMPT ===================================================================================== 
 PROMPT *** End *** ========== Scripts /Sql/BARS/View/V_CP_ACCOUNTS.sql =========*** End *** 
