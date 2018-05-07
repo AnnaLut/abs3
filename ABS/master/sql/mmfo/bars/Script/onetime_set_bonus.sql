0cd68dc4a0cb9c5843bf5b1c73c74d4fe0daa23d
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-07 07:36:14 +0000

    #COBUMMFO-6766# Коментар: Merge.

--- ABS/master/sql/mmfo/bars/Script/onetime_set_bonus.sql
+++ ABS/master/sql/mmfo/bars/Script/onetime_set_bonus.sql
@@ -6,12 +6,13 @@ declare
   l_bonus_cnt number;
   l_bonusdate date;
   l_zp_count  number;
-  p_dat       DATE := trunc(sysdate);
+  p_dat       DATE := trunc(sysdate+1);
   l_brate     bars.int_ratn.br%type;
   l_irate     bars.int_ratn.ir%type;
   l_indrate   bars.Dpt_Vidd_Extdesc%rowtype;
   l_brtype    varchar2(25);
   l_err varchar2(200);
+  l_iserror boolean;
 BEGIN
   
   
@@ -25,10 +26,10 @@ BEGIN
    For i in (select * from tmp_int_ddpt tid
         where tid.kf = cur.kf
         and nvl(trim(tid.note),'-') <> 'OK') loop
-
        BEGIN
         l_bonusval := 0;
         l_err := '';
+        l_iserror := false;
         
         bars_audit.trace('%s запуск процедуры расчета льгот', l_title);
         l_err := l_err ||'1-';
@@ -37,7 +38,8 @@ BEGIN
         bars_audit.trace('%s льготы рассчитаны', l_title);
         bars_audit.trace('%s поиск бонусной процентной ставки, не требующей подтверждения', l_title);
         
-          delete from dpt_depositw
+        --удаляем из доп. параметров и устанавливаем        
+        delete from dpt_depositw
            where tag = 'BONUS'
              and DPT_ID = i.deposit_id;
           l_err := l_err ||'3-';
@@ -47,9 +49,9 @@ BEGIN
                            l_title,
                            to_char(l_bonusval));
 
-        -- если прогрессивный
-        if i.type_code = 'MPRG' then       --#3
+        if i.type_code = 'MPRG' then       --#3 -- если прогрессивный
         l_err := l_err ||'5-';
+         IF i.cnt_dubl > 0 then --#3.1  -- если кол-во пролонгаций больше 0
           begin
             select *
               into l_indrate --индивидуальная ставка для этого кол-ва пролонгаций
@@ -57,36 +59,34 @@ BEGIN
              where dve.base_rate = i.br
                and dve.ext_num = i.cnt_dubl
                and dve.type_id = i.ext_id;
-
           exception
             when others then
               l_indrate := null;
           end;
 
-         if nvl(l_indrate.indv_rate,0) <> 0 --#4
+         if nvl(l_indrate.indv_rate,0) <> 0 --#4  -- если удалось определить значение ставки для этого кол-ва пролонгаций
          then 
-          l_bonusval := l_bonusval + l_indrate.indv_rate;
+
+          l_bonusval := l_bonusval + l_indrate.indv_rate; -- добавляем бонус за пролонгацию к уже имеющимся бонусам
           l_err := l_err ||'6-';
-          merge into bars.int_ratn ir
-          using (select *
-                   from bars.int_ratn
-                  where acc = i.acc
-                    and bdat = p_dat + 1
-                    and br = l_brate
-                    and id = 1) x
-          on (ir.acc = x.acc and ir.bdat = x.bdat and ir.br = x.br and ir.id = x.id)
-          When matched then
-            update
-               set ir.ir = l_bonusval,
-                   op    = case
-                             when nvl(l_indrate.indv_rate, 0) != 0 and nvl(l_indrate.oper_id, 0) = 0 then
-                              1
-                             else
-                              l_indrate.oper_id
-                           end 
-           When not matched then 
-             insert(ir.acc, ir.id, ir.bdat, ir.ir, ir.br, ir.op) values(i.acc, 1, trunc(p_dat + 1), l_bonusval, l_indrate.base_rate, l_indrate.oper_id);
+          
+         --обновляем бонусную ставку с учетом бонуса за кол-во пролонгаций 
+         begin
+          insert into bars.Int_Ratn (acc, id, bdat, ir, br, op)
+          values(i.acc, 1, trunc(p_dat + 1), l_bonusval, l_indrate.base_rate, l_indrate.oper_id);
+         exception when dup_val_on_index then
+          update bars.int_ratn
+          set ir = l_bonusval,
+              op = case when nvl(l_indrate.indv_rate, 0) != 0 and nvl(l_indrate.oper_id, 0) = 0
+                   then 1  else l_indrate.oper_id end
+          where acc = i.acc and bdat = p_dat + 1 and id = 1 and br = l_indrate.base_rate;
+         when others then
+            l_err := 'Ошибка установки бонусной ставки acc = '||to_char(i.acc)||' ir = '||to_char(l_bonusval)||' dat = '||to_char(p_dat+1 ,'DD/MM/YYYY');
+            l_iserror := true;
+          end;
+          
            l_err := l_err ||'7-';
+          -- обновляем параметры депозита 
            begin
               INSERT INTO dpt_depositw (dpt_id, tag, value, branch)
               VALUES (i.deposit_id, 'BONUS', to_char(l_bonusval), i.branch);
@@ -98,20 +98,36 @@ BEGIN
             end;
             l_err := l_err ||'8-';
             bars_audit.trace('%s значение бонуса записано в доп.реквизиты вклада', l_title);                
-            
+          else --#4 -- кол-во пролонгаций больше 0, но не удалось определить бонус за пролонгацию
+            l_err := 'Установлен некорректный бонус для прогрессивного депозита - не удалось определить бонус для acc = '||to_char(i.acc)||' для кол-ва пролонгаций '||to_char(i.cnt_dubl);
+            l_iserror := true;  
           end if; --#4
-        end if; --#3
-        
-        if i.type_code = 'MPRG' then -- но, если депозит прогрессивный, то берем дату открытия или каждой 12-й пролонгации
+          
+         end if; --#3.1 
+
+          -- а еще, если депозит прогрессивный, то берем дату открытия или каждой 12-й пролонгации
               select nvl(max(dat_begin), i.dat_begin)
               into l_bonusdate
               from bars.dpt_deposit_clos
               where deposit_id = i.deposit_id
               and nvl(cnt_dubl,0) = trunc(nvl(i.cnt_dubl,0)/12) * 12;
-        else
-              l_bonusdate := i.dat_begin;
-        end if;
+        else --#3
+           l_bonusdate := i.dat_begin; 
+        end if; --#3
+        
         l_zp_count := dpt_bonus.get_MMFO_ZPcard_count(i.rnk, l_bonusdate);
+        
+        if l_iserror then
+        
+        update BARS.tmp_int_ddpt 
+        set note = 'err: '||l_err,
+        bonus = l_bonusval,
+        open_zp_cnt = l_zp_count
+        where deposit_id = i.deposit_id
+        and acc = i.acc
+        and kf = i.kf;
+        
+        else  
         
         update BARS.tmp_int_ddpt 
         set note = 'OK',
@@ -120,6 +136,8 @@ BEGIN
         where deposit_id = i.deposit_id
         and acc = i.acc
         and kf = i.kf;
+        
+        end if;
          
         EXCEPTION 
           WHEN OTHERS 
--- ABS/master/sql/mmfo/bars/Table/tmp_int_ddpt.sql
+++ ABS/master/sql/mmfo/bars/Table/tmp_int_ddpt.sql
@@ -12,7 +12,7 @@ BEGIN
 
  begin
    execute immediate 'CREATE TABLE tmp_int_ddpt AS
-(select d.deposit_id, d.dat_begin, d.vidd, d.acc, d.kv, d.rnk, d.kf, d.branch, d.cnt_dubl,
+(select d.deposit_id, d.dat_begin, d.vidd, d.acc, d.kv, d.rnk, d.kf, d.branch, nvl(d.cnt_dubl,0) cnt_dubl,
 -999 open_zp_cnt,
 d.wb,
 ir.br,
@@ -22,16 +22,17 @@ dv.extension_id ext_id,
 -999 bonus,
 ''                                                                                                                                                                                                                 '' note
 from bars.dpt_deposit d
-inner join bars.accounts ac on ac.acc = d.acc and ac.kf = d.kf
+inner join bars.accounts ac on ac.acc = d.acc and ac.kf = d.kf and ac.nbs = ''2630''
 inner join bars.int_ratn ir on ir.acc = d.acc and ir.br is not null
-inner join bars.brates br on br.br_id = ir.br 
+inner join bars.brates br on br.br_id = ir.br and br.br_type = 1
 inner join bars.dpt_vidd dv on dv.vidd = d.vidd
 inner join bars.dpt_types dt on dt.type_id = dv.type_id and dt.type_code <> ''AKC''
-where d.dat_begin >= to_date(''28.02.2017'',''DD.MM.YYYY'')
+where 1=1
+and d.deposit_id in (4062700507, 4844210507, 4930800507, 4962850507, 4972190507, 4989570507, 4989610507, 4989630507, 5032740507, 18019278913, 19146088111, 19151143711, 19156502613, 
+631465501, 3615000507, 3624400507, 3692650507, 3725370507, 3741780507, 4110670507, 4112150507, 4121090507, 4121640507, 4502140103, 4578010103, 4645130103, 4781420103, 19159728911, 19159816311, 19160388211)
+and d.dat_begin >= to_date(''28.02.2017'',''DD.MM.YYYY'')
 and (d.dat_end is null or d.dat_end > trunc(sysdate))
-and ir.bdat = (select max(bdat) from bars.int_ratn where acc = ir.acc)
-and br.br_type = 1
-and ac.nbs = ''2630'')';
+and ir.bdat = (select max(bdat) from bars.int_ratn where acc = ir.acc))';
 
 exception
   when OTHERS then 
