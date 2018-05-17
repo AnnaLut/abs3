fa62407e0f5ae4321ba9d90e3fd54919604f4ea6
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-17 13:59:33 +0000

    #COBUMMFO-7453# Коментар: При дозаливке документов в ММФО за предідущие года- возникала ошибка unique constr

--- sql/mmfo/bars/Trigger/taiu_oper_sos_tracker.sql
+++ sql/mmfo/bars/Trigger/taiu_oper_sos_tracker.sql
@@ -10,7 +10,7 @@ PROMPT *** Create  trigger TAIU_OPER_SOS_TRACKER ***
   CREATE OR REPLACE TRIGGER BARS.TAIU_OPER_SOS_TRACKER 
 after insert or update of sos ON BARS.OPER for each row
     WHEN (
-new.tt like 'IB%'
+new.tt like 'IB%' and new.pdat >= trunc(sysdate, 'yyyy')
       ) begin
     -- зберігаємо історію змін статусів документів
     insert into sos_track(ref, sos_tracker, old_sos, new_sos, change_time, userid)
