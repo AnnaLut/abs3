a63048858a3d5e03331d3923a46f590116fef86e
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-22 09:27:07 +0000

    #COBUMMFO-7721# Коментар: Merge.

--- sql/mmfo/bars/Data/fill_table/cash_nbs.sql
+++ sql/mmfo/bars/Data/fill_table/cash_nbs.sql
@@ -1,6 +1,15 @@
+begin
+  delete CASH_NBS where nbs = 3400;
+end;
+/
 
 begin
-  begin insert into CASH_NBS values(3400, null); exception when dup_val_on_index then null; end;
+  begin insert into CASH_NBS values(3400, '08'); exception when dup_val_on_index then null; end;
+end;
+/
+
+begin
+  begin insert into CASH_NBS values(3400, '19'); exception when dup_val_on_index then null; end;
 end;
 /
 commit;
