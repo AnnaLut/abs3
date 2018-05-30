f84c26d026e1344531e47486a45343422a5fe1b7
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-30 13:31:35 +0000

    #COBUMMFO-7985# Коментар:

---
+++ ABS/master/sql/mmfo/bars/Script/add_tips_nli_nlj.sql
@@ -0,0 +1,59 @@
+begin
+  Insert into BARS.TIPS
+      (TIP, NAME)
+   Values
+      ('NLI', 'NLI розрахунки SWIFT по ЮЛ');
+exception when dup_val_on_index then  null;
+end;
+/
+COMMIT;
+
+
+begin
+  Insert into BARS.TIPS
+      (TIP, NAME)
+   Values
+      ('NLJ', 'NLJ розрахунки SWIFT по ФЛ');
+exception when dup_val_on_index then  null;
+end;
+/
+COMMIT;
+
+begin
+  Insert into BARS.NBS_TIPS
+       (NBS, TIP)
+       Values
+      ('3720','NLI');
+exception when dup_val_on_index then  null;
+end;
+/
+COMMIT;
+
+begin
+  Insert into BARS.NBS_TIPS
+       (NBS, TIP)
+       Values
+      ('3720','NLJ');
+exception when dup_val_on_index then  null;
+end;
+/
+COMMIT;
+
+begin
+Insert into BARS.NLK_TT
+   (ID, TT)
+ Values
+   ('99', '024');
+exception when dup_val_on_index then  null;
+end;
+/
+
+begin
+Insert into BARS.NLK_TT
+   (ID, TT)
+ Values
+   ('99', 'PKR');
+exception when dup_val_on_index then  null;
+end;
+/
+COMMIT;
