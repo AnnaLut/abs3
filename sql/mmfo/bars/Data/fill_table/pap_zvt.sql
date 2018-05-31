8180234de77e85ede825cabb95f466c6406dedc3
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-05-31 12:36:22 +0000

    #COBUMMFO-7546# Коментар: Merge.

--- sql/mmfo/bars/Data/fill_table/pap_zvt.sql
+++ sql/mmfo/bars/Data/fill_table/pap_zvt.sql
@@ -1,3 +1,11 @@
 update  PAP_ZVT t set t.prn = 4 where t.tema in (13,15,19,20,29,30,35,36,45,2, 6, 7, 8);
 commit;
 /
+
+update PAP_ZVT t set t.name = 'Доходи за операціями з клієнтами' where t.tema = 26;
+commit;
+/
+
+update PAP_ZVT t set t.name = 'Витрати за операціями з клієнтами' where t.tema = 27;
+commit;
+/
