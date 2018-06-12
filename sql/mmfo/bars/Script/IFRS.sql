65f40ab6cbb086828d1f3dbc46ee1f47fedd8b0e
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-12 13:09:56 +0000

    #COBUPRVNIX-137# Коментар:

--- sql/mmfo/bars/Script/IFRS.sql
+++ sql/mmfo/bars/Script/IFRS.sql
@@ -1,18 +1,41 @@
 ------------------------------------------------------------------      
-begin
-for m in (select kf from mv_kf)
-  loop
-    bc.go (m.kf);    
-    insert into nd_txt
-     (nd, tag, txt)
-  select nd,'IFRS','AC' from nd_txt where ((tag='BUS_MOD' and txt in(1,6,7,8,9,10,11,12,14) )or (tag='SPPI' and txt='Так')) 
+prompt --IFRS ND_TXT
+DECLARE
+  l_stmt varchar2(4000);
+  l_chunk varchar2(4000);
+  p_task varchar2 (100);
+BEGIN
+  p_task:='ifrs_task_ndtxt'||to_char(sysdate,'ddmmhh24miss');
+  l_chunk := 'select kf as START_ID, kf as END_ID from bars.mv_kf';
+  l_stmt := '
+         begin
+           bars_login.login_user(p_sessionid => substr(sys_guid(), 1, 32),
+                                            p_userid    => 1,
+                                            p_hostname  =>null ,
+                                            p_appname   =>null);
+         bc.go(:START_ID);
+         bars.logger.info(''ifrs_task_ndtxt-''||:START_ID||''/''||:END_ID||'' ''||f_ourmfo||''date:''||to_char(sysdate,''dd.mm.hh24:mi:ss''));
+       insert /*+ignore_row_on_dupkey_index(nd_txt PK_NDTXT) */ into nd_txt
+       (nd, tag, txt)
+         select nd,''IFRS'',''AC'' from nd_txt where ((tag=''BUS_MOD'' and txt in(1,6,7,8,9,10,11,12,14) )or (tag=''SPPI'' and txt=''Так'')) 
        group by nd
-       having count(*)>1
-       and nd not in( select nd from nd_txt where tag='IFRS');    
-   end loop;
-  COMMIT;
-bc.home;
-end; 
+       having count(*)>1; 
+       commit;
+       end;';
+
+ dbms_parallel_execute.create_task(p_task); 
+ DBMS_PARALLEL_EXECUTE.create_chunks_by_sql(task_name => p_task,
+                                             sql_stmt  => l_chunk,
+                                             by_rowid  => FALSE);
+
+ DBMS_PARALLEL_EXECUTE.run_task(task_name      => p_task,
+                                 sql_stmt       => l_stmt,
+                                 language_flag  => DBMS_SQL.NATIVE,
+                                 parallel_level => 10);
+                                 
+   dbms_parallel_execute.drop_task(p_task);                              
+
+END;
 / 
 ----------------------------------------------------------         
   begin 
