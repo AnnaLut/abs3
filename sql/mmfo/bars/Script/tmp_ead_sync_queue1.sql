a089719154e7d59cf3a42daac7170a1fa7356212
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-13 13:58:51 +0000

    #COBUMMFO-8196# Коментар:

--- sql/mmfo/bars/Script/tmp_ead_sync_queue1.sql
+++ sql/mmfo/bars/Script/tmp_ead_sync_queue1.sql
@@ -1,28 +1,29 @@
 
 
 PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/Table/EAD_SYNC_QUEUE.sql =========*** Run **
+PROMPT *** Run *** ========== Scripts /Sql/BARS/Table/TMP_EAD_SYNC_QUEUE.sql =========*** Run **
 PROMPT ===================================================================================== 
 
 
-PROMPT *** ALTER_POLICY_INFO to EAD_SYNC_QUEUE ***
+PROMPT *** ALTER_POLICY_INFO to TMP_EAD_SYNC_QUEUE ***
 
 
 BEGIN 
         execute immediate  
           'begin  
-               bpa.alter_policy_info(''EAD_SYNC_QUEUE'', ''CENTER'' , null, null, null, null);
-               bpa.alter_policy_info(''EAD_SYNC_QUEUE'', ''FILIAL'' , null, null, null, null);
-               bpa.alter_policy_info(''EAD_SYNC_QUEUE'', ''WHOLE'' , null, null, null, null);
+               bpa.alter_policy_info(''TMP_EAD_SYNC_QUEUE'', ''CENTER'' , null, null, null, null);
+               bpa.alter_policy_info(''TMP_EAD_SYNC_QUEUE'', ''FILIAL'' , null, null, null, null);
+               bpa.alter_policy_info(''TMP_EAD_SYNC_QUEUE'', ''WHOLE'' , null, null, null, null);
                null;
            end; 
           '; 
 END; 
 /
 
-PROMPT *** Create  table EAD_SYNC_QUEUE ***
+PROMPT *** Create  table TMP_EAD_SYNC_QUEUE ***
+
 begin 
-  execute immediate 'create table EAD_SYNC_QUEUE
+  execute immediate 'create table TMP_EAD_SYNC_QUEUE
 (
   id            NUMBER,
   crt_date      DATE,
@@ -260,140 +261,16 @@ partition EAD_ST_NEW_P                  values (''OUTDATED'') tablespace BRSBIGD
     subpartition SP_EAD_356334_NEW values (''356334'') tablespace BRSBIGD,
     subpartition SP_EAD_353553_NEW values (''353553'') tablespace BRSBIGD)
 )';
+    
 exception when others then       
   if sqlcode=-955 then null; else raise; end if; 
 end; 
 /
 
-
--- Add/modify columns 
-begin
-  for i in (select 1 from dual where not exists (select 1 from user_tab_cols where TABLE_NAME = 'EAD_SYNC_QUEUE' and COLUMN_NAME = 'RNK')) loop
-    execute immediate 'alter table bars.ead_sync_queue add rnk number(38)';
-  end loop;
-end;
-/
-
-
-PROMPT *** ALTER_POLICIES to EAD_SYNC_QUEUE ***
- exec bpa.alter_policies('EAD_SYNC_QUEUE');
-
-
-COMMENT ON TABLE  BARS.EAD_SYNC_QUEUE IS 'Черга повідомлень для синхронізаціх з ЕА';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.ID IS 'Ідентифікатор';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.CRT_DATE IS 'Дата створення';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.TYPE_ID IS 'Тип повідомлення';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.OBJ_ID IS 'Ід. обєкту (для док. - ід документу, для клієнта - РНК і тп)';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.RNK IS 'РНК клієнта по котрому формується повідомлення';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.STATUS_ID IS 'Ід. статусу';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.ERR_TEXT IS 'Текст помилки';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.ERR_COUNT IS 'Кіл-ть спроб з помилкою';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.MESSAGE_ID IS 'Ід. повідомлення';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.MESSAGE_DATE IS 'Дата відправки повідомлення';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.MESSAGE IS 'Текст повідомлення';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.RESPONCE_ID IS 'Ід. відповіді';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.RESPONCE_DATE IS 'Дата відповіді';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.RESPONCE IS 'Текст відповіді';
-COMMENT ON COLUMN BARS.EAD_SYNC_QUEUE.KF IS 'МФО';
-
-
-
-PROMPT *** Create  index PK_EADSYNCQ1 ***
-begin   
- execute immediate '
-  CREATE UNIQUE INDEX BARS.PK_EADSYNCQ1 ON BARS.EAD_SYNC_QUEUE (ID) 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSBIGI ';
-exception when others then
-  if  sqlcode=-955  then null; else raise; end if;
-end;
-/
-
-PROMPT *** Create  index PK_EADSYNCQ1 ***
-begin   
- execute immediate '  CREATE INDEX ind_EADSYNCQ_DATE_RNK1 ON EAD_SYNC_QUEUE (CRT_DATE,RNK) LOCAL';
-exception when others then
-  if  sqlcode=-955  then null; else raise; end if;
-end;
-/
-
-
-PROMPT *** Create  constraint PK_EADSYNCQ1 ***
-begin   
- execute immediate '
-  ALTER TABLE BARS.EAD_SYNC_QUEUE ADD CONSTRAINT PK_EADSYNCQ1 PRIMARY KEY (ID)
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSBIGI  ENABLE';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
-end;
-/
-
-
-
-PROMPT *** Create  constraint FK_EADSYNCQ_STSID_STATUSES1 ***
-begin   
- execute immediate '
-  ALTER TABLE BARS.EAD_SYNC_QUEUE ADD CONSTRAINT FK_EADSYNCQ_STSID_STATUSES1 FOREIGN KEY (STATUS_ID)
-    REFERENCES BARS.EAD_STATUSES (ID) ENABLE';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
-end;
-/
-
-
-
-PROMPT *** Drop  constraint FK_EADSYNCQ_TID_TYPES1 ***
-begin   
-  execute immediate 'ALTER TABLE BARS.EAD_SYNC_QUEUE DROP CONSTRAINT FK_EADSYNCQ_TID_TYPES1';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-2443 then null; else raise; end if;
-end;
-/
-
-PROMPT *** Create  constraint FK_EADSYNCQ_TID_TYPES1 ***
-begin   
- execute immediate '
-  ALTER TABLE BARS.EAD_SYNC_QUEUE ADD CONSTRAINT FK_EADSYNCQ_TID_TYPES1 FOREIGN KEY (TYPE_ID)
-    REFERENCES BARS.EAD_TYPES (ID) DEFERRABLE INITIALLY DEFERRED ENABLE';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
-end;
-/
-
-
-/*PROMPT *** Create  index I_EADSYNCQUEUE_TYPE_OBJ1 ***
-begin
-  for i in (select 1 from dual where not exists (select 1 from user_indexes where index_name = 'I_EADSYNCQUEUE_TYPE_OBJ1')) loop
-    execute immediate '
-      CREATE INDEX BARS.I_EADSYNCQUEUE_TYPE_OBJ1 ON BARS.EAD_SYNC_QUEUE (TYPE_ID, OBJ_ID) 
-      TABLESPACE BRSBIGI  LOCAL (PARTITION P_FIRST TABLESPACE BRSBIGI ) ';
-  end loop;
-end;
-/
-*/
-
-PROMPT *** Create  constraint FK_EADSYNCQUEUE_KF1 ***
-begin   
- execute immediate '
-  ALTER TABLE BARS.EAD_SYNC_QUEUE ADD CONSTRAINT FK_EADSYNCQUEUE_KF1 FOREIGN KEY (KF)
-    REFERENCES BARS.BANKS$BASE (MFO) ENABLE';
-exception when others then
-  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
-end;
-/
-
---ENABLE ROW MOVEMENT -- для возможности перемещения между партициями
-PROMPT *** ALTER TABLE ENABLE_ROW MOVEMENT ***
-ALTER TABLE EAD_SYNC_QUEUE ENABLE ROW MOVEMENT;
-/
-
-PROMPT *** Create  grants  EAD_SYNC_QUEUE ***
-grant SELECT on EAD_SYNC_QUEUE  to BARS_ACCESS_DEFROLE;
-grant SELECT on EAD_SYNC_QUEUE  to BARS_DM;
-
-
+PROMPT *** Create  grants  TMP_EAD_SYNC_QUEUE ***
+grant SELECT on TMP_EAD_SYNC_QUEUE  to BARS_ACCESS_DEFROLE;
+grant SELECT on TMP_EAD_SYNC_QUEUE  to BARS_DM;
 
 PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/Table/EAD_SYNC_QUEUE.sql =========*** End **
+PROMPT *** End *** ========== Scripts /Sql/BARS/Table/TMP_EAD_SYNC_QUEUE.sql =========*** End **
 PROMPT ===================================================================================== 
