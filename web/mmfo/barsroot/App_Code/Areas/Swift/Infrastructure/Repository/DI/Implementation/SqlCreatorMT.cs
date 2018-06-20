5899c399776e0d252e46a62fd8451dc441103794
stanislav.zuiko <stanislav.zuiko@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-20 15:23:09 +0000

    #COBUMMFO-8019# Коментар: MERGE Анна Крахмальова

---
+++ web/mmfo/barsroot/App_Code/Areas/Swift/Infrastructure/Repository/DI/Implementation/SqlCreatorMT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BarsWeb.Areas.Kernel.Models;
+using Oracle.DataAccess.Client;
+
+namespace BarsWeb.Areas.Swift.Infrastructure.DI.Implementation
+{
+    public class SqlCreatorMT
+    {       
+        public static BarsSql GetSWREFValue(string uetr)
+        {
+            return new BarsSql()
+            {
+                SqlText = @"Select swref from sw_journal where uetr=:p_uetr and MT='103'",
+                SqlParams = new object[]
+                {
+                    new OracleParameter("p_uetr", OracleDbType.Varchar2, System.Data.ParameterDirection.Input) { Value = uetr}
+                }
+            };
+        }
+
+        public static BarsSql GenerateReject(string uetr)
+        {
+            return new BarsSql()
+            {
+                SqlText = @"begin bars_swift_msg.generate_reject (:p_uetr); end;",
+                SqlParams = new object[]
+                {
+                    new OracleParameter("p_uetr", OracleDbType.Varchar2, System.Data.ParameterDirection.Input) { Value = uetr}
+                }
+            };
+        }
+
+        public static BarsSql GenerateACSC(string uetr)
+        {
+            return new BarsSql()
+            {
+                SqlText = @"begin bars_swift_msg.generate_acsc (:p_uetr); end;",
+                SqlParams = new object[]
+                {
+                    new OracleParameter("p_uetr", OracleDbType.Varchar2, System.Data.ParameterDirection.Input) { Value = uetr}
+                }
+            };
+        }
+    }
+}
