a9c1a6a00b1cf04ad8f6e659493f522e3d5e1327
stanislav.zuiko <stanislav.zuiko@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-20 15:23:09 +0000

    #COBUMMFO-8019# Коментар: MERGE Анна Крахмальова

---
+++ ABS/master/web/mmfo/barsroot/App_Code/Areas/Swift/Controllers/SwiftMTController.cs
@@ -0,0 +1,80 @@
+﻿using BarsWeb.Controllers;
+using System;
+using BarsWeb.Areas.Swift.Infrastructure.DI.Abstract;
+using System.Web.Mvc;
+using BarsWeb.Areas.Kernel.Models;
+using BarsWeb.Areas.Swift.Infrastructure.DI.Implementation;
+using System.Linq;
+using BarsWeb.Models;
+
+namespace BarsWeb.Areas.Swift.Controllers
+{
+    [AuthorizeUser]
+    public class SwiftMTController : ApplicationController
+    {
+
+        readonly ISwiftRepository _repo;
+        public SwiftMTController(ISwiftRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public ActionResult Index(bool IsRejectView)
+        {
+            ViewBag.IsRejectView = IsRejectView;
+            return View();
+        }
+
+        public ActionResult GetSWREF(string UETR)
+        {
+            JsonResponse result = new JsonResponse(JsonResponseStatus.Ok);
+            try
+            {
+                BarsSql sql = SqlCreatorMT.GetSWREFValue(UETR);
+                var code = _repo.ExecuteStoreQuery<double?>(sql).FirstOrDefault();
+                result.data = (code == null) ? code : code.Value;
+            }
+            catch (Exception ex)
+            {
+                result.status = JsonResponseStatus.Error;
+                result.message = ex.Message;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SendReject(string UETR)
+        {
+            JsonResponse result = new JsonResponse(JsonResponseStatus.Ok);
+            try
+            {
+                BarsSql sql = SqlCreatorMT.GenerateReject(UETR);
+                var retValue = _repo.ExecuteStoreCommand(sql.SqlText, sql.SqlParams);
+            }
+            catch (Exception ex)
+            {
+                result.status = JsonResponseStatus.Error;
+                result.message = ex.Message;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SendGenerateACSC(string UETR)
+        {
+            JsonResponse result = new JsonResponse(JsonResponseStatus.Ok);
+            try
+            {
+                BarsSql sql = SqlCreatorMT.GenerateACSC(UETR);
+                var retValue = _repo.ExecuteStoreCommand(sql.SqlText, sql.SqlParams);
+            }
+            catch (Exception ex)
+            {
+                result.status = JsonResponseStatus.Error;
+                result.message = ex.Message;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
