d6e0007ec518d7874f869557e593c89dffba5e53
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-21 08:34:51 +0000

    #COBUMMFO-7728# Коментар:

--- sql/mmfo/bars/View/vw_escr_reg_all_credits.sql
+++ sql/mmfo/bars/View/vw_escr_reg_all_credits.sql
@@ -494,17 +494,18 @@
                           AND t.sdate < TO_DATE ('19/09/2016', 'dd/mm/yyyy')
                      THEN
                         TO_NUMBER (3)
+		     WHEN     EXTRACT (YEAR FROM t.sdate) >= 2017
+                          AND SUBSTR (t.prod, 1, 6) NOT IN ('220257',
+                                                            '220347',
+                                                            '220373')
+                          AND t.sdate >= TO_DATE ('19/09/2016', 'dd/mm/yyyy')
+                     THEN
+                        TO_NUMBER (6)
                      WHEN     cs.subs_numb IS NOT NULL
                           AND EXTRACT (YEAR FROM t.sdate) <> 2017
                           AND t.sdate >= TO_DATE ('19/09/2016', 'dd/mm/yyyy')
                      THEN
                         TO_NUMBER (4)
-					 WHEN     EXTRACT (YEAR FROM t.sdate) >= 2017
-                          AND SUBSTR (t.prod, 1, 6) NOT IN ('220257',
-                                                            '220347',
-                                                            '220373')
-                     THEN
-                        TO_NUMBER (6)
                      WHEN     cs.subs_numb IS NULL
                           AND EXTRACT (YEAR FROM t.sdate) <> 2017
                           AND t.sdate >= TO_DATE ('19/09/2016', 'dd/mm/yyyy')
