09fa8f15879c529885fa3dcb7dfb72f64c0e3bfc
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-21 14:40:12 +0000

    #COBUSUPCORP-805# Коментар:

--- ABS/master/sql/mmfo/barsaq/Job/CHANGE_STATE_CORP2.sql
+++ ABS/master/sql/mmfo/barsaq/Job/CHANGE_STATE_CORP2.sql
@@ -1,31 +1,36 @@
-  begin
+begin 
+
+  dbms_scheduler.disable('CHANGE_STATE_CORP2', force => true);
+
+  for c0 in (select kf from bars.mv_kf) loop
     begin 
-      dbms_scheduler.disable('CHANGE_STATE_CORP2', force => true);
+      dbms_scheduler.disable('CHANGE_STATE_CORP2_'||c0.kf, force => true);
 
       declare
         running integer := 1;
       begin
         while running = 1 loop
-          select count(*) into running from user_scheduler_running_jobs where job_name = 'CHANGE_STATE_CORP2';
+          select count(*) into running from user_scheduler_running_jobs where job_name = 'CHANGE_STATE_CORP2_'||c0.kf;
           dbms_lock.sleep(5);
         end loop;
       end;
 
-      dbms_scheduler.drop_job('CHANGE_STATE_CORP2');
+      dbms_scheduler.drop_job('CHANGE_STATE_CORP2_'||c0.kf);
       exception when others then
         if sqlcode = 27476 then null; end if;
     end;
 
-    dbms_scheduler.create_job(job_name            => 'CHANGE_STATE_CORP2',
+    dbms_scheduler.create_job(job_name            => 'CHANGE_STATE_CORP2_'||c0.kf,
                               job_type            => 'PLSQL_BLOCK',
-                              job_action          => 'BEGIN barsaq.data_import.notify_ibank; END;',
+                              job_action          => 'BEGIN barsaq.data_import.notify_ibank_kf('''||c0.kf||'''); END;',
                               start_date          => to_timestamp_tz('11-04-2018 Europe/Kiev', 'dd-mm-yyyy tzr'),
                               repeat_interval     => 'Freq=MINUTELY;Interval=1',
                               end_date            => to_date(null),
                               job_class           => 'DEFAULT_JOB_CLASS',
                               enabled             => false,
                               auto_drop           => false,
-                              comments            => 'Джоб по смене статусов документов Corp2');
-    dbms_scheduler.enable('CHANGE_STATE_CORP2');
-  end;
+                              comments            => 'Джоб по смене статусов документов Corp2 по '||c0.kf);
+    dbms_scheduler.enable('CHANGE_STATE_CORP2_'||c0.kf);
+  end loop;
+end;
 /
