bde270690cedc54d14bd6495222dde3bb7a38a31
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-06-21 09:42:41 +0000

    #COBUXRMIII-76# Коментар:

--- ABS/master/sql/ru/bars/View/v_tts_xrm.sql
+++ ABS/master/sql/ru/bars/View/v_tts_xrm.sql
@@ -9,8 +9,10 @@ select tt,
        case when nlsa is not null and dk=1 then 1 when nlsb is not null and dk=0 then 1 else 0 end contracta_abs_sel,
        case when nlsb is not null and dk=1 then 1 when nlsa is not null and dk=0 then 1 else 0 end contractb_abs_sel       
   from tts
- where tt in (select tt from staff_tts s where date_is_valid (s.adate1, s.adate2, s.rdate1,s.rdate2) = 1)
- and substr(flags,1,1)=1
+ where (tt in (select tt from staff_tts s where date_is_valid (s.adate1, s.adate2, s.rdate1,s.rdate2) = 1 and substr(flags,1,1)=1)
+        or
+        exists (select null from dpt_tts_vidd d where d.tt = tts.tt ))
+
  and fli < 3;
 /
 grant select,delete,update,insert on bars.v_tts_xrm to bars_access_defrole;
