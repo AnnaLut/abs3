759acc55ed1dc757b1d2286b53d352bd85a95a12
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-04 13:06:38 +0000

    #COBUMMFO-7985# Коментар: Merge

--- sql/mmfo/bars/Script/add_funk_NLI_NLJ.sql
+++ sql/mmfo/bars/Script/add_funk_NLI_NLJ.sql
@@ -3,32 +3,36 @@
 --------------------------------------------
 declare 
     l_codeoper number;
-    l_codearm  varchar2(10) := '$RM_@WF1'; -- АРМ МВПС. Управление платежами
+    l_codearm  varchar2(10) := '$RM_VALB'; -- АРМ Валютний Бухгалтер 
+    l_codearm_1  varchar2(10) := '$RM_@WF1'; -- АРМ SWIFT. Обробка повідомлень (повна) 
 begin   
     -- Создать обновить функцию
     l_codeoper := operlist_adm.add_new_func
-                   (p_name      =>     '2909/3720 розбір SWIFT по ЮЛ',          -- Наименование функции
+                   (p_name      =>     '3720 NLI ->розбір нез’ясованих SWIFT по Юр. особам',          -- Наименование функции
                     p_funcname  =>     '/barsroot/gl/nl/index?tip=nli'||chr(38)||'ttList=99', 
                     p_frontend  =>      1 );                       -- 1 - web интерфейс, 0 - desctop
  
     -- добавить функциюв Арм
     umu.add_func2arm(l_codeoper, l_codearm, 1 );                 --(1/0 - подтвержденный/неподтвержденный ресурс)
+    umu.add_func2arm(l_codeoper, l_codearm_1, 1 );                 --(1/0 - подтвержденный/неподтвержденный ресурс)
     commit;
 end;
 /
 
 declare 
     l_codeoper number;
-    l_codearm  varchar2(10) := '$RM_@WF1'; -- АРМ МВПС. Управление платежами
+    l_codearm  varchar2(10) := '$RM_VALB'; -- АРМ Валютний Бухгалтер
+    l_codearm_1  varchar2(10) := '$RM_@WF1'; -- АРМ SWIFT. Обробка повідомлень (повна)
 begin   
     -- Создать обновить функцию
     l_codeoper := operlist_adm.add_new_func
-                   (p_name      =>     '2909/3720 розбір SWIFT по ФЛ',          -- Наименование функции
+                   (p_name      =>     '3720 NLJ ->розбір нез’ясованих SWIFT по Фіз. Особам',          -- Наименование функции
                     p_funcname  =>     '/barsroot/gl/nl/index?tip=nlj'||chr(38)||'ttList=99', 
                     p_frontend  =>      1 );                       -- 1 - web интерфейс, 0 - desctop
  
     -- добавить функциюв Арм
     umu.add_func2arm(l_codeoper, l_codearm, 1 );                 --(1/0 - подтвержденный/неподтвержденный ресурс)
+    umu.add_func2arm(l_codeoper, l_codearm_1, 1 );                 --(1/0 - подтвержденный/неподтвержденный ресурс)
     commit;
 end;
 /
