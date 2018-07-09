6258a44a6e76fe22dd3d6c7f97d06adad97f84c6
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-09 11:03:30 +0000

    #COBUSUPCORP-805# Коментар:

--- sql/mmfo/barsaq/Procedure/p_doc_export_align_byref.sql
+++ sql/mmfo/barsaq/Procedure/p_doc_export_align_byref.sql
@@ -11,10 +11,10 @@ PROMPT *** Create  procedure P_DOC_EXPORT_ALIGN_BYREF ***
 as 
 begin
     for c in (select * from v_doc_statuses where ref=p_ref)
-    loop 
-        update doc_export set status_id=c.bars_status_id, bank_back_reason=c.back_reason, status_change_time=c.change_time where doc_id=c.doc_id;
-        update ibank.v_doc_export set status_id=c.bars_status_id, bank_back_reason=c.back_reason, status_change_time=c.change_time where doc_id=c.doc_id; 
-    end loop; 
+    loop
+        update doc_export set BANK_ACCEPT_DATE= c.change_time, status_id=c.bars_status_id, bank_back_reason=c.back_reason, status_change_time=c.change_time where doc_id=c.doc_id;
+        update ibank.v_doc_export set BANK_ACCEPT_DATE= c.change_time, status_id=c.bars_status_id, bank_back_reason=c.back_reason, status_change_time=c.change_time where doc_id=c.doc_id;
+    end loop;
     commit;
 end;
 /
