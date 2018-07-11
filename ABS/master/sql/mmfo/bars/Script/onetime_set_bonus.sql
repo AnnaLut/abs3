7fda4cd3376a27449f4f1216ec06c1c2b787d4f9
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-11 13:41:05 +0000

    #COBUMMFO-6766# Коментар: Merge.

--- ABS/master/sql/mmfo/bars/Script/onetime_set_bonus.sql
+++ ABS/master/sql/mmfo/bars/Script/onetime_set_bonus.sql
@@ -14,10 +14,8 @@ declare
   l_err varchar2(200);
   l_iserror boolean;
 BEGIN
-  
-  
   bc.go('/');
-  FOR cur IN (SELECT kf FROM bars.mv_kf)
+  FOR cur IN (SELECT kf FROM bars.mv_kf where kf in (328845, 337568))
   
    LOOP
     bc.go(cur.kf);
@@ -43,68 +41,19 @@ BEGIN
            where tag = 'BONUS'
              and DPT_ID = i.deposit_id;
           l_err := l_err ||'3-';
-          DPT_BONUS.SET_BONUS_RATE(i.deposit_id, p_dat + 1, l_bonusval);
-          l_err := l_err ||'4-';
+        begin
+          DPT_BONUS.SET_BONUS_RATE_long(i.deposit_id, p_dat + 1, l_bonusval);
           bars_audit.trace('%s встановлена бонусна ставка = %s',
                            l_title,
                            to_char(l_bonusval));
-
-        if i.type_code = 'MPRG' then       --#3 -- если прогрессивный
-        l_err := l_err ||'5-';
-         IF i.cnt_dubl > 0 then --#3.1  -- если кол-во пролонгаций больше 0
-          begin
-            select *
-              into l_indrate --индивидуальная ставка для этого кол-ва пролонгаций
-              from bars.Dpt_Vidd_Extdesc dve
-             where dve.base_rate = i.br
-               and dve.ext_num = i.cnt_dubl
-               and dve.type_id = i.ext_id;
-          exception
-            when others then
-              l_indrate := null;
-          end;
-
-         if nvl(l_indrate.indv_rate,0) <> 0 --#4  -- если удалось определить значение ставки для этого кол-ва пролонгаций
-         then 
-
-          l_bonusval := l_bonusval + l_indrate.indv_rate; -- добавляем бонус за пролонгацию к уже имеющимся бонусам
-          l_err := l_err ||'6-';
-          
-         --обновляем бонусную ставку с учетом бонуса за кол-во пролонгаций 
-         begin
-          insert into bars.Int_Ratn (acc, id, bdat, ir, br, op)
-          values(i.acc, 1, trunc(p_dat + 1), l_bonusval, l_indrate.base_rate, l_indrate.oper_id);
-         exception when dup_val_on_index then
-          update bars.int_ratn
-          set ir = l_bonusval,
-              op = case when nvl(l_indrate.indv_rate, 0) != 0 and nvl(l_indrate.oper_id, 0) = 0
-                   then 1  else l_indrate.oper_id end
-          where acc = i.acc and bdat = p_dat + 1 and id = 1 and br = l_indrate.base_rate;
-         when others then
-            l_err := 'Ошибка установки бонусной ставки acc = '||to_char(i.acc)||' ir = '||to_char(l_bonusval)||' dat = '||to_char(p_dat+1 ,'DD/MM/YYYY');
-            l_iserror := true;
-          end;
-          
-           l_err := l_err ||'7-';
-          -- обновляем параметры депозита 
-           begin
-              INSERT INTO dpt_depositw (dpt_id, tag, value)
-              VALUES (i.deposit_id, 'BONUS', to_char(l_bonusval));
-           exception when dup_val_on_index then
-              update dpt_depositw
-              set value = to_char(l_bonusval)
-              where tag = 'BONUS' and dpt_id = i.deposit_id;
-            end;
-            l_err := l_err ||'8-';
-            bars_audit.trace('%s значение бонуса записано в доп.реквизиты вклада', l_title);                
-          else --#4 -- кол-во пролонгаций больше 0, но не удалось определить бонус за пролонгацию
-            l_err := 'Установлен некорректный бонус для прогрессивного депозита - не удалось определить бонус для acc = '||to_char(i.acc)||' для кол-ва пролонгаций '||to_char(i.cnt_dubl);
-            l_iserror := true;  
-          end if; --#4
-          
-         end if; --#3.1 
-
-          -- а еще, если депозит прогрессивный, то берем дату открытия или каждой 12-й пролонгации
+        exception when others then
+          l_err := 'Ошибка установки бонусной ставки acc = '||to_char(i.acc)||' ir = '||to_char(l_bonusval)||' dat = '||to_char(p_dat+1 ,'DD/MM/YYYY');
+          l_iserror := true; 
+        end;    
+          l_err := l_err ||'4-';
+        
+        if i.type_code = 'MPRG' then --#3
+                 -- а еще, если депозит прогрессивный, то берем дату открытия или каждой 12-й пролонгации
               select nvl(max(dat_begin), i.dat_begin)
               into l_bonusdate
               from bars.dpt_deposit_clos
