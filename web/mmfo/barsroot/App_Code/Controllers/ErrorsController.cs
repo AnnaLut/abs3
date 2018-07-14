0f0b9e7c6bde4816924ffaf734d9c01f52e307b2
anna.lut <anna.lut@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-14 09:54:32 +0000

    #COBUMMFO-8553# Коментар: откат обновлений , посокольку переданные бф-ом  поламали работу

--- web/mmfo/barsroot/App_Code/Controllers/ErrorsController.cs
+++ web/mmfo/barsroot/App_Code/Controllers/ErrorsController.cs
@@ -15,35 +15,10 @@ namespace BarsWeb.Controllers
                     model = System.Web.HttpContext.Current.Application["AppError"];
                 if (model == null)
                     model = new Exception("Помилка незнайдена");
-                ViewBag.FatalError = 0;
+
                 return View(model);
             }
-
-            if (type.ToUpper() == "HTTP_STATUS_REQUEST_TIMEOUT")
-            {
-                var model = System.Web.HttpContext.Current.Session["AppError"];
-                if (model == null)
-                    model = System.Web.HttpContext.Current.Application["AppError"];
-                if (model == null)
-                    model = new Exception("Помилка незнайдена");
-                ViewBag.FatalError = 1;
-                System.Web.HttpContext.Current.Response.StatusCode = 408;
-                return View(model);
-            }
-
-            if (type.ToUpper()=="FATAL")
-            {
-                //ложим сайт (заявка COBUMMFO-8553)
-                System.Web.HttpContext.Current.Response.StatusCode = 408;
-
-                Response.Flush();
-                Response.Close();
-                Response.End();
-                return null;
-            }
-
-            var result= View();
-            return result;
+            return View();
         }
     }
 }
