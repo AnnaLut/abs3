4537889f3811c5b86cec86c85a568a2bf7446ca6
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-17 09:51:31 +0000

    #COBUMMFO-6382# Коментар:

--- ABS/master/sql/mmfo/bars/Job/sto_pm_auto_try.sql
+++ ABS/master/sql/mmfo/bars/Job/sto_pm_auto_try.sql
@@ -24,7 +24,7 @@ begin
                                                                  bars.sto_payment_utl.auto_try;
                                                                  commit;
                                                            end loop;
-                                                        end',
+                                                        end;',
                                 start_date          => to_date('01-01-2000 00:00:00', 'dd-mm-yyyy hh24:mi:ss'),
                                 repeat_interval     => 'Freq=Daily;Interval=1',
                                 end_date            => to_date(null),
