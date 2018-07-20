a9d3f6861dc7bab19f2354b4275a2eb3eaf1de45
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-20 15:01:42 +0000

    #COBUMMFO-7839# Коментар: MERGE: COBUMMFO-7839 Олександр Кузяков

--- ABS/master/web/ru/barsroot/App_Code/Areas/Cdm/Models/Transport/V2/DuplicatesV2.cs
+++ ABS/master/web/ru/barsroot/App_Code/Areas/Cdm/Models/Transport/V2/DuplicatesV2.cs
@@ -7,21 +7,13 @@ using Oracle.DataAccess.Types;
 
 namespace BarsWeb.Areas.Cdm.Models.Transport
 {
-    // A custom class mapping to an Oracle user defined type.
-    // Provided all required implementations by ODP.NET developer guide to represent the Oracle UDT as custom type.
-    // The custom class must implement IOracleCustomType and INullable interfaces.
-    // Note: Any Oracle UDT name must be uppercase.
     [DataContract]
-    public class DuplicatesV2 : IOracleCustomType, INullable
+    public class DuplicatesV2
     {
-        private string _gcif=string.Empty;
-        private string _masterGcif = string.Empty;
-
         // Код РУ (код МФО)
         [XmlAttribute("kf")]
         [DataMember(IsRequired = true)]
         [Required]
-        [OracleObjectMapping("KF")]
         public string Kf { get; set; }
 
 
@@ -29,16 +21,11 @@ namespace BarsWeb.Areas.Cdm.Models.Transport
         [XmlAttribute("rnk")]
         [DataMember(IsRequired = true)]
         [Required]
-        [OracleObjectMapping("RNK")]
         public long Rnk { get; set; }
 
         // Ідентифікатор майстер-запису
         [XmlAttribute("gcif")]
-        [OracleObjectMapping("GCIF")]
-        public string Gcif {
-            get { return _gcif; }
-            set { _gcif = value; }
-        }
+        public string Gcif { get; set; }
         public bool ShouldSerializeGcif()
         {
             return !string.IsNullOrEmpty(Gcif);
@@ -46,70 +33,10 @@ namespace BarsWeb.Areas.Cdm.Models.Transport
 
         // Ідентифікатор майстер-запису майстер-картки
         [XmlAttribute("masterGcif")]
-        [OracleObjectMapping("MASTERGCIF")]
-        public string MasterGcif
-        {
-            get { return _masterGcif;}
-            set { _masterGcif = value; }
-        }
+        public string MasterGcif { get; set; }
         public bool ShouldSerializeMasterGcif()
         {
             return !string.IsNullOrEmpty(MasterGcif);
-        }
-
-        // Implementation of interface IOracleCustomType method FromCustomObject.
-        // Set Oracle object attribute values from .NET custom type object.
-        public void FromCustomObject(OracleConnection con, IntPtr pUdt)
-        {
-            OracleUdt.SetValue(con, pUdt, "KF", Kf);
-            OracleUdt.SetValue(con, pUdt, "RNK", Rnk);
-            if (null != Gcif) OracleUdt.SetValue(con, pUdt, "GCIF", Gcif);
-            if (null != MasterGcif) OracleUdt.SetValue(con, pUdt, "MASTERGCIF", MasterGcif);
-        }
-
-        // Implementation of interface IOracleCustomType method ToCustomObject.
-        // Set .NET custom type object members from Oracle object attributes.
-        public void ToCustomObject(OracleConnection con, IntPtr pUdt)
-        {
-            Kf = (string)OracleUdt.GetValue(con, pUdt, "KF");
-            Rnk = (long)OracleUdt.GetValue(con, pUdt, "RNK");
-            Gcif = (string)OracleUdt.GetValue(con, pUdt, "GCIF");
-            MasterGcif = (string)OracleUdt.GetValue(con, pUdt, "MASTERGCIF");
-        }
-
-        // A property of interface INullable. Indicate whether the custom type object is null.
-        public bool IsNull
-        {
-            get { return string.IsNullOrWhiteSpace(Kf); }
-        }
-    }
-
-    [OracleCustomTypeMapping("BARS.R_SLAVE_CLIENT_EBK")]
-    public class DuplicatesV2Factory : IOracleCustomTypeFactory
-    {
-        // Implementation of interface IOracleCustomTypeFactory method CreateObject.
-        // Return a new .NET custom type object representing an Oracle UDT object.
-        public IOracleCustomType CreateObject()
-        {
-            return new DuplicatesV2();
-        }
-    }
-
-
-    [OracleCustomTypeMapping("BARS.T_SLAVE_CLIENT_EBK")]
-    public class DuplicatesV2ListFactory : IOracleArrayTypeFactory
-    {
-        // Implementation of interface IOracleArrayTypeFactory method CreateArray to return a new array.
-        public Array CreateArray(int numElems)
-        {
-            return new DuplicatesV2[numElems];
-        }
-
-        // Implementation of interface IOracleArrayTypeFactory method CreateStatusArray to return a new OracleUdtStatus array.
-        public Array CreateStatusArray(int numElems)
-        {
-            //return null;
-            return new OracleUdtStatus[numElems];
         }
     }
 }
