af77f4fbaaeef6cbe4c142bd12a9b21c0bab546d
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-23 06:27:08 +0000

    #COBUMMFO-8737# Коментар:

--- sql/mmfo/bars/Script/add_flag_13.sql
+++ sql/mmfo/bars/Script/add_flag_13.sql
@@ -1,15 +1,7 @@
 BEGIN
    FOR k IN (SELECT *
                FROM tts
-              WHERE tt IN ('IB1',
-                           'IB2',
-                           'IB3',
-                           'IB4',
-                           'IB5',
-                           'IB6',
-                           'CL1',
-                           'CL2',
-                           'CL5'))
+              WHERE tt like 'IB%' or tt like 'CL%')
    LOOP
       UPDATE tts
          SET flags = SUBSTR (flags, 1, 13) || 1 || SUBSTR (flags, 15, 50)
