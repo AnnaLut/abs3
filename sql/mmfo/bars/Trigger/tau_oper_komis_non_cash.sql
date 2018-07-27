5960e141ea9033415c8926c2960d7ba0d3c5a5c6
andrii.biletskyi <andrii.biletskyi@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-27 12:22:22 +0000

    #COBUMMFO-8785# Коментар: Merge.

--- sql/mmfo/bars/Trigger/tau_oper_komis_non_cash.sql
+++ sql/mmfo/bars/Trigger/tau_oper_komis_non_cash.sql
@@ -13,8 +13,13 @@ WHEN ( new.TT = 'R01'         -- міжбанк
 begin
 
   insert -- добавляем документ в очередь на снятие комиссии
-    into KOMIS_NON_CASH (ref)
-  values (:new.ref);
+    into KOMIS_NON_CASH ( REF )
+  select :new.REF
+    from ACCOUNTS
+   where KF  = :new.KF
+     and NLS = :new.NLSB
+     and KV  = :new.KV
+     and TIP = 'DEP';
 
 end TAU_OPER_KOMIS_NON_CASH;
 /
