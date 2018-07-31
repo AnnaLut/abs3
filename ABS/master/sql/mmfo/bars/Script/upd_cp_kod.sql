ee3fdaeaefb2a5fb77372c512d55bbb8536ab606
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-07-31 09:27:35 +0000

    #COBUMMFO-7827# Коментар: merge to master

--- ABS/master/sql/mmfo/bars/Script/upd_cp_kod.sql
+++ ABS/master/sql/mmfo/bars/Script/upd_cp_kod.sql
@@ -8,4 +8,16 @@ begin
       dbms_output.put_line('Не вдалося встановити значення стартовоъ цыни для ЦП 303');
 end;
 /
+
+begin
+  bc.go(300465);
+  update cp_kod set cena_start = 0.0001000841 where id = 302;
+  bc.home;
+  exception
+    when others then
+      bc.home;
+      dbms_output.put_line('Не вдалося встановити значення стартовоъ цыни для ЦП 302');
+end;
+/
+
 commit;
