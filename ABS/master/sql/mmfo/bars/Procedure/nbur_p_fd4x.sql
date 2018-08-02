c6975debf0a68277b50854a3ca673690154a2d2b
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-08-02 13:30:14 +0000

    #COBUMMFO-8418# Коментар: Merge

--- ABS/master/sql/mmfo/bars/Procedure/nbur_p_fd4x.sql
+++ ABS/master/sql/mmfo/bars/Procedure/nbur_p_fd4x.sql
@@ -1,5 +1,4 @@
-CREATE OR REPLACE PROCEDURE NBUR_P_FD4X (
-                                          p_kod_filii        varchar2
+CREATE OR REPLACE PROCEDURE NBUR_P_FD4X (p_kod_filii        varchar2
                                           , p_report_date    date
                                           , p_form_id        number
                                           , p_scheme           varchar2 default 'C'
@@ -10,9 +9,9 @@ is
 % DESCRIPTION : Процедура формирования D4X для Ощадного банку
 % COPYRIGHT   : Copyright UNITY-BARS Limited, 1999.  All Rights Reserved.
 %
-% VERSION     :  v.1.000  26/06/2018
+% VERSION     :  v.1.001  30/07/2018 (26/06/2018)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
-  ver_              char(30)  := 'v.1.000  26/06/2018';
+  ver_              char(30)  := 'v.1.001  30/07/2018';
   c_title           constant varchar2(100 char) := $$PLSQL_UNIT || '.';  
   c_AD4001          constant varchar2(100 char) := 'AD4001';
   c_AD4002          constant varchar2(100 char) := 'AD4002';
@@ -23,6 +22,8 @@ is
   l_file_code     varchar2(2) := substr(p_file_code, 2, 2);
   l_version_id    nbur_lst_files.version_id%type;
   l_start_date    date;--Дата начала охвата
+  
+  l_old_file_code varchar2(3) := '#D4';
 
   --Exception
   e_ptsn_not_exsts exception;
@@ -56,10 +57,12 @@ BEGIN
                           );
 
   logger.trace(c_title || ' Version_id is ' || l_version_id);
+  
+  -- очікуємо формування старого файлу
+  nbur_waiting_form(p_kod_filii, p_report_date, l_old_file_code, c_title);    
 
   insert into nbur_log_fd4x(report_date, kf, nbuc, version_id, ekp, r030, f025, b010, q006, t071, description, acc_id, acc_num, kv, maturity_date, cust_id, ref, nd, branch)
-     select 
-            p_report_date /*report_date*/
+     select p_report_date /*report_date*/
             , p_kod_filii /*kf*/
             , p_kod_filii /*nbuc*/
             , l_version_id /*version_id*/
@@ -72,70 +75,32 @@ BEGIN
             , lpad(kv, 3, '0') /*r030*/
             , dd /*f025*/
             , kb /*b010*/
-            , null /*q006*/
-            , bal /*t071*/ 
+            , comm /*q006*/
+            , t071 /*t071*/ 
             , ' ' /*description*/
-            , acc_id /*acc_id*/
-            , acc_num /*acc_num*/
-            , kv /*kv*/
+            , null /*acc_id*/
+            , null /*acc_num*/
+            , null /*kv*/
             , null /*maturity_date*/
-            , CUST_ID /*cust_id*/
-            , REF /*ref*/
+            , null /*cust_id*/
+            , null /*ref*/
             , null /*nd*/
-            , branch /*branch*/
-      from   (  
-                select  cust_id, tr.acc_id, tr.acc_num, tr.ref, tr.kv, tr.bal, tr.branch
-                        , case
-                            when tr.acc_id = tr.acc_id_db and tr.r020_cr like '3739%' and tr.dd = '00' then '31'
-                            when tr.acc_id = tr.acc_id_cr and tr.r020_db like '3739%' and tr.dd = '00' then '13'                        
-                          else
-                            dd
-                          end as dd                           
-                        , case
-                            when alt_bic is null or alt_bic = ' ' then '0000000000'
-                          else
-                            SUBSTR(TO_CHAR(10000000000 + TO_NUMBER(alt_bic)), 2, 10)
-                          end as kb
-                from  (
-                          select cust.cust_id, ac.acc_id, ac.acc_num, ac.kv, cb.alt_bic, tr.ref, tr.bal, tr.bal_uah, ac.branch, tr.acc_id_db, tr.r020_db, tr.acc_id_cr, tr.r020_cr, tr.operation_date, ac.kf, tr.fdate
-                                 , NVL(substr(w.value, 1, 2), '00') as dd
-                          from   nbur_dm_customers cust
-                                 join custbank cb  on (cust.cust_id = cb.rnk)
-                                 join nbur_dm_accounts ac on (cust.kf = ac.kf)
-                                                             and (cust.cust_id = ac.cust_id)                                   
-                                 join table(F_GET_ACC_TRANSACTION(ac.acc_id, l_start_date, p_report_date)) tr on (1 = 1)   
-                                 left join operw w on (tr.ref = w.ref) 
-                                                      and (w.tag like 'D#40%')                                                          
-                          where  cust.kf = p_kod_filii
-                                 and cust.k030 = 2
-                                 and ac.nbs in (
-                                                 select r020 
-                                                 from   kl_f3_29 
-                                                 where  kf = 'D4'
-                                               ) 
-                                 and tr.sos = 5
-                                 and tr.bal <> 0                                                                           
-                        ) tr      
-                  where   not exists(
-                                            select null
-                                            from   v_nbur_dm_transactions t
-                                            where  t.kf = tr.kf
-                                                   and t.ref = tr.ref                              
-                                                   and (
-                                                         (
-                                                           t.bal = tr.bal and 
-                                                           (
-                                                             (t.R020_DB = '1608' and t.r020_cr = '1600') 
-                                                             OR (t.r020_db = '1600' and t.r020_cr = '6110')
-                                                           )
-                                                         ) 
-                                                         OR  
-                                                         (
-                                                           t.bal = GL.P_ICURVAL(tr.kv, tr.bal, tr.fdate) 
-                                                           and t.R020_DB = '3801' and t.r020_cr = '6110'
-                                                         )
-                                                       )
-                                    )    
+            , null /*branch*/
+      from (select v1.pp, v1.dd, v1.kb, v1.kv, v1.t071, v2.comm 
+            from (select substr(field_code,2) kodp, seg_01 pp, seg_02 dd, seg_03 kb, seg_04 kv, field_value t071
+                  from v_nbur_#d4 
+                  where report_date = p_report_date and
+                        kf = p_kod_filii and
+                        seg_01 = '1'
+                  ) v1
+                  left outer join
+                  (select substr(field_code,2) kodp, seg_01 pp, seg_02 dd, seg_03 kb, seg_04 kv, substr(field_value,1,160) comm
+                  from v_nbur_#d4 
+                  where report_date = p_report_date and
+                        kf = p_kod_filii and
+                        seg_01 = '9'
+                  ) v2
+                  on (v1.kodp = v2.kodp)
       );
 
   logger.info(c_title || ' end for date = '||to_char(p_report_date, 'dd.mm.yyyy'));
