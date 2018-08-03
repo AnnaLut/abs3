781fffecbb30fd2031aabde9d248b459fbc3ca41
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-08-03 12:57:09 +0000

    #COBUMMFO-81# Коментар:

--- ABS/master/sql/mmfo/bars/View/v_mbm_customers.sql
+++ ABS/master/sql/mmfo/bars/View/v_mbm_customers.sql
@@ -1,14 +1,65 @@
+/* Formatted on 03/08/2018 14:20:07 (QP5 v5.267.14150.38573) */
+PROMPT View V_MBM_CUSTOMERS;
+--
+-- V_MBM_CUSTOMERS  (View)
+--
 
-
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/View/V_MBM_CUSTOMERS.sql =========*** Run **
-PROMPT ===================================================================================== 
-
-
-PROMPT *** Create  view V_MBM_CUSTOMERS ***
-
-  CREATE OR REPLACE FORCE VIEW BARS.V_MBM_CUSTOMERS ("RNK", "TGR", "CUSTTYPE", "COUNTRY", "NMK", "NMKV", "NMKK", "CODCAGENT", "PRINSIDER", "OKPO", "ADR", "SAB", "C_REG", "C_DST", "RGTAX", "DATET", "ADM", "DATEA", "STMT", "DATE_ON", "DATE_OFF", "NOTES", "NOTESEC", "CRISK", "PINCODE", "ND", "RNKP", "ISE", "FS", "OE", "VED", "SED", "LIM", "MB", "RGADM", "BC", "BRANCH", "TOBO", "ISP", "TAXF", "NOMPDV", "K050", "NREZID_CODE", "BANK_DATE") AS 
-  SELECT c."RNK",
+CREATE OR REPLACE FORCE VIEW BARS.V_MBM_CUSTOMERS
+(
+   RNK,
+   TGR,
+   CUSTTYPE,
+   COUNTRY,
+   NMK,
+   NMKV,
+   NMKK,
+   CODCAGENT,
+   PRINSIDER,
+   OKPO,
+   ADR,
+   SAB,
+   C_REG,
+   C_DST,
+   RGTAX,
+   DATET,
+   ADM,
+   DATEA,
+   STMT,
+   DATE_ON,
+   DATE_OFF,
+   NOTES,
+   NOTESEC,
+   CRISK,
+   PINCODE,
+   ND,
+   RNKP,
+   ISE,
+   FS,
+   OE,
+   VED,
+   SED,
+   LIM,
+   MB,
+   RGADM,
+   BC,
+   BRANCH,
+   TOBO,
+   ISP,
+   TAXF,
+   NOMPDV,
+   K050,
+   NREZID_CODE,
+   PHONE,
+   BANK_NAME,
+   ADDRESS_BANK,
+   TORG_ACC,
+   BUY_COM,
+   SELL_COM,
+   KONV_COM,
+   BANK_DATE
+)
+AS
+   SELECT c."RNK",
           c."TGR",
           c."CUSTTYPE",
           c."COUNTRY",
@@ -18,7 +69,16 @@ PROMPT *** Create  view V_MBM_CUSTOMERS ***
           c."CODCAGENT",
           c."PRINSIDER",
           c."OKPO",
-          c."ADR",
+          (SELECT    ca.zip
+                  || ', '
+                  || (SELECT name
+                        FROM bars.country
+                       WHERE country = ca.country)
+                  || ', '
+                  || ca.address
+             FROM bars.customer_address ca
+            WHERE ca.rnk = c.rnk AND ca.type_id = 1)
+             adr,
           c."SAB",
           c."C_REG",
           c."C_DST",
@@ -51,17 +111,41 @@ PROMPT *** Create  view V_MBM_CUSTOMERS ***
           c."NOMPDV",
           c."K050",
           c."NREZID_CODE",
-          GL.BD as BANK_DATE
-     FROM customer c
-    WHERE custtype IN (2,3);
+          (SELECT p.telw
+             FROM bars.person p
+            WHERE p.rnk = c.rnk)
+             phone,
+          (SELECT t.val
+             FROM params t
+            WHERE t.par = 'NAME')
+             bank_name,
+          (SELECT bav.attribute_value
+             FROM branch_attribute_value bav
+            WHERE     bav.attribute_code = 'ADDRESS'
+                  AND bav.branch_code = '/' || c.kf || '/')
+             address_bank,
+          cz.NLS29 torg_acc,
+          cz.kom buy_com,
+          cz.kom2 sell_com,
+          cz.kom3 konv_com,
+          GL.BD AS BANK_DATE
+     FROM bars.customer c LEFT JOIN bars.cust_zay cz ON (c.rnk = cz.rnk)
+    WHERE custtype IN (2, 3)
+/
 
-PROMPT *** Create  grants  V_MBM_CUSTOMERS ***
-grant SELECT                                                                 on V_MBM_CUSTOMERS to BARSREADER_ROLE;
-grant SELECT                                                                 on V_MBM_CUSTOMERS to BARS_ACCESS_DEFROLE;
-grant SELECT                                                                 on V_MBM_CUSTOMERS to UPLD;
+COMMENT ON TABLE BARS.V_MBM_CUSTOMERS IS 'Клієнти доступні для підлючення до мобільного банкінгу'
+/
 
 
 
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/View/V_MBM_CUSTOMERS.sql =========*** End **
-PROMPT ===================================================================================== 
+Prompt Grants on VIEW V_MBM_CUSTOMERS TO BARSREADER_ROLE to BARSREADER_ROLE;
+GRANT SELECT ON BARS.V_MBM_CUSTOMERS TO BARSREADER_ROLE
+/
+
+Prompt Grants on VIEW V_MBM_CUSTOMERS TO BARS_ACCESS_DEFROLE to BARS_ACCESS_DEFROLE;
+GRANT SELECT ON BARS.V_MBM_CUSTOMERS TO BARS_ACCESS_DEFROLE
+/
+
+Prompt Grants on VIEW V_MBM_CUSTOMERS TO UPLD to UPLD;
+GRANT SELECT ON BARS.V_MBM_CUSTOMERS TO UPLD
+/
