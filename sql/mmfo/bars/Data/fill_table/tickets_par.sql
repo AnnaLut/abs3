3de6418f27ffbc15dab21ea4eef7973efd980561
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-08-04 09:14:26 +0000

    #COBUMMFO-5602# Коментар: merge to master

--- sql/mmfo/bars/Data/fill_table/tickets_par.sql
+++ sql/mmfo/bars/Data/fill_table/tickets_par.sql
@@ -23,7 +23,7 @@ update tickets_par p
         or
         o.tt in (select tt from tts_vob where vob=6 and tt not in (''SN1'', ''SN3''))
        )
-   and o.tt not in (''015'',''101'',''445'',''KK1'',''PO3'',''ELT'',''I00'',''G02'')'
+   and o.tt not in (''015'',''101'',''445'',''KK1'',''PO3'',''ELT'',''I00'',''G02'', ''SN1'', ''SN3'')'
  where p.par = 'U_ALK_NLSK'
    and p.rep_prefix = 'DEFAULT';
 
@@ -40,7 +40,7 @@ update tickets_par p
               ''ELT'',
               ''KK1'',
               ''G02'',
-			  ''ELA'',
+              ''ELA'',
 	          ''SN1'',
 	          ''SN3'')'
  where p.par = 'U_015_tts'
