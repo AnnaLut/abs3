274a47d30645a3a5debe7f6bac9a7a1a61f7a0a6
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-08-04 14:56:43 +0000

    #COBUMMFO-8548# Коментар: MERGE: COBUMMFO-8548 Констянтин Дзедзюль

---
+++ web/mmfo/barsroot/App_Code/Areas/Kernel/Infrastructure/Extentions/ReaderExtentions.cs
@@ -0,0 +1,27 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ReaderExtentions
+/// </summary>
+namespace BarsWeb.Areas.Kernel.Infrastructure.Extentions
+{
+    public static class ReaderExtentions
+    {
+        public static IEnumerable<Dictionary<string, object>> ReadAllLazy(OracleDataReader reader)
+        {
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+                cols.Add(reader.GetName(i));
+            while (reader.Read())
+            {
+                var row = cols.ToDictionary(col => col, col => reader[col]);
+                yield return row;
+            }
+        }
+
+    }
+}
