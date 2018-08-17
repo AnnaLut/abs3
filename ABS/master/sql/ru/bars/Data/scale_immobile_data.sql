924b8e2aac4653723784e757570396c41401bee9
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-08-17 13:57:30 +0000

    #COBUMMFO-8535# Коментар:

--- ABS/master/sql/ru/bars/Data/scale_immobile_data.sql
+++ ABS/master/sql/ru/bars/Data/scale_immobile_data.sql
@@ -1,5 +1,10 @@
+PROMPT start update currency in SCALE_IMMOBILE
 UPDATE SCALE_IMMOBILE
    SET val = 0
  WHERE kv <> 980;
-
-COMMIT;
+ UPDATE SCALE_IMMOBILE
+      SET val = 10000
+   WHERE kv = 980;
+commit;
+/
+PROMPT end update SCALE_IMMOBILE
