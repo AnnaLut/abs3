4b4203dd8efe895da911815018142c98f8dfe286
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-08-30 06:35:25 +0000

    #COBUMMFO-8326# Коментар:

---
+++ sql/mmfo/bars/Procedure/nbur_p_f08x.sql
@@ -0,0 +1,160 @@
+CREATE OR REPLACE PROCEDURE NBUR_P_F08X (
+                                          p_kod_filii        varchar2
+                                          , p_report_date    date
+                                          , p_form_id        number
+                                          , p_scheme           varchar2 default 'C'
+                                          , p_balance_type     varchar2 default 'S'
+                                          , p_file_code        varchar2 default '08X'
+                                        )
+is
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DESCRIPTION : Процедура формирования 08X для Ощадного банку
+% COPYRIGHT   : Copyright UNITY-BARS Limited, 1999.  All Rights Reserved.
+%
+% VERSION     :  v.1.001  29/08/2018 (08/06/2018)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+  ver_              char(30)  := 'v.1.001  29/08/2018';
+  c_title           constant varchar2(100 char) := $$PLSQL_UNIT || '.';
+
+  c_old_file_code   constant varchar2(3 char) := '#08';
+
+  c_XXXXXX               constant varchar2(6 char) := 'XXXXXX';
+
+  l_nbuc          varchar2(20);
+  l_type          number;
+  l_datez         date := p_report_date + 1;
+  l_file_code     varchar2(2) := substr(p_file_code, 2, 2);
+  l_file_id       nbur_ref_files.id%type := nbur_files.GET_FILE_ID(p_file_code => p_file_code);
+  l_version_id    nbur_lst_files.version_id%type;
+  l_ret           number;  
+
+  --Exception
+  e_ptsn_not_exsts exception;
+
+  pragma exception_init( e_ptsn_not_exsts, -02149 );
+BEGIN
+  logger.info (c_title || ' begin for date = '||to_char(p_report_date, 'dd.mm.yyyy'));
+
+  -- определение начальных параметров (код области или МФО или подразделение)
+  nbur_files.P_PROC_SET(p_kod_filii, p_file_code, p_scheme, l_datez, 1, l_file_code, l_nbuc, l_type);
+  
+  execute immediate 'truncate table NBUR_TMP_DESC_EKP';  
+
+  --Очистка партиции для хранения детального протокола
+  begin
+    execute immediate 'alter table NBUR_LOG_F08X truncate subpartition for ( to_date('''
+                      || to_char(p_report_date,'YYYYMMDD')||''',''YYYYMMDD''), ''' || p_kod_filii || ''' )';
+  exception
+    when e_ptsn_not_exsts then
+      null;
+  end;
+
+  --Определяем версию файла для хранения детальеного протокола
+  l_version_id := f_nbur_get_run_version(
+                                          p_file_code => p_file_code
+                                          , p_kf => p_kod_filii
+                                          , p_report_date => p_report_date
+                                        );
+
+  logger.trace(c_title || ' Version_id is ' || l_version_id);
+
+  -- очікуємо формування старого файлу
+  nbur_waiting_form(p_kod_filii, p_report_date, c_old_file_code, c_title);
+  
+  -- наповнення довідника для визначення кодів показників
+  l_ret := f_nbur_get_ekp_08x(l_datez);       
+
+  --Теперь сохрянем полученные данные в детальном протоколе
+  insert into nbur_log_f08x(
+                             report_date
+                             , kf
+                             , nbuc
+                             , version_id
+                             , EKP
+                             , KU
+                             , T020
+                             , R020
+                             , R011
+                             , R030
+                             , K040
+                             , K072
+                             , S130
+                             , S183
+                             , T070
+                             , description
+                             , acc_id
+                             , acc_num
+                             , kv
+                             , maturity_date
+                             , cust_id
+                             , ref
+                             , nd
+                             , branch
+                           )
+      select 
+             p_report_date as report_date
+             , p_kod_filii as kf
+             , p_kod_filii as nbuc
+             , l_version_id
+             , nvl(ekp, c_XXXXXX) as ekp
+             , f_get_ku_by_nbuc(nbuc) as KU
+             , seg_d as T020
+             , seg_b as R020
+             , seg_z as R011
+             , seg_v as R030
+             , seg_m as K040
+             , seg_y as K072
+             , seg_p as S130
+             , seg_c as S183
+             , to_number(znap) as T070          
+             , comm as description
+             , acc as ACC_ID
+             , nls as ACC_NUM
+             , kv as KV
+             , mdate as MATURITY_DATE
+             , rnk as CUST_ID
+             , ref as REF
+             , nd as ND
+             , tobo as BRANCH 
+      from   (select * from(
+                select 
+                    t.seg_01 as seg_d,
+                    t.seg_02 as seg_b,
+                    t.seg_03 as seg_z,
+                    t.seg_04 as seg_y,
+                    t.seg_05 as seg_r,
+                    t.seg_06 as seg_v,
+                    t.seg_07 as seg_c,
+                    t.seg_08 as seg_p,
+                    t.seg_09 as seg_m,
+                    acc_num as nls, 
+                    kv,
+                    FIELD_CODE as kodp, 
+                    nbuc, 
+                    cust_id as rnk, 
+                    acc_id as acc, 
+                    FIELD_VALUE as znap,
+                    ref,
+                    DESCRIPTION as comm,
+                    nd,
+                    branch as tobo,
+                    MATURITY_DATE as mdate
+                from v_nbur_#08_dtl t
+                where t.report_date = p_report_date and
+                     t.kf = p_kod_filii
+                ) t  
+                left join
+                    (select T.R020, max(t.i010) as i010 
+                     from kl_r020 t 
+                     where T.D_OPEN < sysdate and (T.D_CLOSE is null or t.d_close > sysdate)
+                     group by T.R020) t2 
+                on (t.seg_b = t2.R020)
+                left join nbur_tmp_desc_ekp p
+                on (p.I010 = t2.I010 and
+                    p.t020 = t.seg_d and
+                    p.r020 = t.seg_b)                
+             );
+
+  logger.info(c_title || ' end for date = '||to_char(p_report_date, 'dd.mm.yyyy'));
+END;
+/
---
+++ sql/mmfo/bars/View/v_nbur_08x_dtl.sql
@@ -0,0 +1,85 @@
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/BARS/View/v_nbur_08x_dtl.sql =========*** Run *** =
+PROMPT ===================================================================================== 
+
+PROMPT *** Create  view v_nbur_08x_dtl ***
+
+create or replace view v_nbur_08x_dtl as
+select p.REPORT_DATE
+         , p.KF
+         , p.NBUC
+         , p.VERSION_ID
+         , null as FIELD_CODE
+	     , p.EKP
+	     , p.KU
+	     , p.T020
+	     , p.R020
+	     , p.R011
+	     , p.R030
+	     , p.K040
+	     , p.K072
+	     , p.S130
+	     , p.S183
+	     , p.T070
+         , p.DESCRIPTION
+         , p.ACC_ID
+         , p.ACC_NUM
+         , p.KV
+         , p.MATURITY_DATE
+         , p.CUST_ID
+         , c.CUST_CODE
+         , c.CUST_NAME
+         , p.ND
+         , a.AGRM_NUM
+         , a.BEG_DT
+         , a.END_DT
+         , p.REF
+         , p.BRANCH
+    from NBUR_LOG_F08X p
+         join NBUR_REF_FILES f on (f.FILE_CODE = '08X' )
+         join NBUR_LST_FILES v on (v.REPORT_DATE = p.REPORT_DATE)
+                                  and (v.KF = p.KF)
+                                  and (v.VERSION_ID = p.VERSION_ID)
+                                  and (v.FILE_ID = f.ID)
+         left join V_NBUR_DM_CUSTOMERS c on (p.REPORT_DATE = c.REPORT_DATE)
+                                            and (p.KF = c.KF)
+                                            and (p.CUST_ID    = c.CUST_ID)
+         left  join V_NBUR_DM_AGREEMENTS a on (p.REPORT_DATE = a.REPORT_DATE)
+                                              and (p.KF = a.KF)
+                                              and (p.nd = a.AGRM_ID)
+   where v.FILE_STATUS IN ('FINISHED', 'BLOCKED');
+
+comment on table V_NBUR_08X_DTL is 'Детальний протокол файлу 08X';
+comment on column V_NBUR_08X_DTL.REPORT_DATE is 'Звітна дата';
+comment on column V_NBUR_08X_DTL.KF is 'Код фiлiалу (МФО)';
+comment on column V_NBUR_08X_DTL.VERSION_ID is 'Ід. версії файлу';
+comment on column V_NBUR_08X_DTL.FIELD_CODE is 'Зведений код показника';
+comment on column V_NBUR_08X_DTL.EKP is 'Код показника';
+comment on column V_NBUR_08X_DTL.KU is 'Код території';
+comment on column V_NBUR_08X_DTL.T020 is 'Елемент рахунку';
+comment on column V_NBUR_08X_DTL.R020 is 'Номер рахунку';
+comment on column V_NBUR_08X_DTL.R011 is 'Код за параметром розподілу аналітичного рахунку R011';
+comment on column V_NBUR_08X_DTL.R030 is 'Код валюти';
+comment on column V_NBUR_08X_DTL.K040 is 'Код країни';
+comment on column V_NBUR_08X_DTL.K072 is 'Код сектору економіки';
+comment on column V_NBUR_08X_DTL.S130 is 'Код виду цінного паперу';
+comment on column V_NBUR_08X_DTL.S183 is 'Узагальнений код початкових строків погашення';
+comment on column V_NBUR_08X_DTL.T070 is 'Сума';
+comment on column V_NBUR_08X_DTL.DESCRIPTION is 'Опис (коментар)';
+comment on column V_NBUR_08X_DTL.ACC_ID is 'Ід. рахунка';
+comment on column V_NBUR_08X_DTL.ACC_NUM is 'Номер рахунка';
+comment on column V_NBUR_08X_DTL.KV is 'Ід. валюти';
+comment on column V_NBUR_08X_DTL.MATURITY_DATE is 'Дата Погашення';
+comment on column V_NBUR_08X_DTL.CUST_ID is 'Ід. клієнта';
+comment on column V_NBUR_08X_DTL.CUST_CODE is 'Код клієнта';
+comment on column V_NBUR_08X_DTL.CUST_NAME is 'Назва клієнта';
+comment on column V_NBUR_08X_DTL.ND is 'Ід. договору';
+comment on column V_NBUR_08X_DTL.AGRM_NUM is 'Номер договору';
+comment on column V_NBUR_08X_DTL.BEG_DT is 'Дата початку договору';
+comment on column V_NBUR_08X_DTL.END_DT is 'Дата закінчення договору';
+comment on column V_NBUR_08X_DTL.REF is 'Ід. платіжного документа';
+comment on column V_NBUR_08X_DTL.BRANCH is 'Код підрозділу';
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/BARS/View/v_nbur_08x_dtl.sql =========*** End *** =
+PROMPT ===================================================================================== 
