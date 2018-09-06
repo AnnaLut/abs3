0dea55f4946b0dc81fcf7750e09d7bdab6e4b796
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-09-06 08:50:17 +0000

    #COBUMMFO-7644# Коментар: change script-7644

--- sql/mmfo/nbu_gateway/Script/script-7644.sql
+++ sql/mmfo/nbu_gateway/Script/script-7644.sql
@@ -1,2 +1,18 @@
-drop type t_compare_json_tab;
-drop type t_compare_json;
+declare 
+  v_num integer;
+begin
+  select count(1) into v_num
+    from user_types 
+    where lower(type_name) = 't_compare_json_tab';
+  if v_num = 1 then
+   execute immediate 'drop type t_compare_json_tab';
+  end if;
+
+  select count(1) into v_num
+    from user_types 
+    where lower(type_name) = 't_compare_json';
+  if v_num = 1 then
+   execute immediate 'drop type t_compare_json';
+  end if;
+end;
+/
