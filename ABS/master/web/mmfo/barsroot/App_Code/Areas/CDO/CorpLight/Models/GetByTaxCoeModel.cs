e64a488f3722bc78af81b7570d75f0a64182e547
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-09-10 13:14:32 +0000

    #COBUMMFO-9378# Коментар: MERGE: COBUMMFO-9378 Констянтин Дзедзюль

---
+++ ABS/master/web/mmfo/barsroot/App_Code/Areas/CDO/CorpLight/Models/GetByTaxCoeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for GetByTaxCoeModel
+/// </summary>
+public class GetByTaxCoeModel
+{
+    public GetByTaxCoeModel()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public decimal CustId { get; set; }
+    public string TaxCode { get; set; }
+    public string DocSeries { get; set; }
+    public string DocNumber { get; set; }
+}
