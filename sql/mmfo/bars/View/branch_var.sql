f2eecce52e4431622e980430009024c106351702
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-10-09 06:52:43 +0000

    #COBUMMFO-9435# Коментар: MERGE COBUMMFO-9435 to master (Микола Домалєга)

--- sql/mmfo/bars/View/branch_var.sql
+++ sql/mmfo/bars/View/branch_var.sql
@@ -1,28 +1,39 @@
 
-
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/View/BRANCH_VAR.sql =========*** Run *** ===
-PROMPT ===================================================================================== 
-
-
-PROMPT *** Create  view BRANCH_VAR ***
-
+ 
+ PROMPT ===================================================================================== 
+ PROMPT *** Run *** ========== Scripts /Sql/BARS/view/branch_var.sql =========*** Run *** ===
+ PROMPT ===================================================================================== 
+ 
   CREATE OR REPLACE FORCE VIEW BARS.BRANCH_VAR ("BRANCH", "NAME") AS 
   SELECT BRANCH, NAME
-     FROM BRANCH
-    WHERE     LENGTH (BRANCH) >= 15
-          AND branch LIKE SYS_CONTEXT ('bars_context', 'user_branch_mask')
-          and date_closed is null
-   UNION ALL
-   SELECT BRANCH || '%', '*' || NAME
-     FROM BRANCH
-    WHERE     LENGTH (BRANCH) = 15
-          AND branch LIKE SYS_CONTEXT ('bars_context', 'user_branch_mask')
-          and date_closed is null
-	UNION ALL
-   SELECT '/______/______/______/' BRANCH, 'Áðàí÷³ ²²² ð³âíÿ' NAME
-   FROM DUAL;
-
+  FROM BRANCH
+ WHERE LENGTH(BRANCH) >= 15
+   AND branch LIKE SYS_CONTEXT('bars_context', 'user_branch_mask')
+   AND date_closed IS NULL
+ UNION ALL
+SELECT BRANCH || '%', '*' || NAME
+  FROM BRANCH
+ WHERE LENGTH(BRANCH) = 15
+   AND branch LIKE SYS_CONTEXT('bars_context', 'user_branch_mask')
+   AND date_closed IS NULL
+ UNION ALL
+SELECT BRANCH || '______/', 'Áðàí÷³ ²² ð³âíÿ ' || NAME
+  FROM BRANCH
+ WHERE LENGTH (BRANCH) = 8
+   AND branch LIKE SYS_CONTEXT('bars_context', 'user_branch_mask')
+   and date_closed is null
+ UNION ALL
+SELECT BRANCH || '______/______/', 'Áðàí÷³ ²²² ð³âíÿ ' || NAME
+  FROM BRANCH
+ WHERE LENGTH (BRANCH) = 8
+   AND branch LIKE SYS_CONTEXT('bars_context', 'user_branch_mask')
+   and date_closed is null
+ UNION ALL
+SELECT '/______/______/______/' BRANCH, 'Âñ³ áðàí÷³ ²²² ð³âíÿ' NAME
+  FROM DUAL
+;
+ show err;
+ 
 PROMPT *** Create  grants  BRANCH_VAR ***
 grant SELECT                                                                 on BRANCH_VAR      to BARSREADER_ROLE;
 grant SELECT                                                                 on BRANCH_VAR      to BARS_ACCESS_DEFROLE;
@@ -30,8 +41,9 @@ grant SELECT                                                                 on 
 grant SELECT                                                                 on BRANCH_VAR      to START1;
 grant SELECT                                                                 on BRANCH_VAR      to UPLD;
 
-
-
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/View/BRANCH_VAR.sql =========*** End *** ===
-PROMPT ===================================================================================== 
+ 
+ 
+ PROMPT ===================================================================================== 
+ PROMPT *** End *** ========== Scripts /Sql/BARS/view/branch_var.sql =========*** End *** ===
+ PROMPT ===================================================================================== 
+ 
