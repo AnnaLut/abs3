bb0cb4a8cac7b98708a0f0c0eb184e49dfb3d5d7
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-10-29 07:05:54 +0000

    #COBUMMFO-9883# Коментар: Merge

--- sql/mmfo/bars/Table/ref_que.sql
+++ sql/mmfo/bars/Table/ref_que.sql
@@ -28,10 +28,36 @@ begin
 	FMCHECK NUMBER(38,0) DEFAULT 0, 
 	KF VARCHAR2(6) DEFAULT sys_context(''bars_context'',''user_mfo''), 
 	OTM NUMBER(*,0)
-   ) SEGMENT CREATION IMMEDIATE 
-  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
- NOCOMPRESS LOGGING
-  TABLESPACE BRSDYND ';
+   ) 
+PARTITION BY LIST (KF)
+( PARTITION KF_300465 VALUES (''300465'')
+, PARTITION KF_302076 VALUES (''302076'')
+, PARTITION KF_303398 VALUES (''303398'')
+, PARTITION KF_304665 VALUES (''304665'')
+, PARTITION KF_305482 VALUES (''305482'')
+, PARTITION KF_311647 VALUES (''311647'')
+, PARTITION KF_312356 VALUES (''312356'')
+, PARTITION KF_313957 VALUES (''313957'')
+, PARTITION KF_315784 VALUES (''315784'')
+, PARTITION KF_322669 VALUES (''322669'')
+, PARTITION KF_323475 VALUES (''323475'')
+, PARTITION KF_324805 VALUES (''324805'')
+, PARTITION KF_325796 VALUES (''325796'')
+, PARTITION KF_326461 VALUES (''326461'')
+, PARTITION KF_328845 VALUES (''328845'')
+, PARTITION KF_331467 VALUES (''331467'')
+, PARTITION KF_333368 VALUES (''333368'')
+, PARTITION KF_335106 VALUES (''335106'')
+, PARTITION KF_336503 VALUES (''336503'')
+, PARTITION KF_337568 VALUES (''337568'')
+, PARTITION KF_338545 VALUES (''338545'')
+, PARTITION KF_351823 VALUES (''351823'')
+, PARTITION KF_352457 VALUES (''352457'')
+, PARTITION KF_353553 VALUES (''353553'')
+, PARTITION KF_354507 VALUES (''354507'')
+, PARTITION KF_356334 VALUES (''356334'')
+)
+tablespace brsdynd   ';
 exception when others then       
   if sqlcode=-955 then null; else raise; end if; 
 end; 
@@ -50,22 +76,26 @@ COMMENT ON COLUMN BARS.REF_QUE.FMCHECK IS '1-проверено фин.мониторингом';
 COMMENT ON COLUMN BARS.REF_QUE.KF IS '';
 COMMENT ON COLUMN BARS.REF_QUE.OTM IS 'Признак возврата на пред.визу';
 
-
+PROMPT *** Create  index PK_REFQUE ***
+begin   
+ execute immediate '
+  CREATE UNIQUE INDEX BARS.PK_REFQUE ON BARS.REF_QUE (KF, REF) 
+LOCAL COMPUTE STATISTICS';
+exception when others then
+  if  sqlcode=-955  then null; else raise; end if;
+ end;
+/
 
 
 PROMPT *** Create  constraint PK_REFQUE ***
 begin   
  execute immediate '
-  ALTER TABLE BARS.REF_QUE ADD CONSTRAINT PK_REFQUE PRIMARY KEY (REF)
-  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSDYNI  ENABLE';
+  ALTER TABLE BARS.REF_QUE ADD CONSTRAINT PK_REFQUE PRIMARY KEY (KF, REF)
+  USING INDEX BARS.PK_REFQUE';
 exception when others then
   if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
  end;
 /
-
-
-
 
 PROMPT *** Create  constraint SYS_C008704 ***
 begin   
@@ -76,9 +106,6 @@ exception when others then
  end;
 /
 
-
-
-
 PROMPT *** Create  constraint CC_REFQUE_KF_NN ***
 begin   
  execute immediate '
@@ -87,22 +114,6 @@ exception when others then
   if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
  end;
 /
-
-
-
-
-PROMPT *** Create  index PK_REFQUE ***
-begin   
- execute immediate '
-  CREATE UNIQUE INDEX BARS.PK_REFQUE ON BARS.REF_QUE (REF) 
-  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
-  TABLESPACE BRSDYNI ';
-exception when others then
-  if  sqlcode=-955  then null; else raise; end if;
- end;
-/
-
-
 
 PROMPT *** Create  grants  REF_QUE ***
 grant DELETE,INSERT,SELECT,UPDATE                                            on REF_QUE         to ABS_ADMIN;
