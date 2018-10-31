4a15056c798d820c891cfda5706833590e40eb69
vitalii.khomida <vitalii.khomida@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-10-31 08:54:53 +0000

    #COBUMMFO-7501,COBUMMFO-9910# Коментар: Merge

--- ABS/master/sql/mmfo/bars/Package/pfu_ru_file_utl.sql
+++ ABS/master/sql/mmfo/bars/Package/pfu_ru_file_utl.sql
@@ -122,10 +122,10 @@ CREATE OR REPLACE PACKAGE BODY BARS.PFU_RU_FILE_UTL is
 					select ac.nbs, ac.tip
 						into l_nbs, l_tip
 						from accounts ac
-					 where ac.nls = l_nlsb
+					 where ac.nls = l_nlsb or (ac.nls = l_nlsb and ac.tip like 'W4%' and ac.nls like '2620%') -- COBUMMFO-7501 ƒобавлен поиск по альтернативе
 						 and ac.kv = 980;
 
-					if (l_nbs = '2625' and l_tip like 'W4%') then
+					if (/*l_nbs = '2625' and Comment COBUMMFO-7501 оставили проверку только по типу счета */ l_tip like 'W4%') then
 						select pqh.resp_code
 							into l_state
 							from (select PQ.REF, PQ.ACC, PQ.SOS, PQ.F_N, PQ.RESP_CODE
