48ef2d673a0ca1bcf38289ef7235d7afc0ef0f51
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-01 07:45:02 +0000

    #COBUMMFO-8184# Коментар: MERGE COBUMMFO-8184 to master (Микола Домалєга)

--- ABS/master/sql/mmfo/bars/Procedure/p_ani34.sql
+++ ABS/master/sql/mmfo/bars/Procedure/p_ani34.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE PROCEDURE BARS.p_ani34 (
 ---------------------------------------------------
   Z_KOD   forex_ob22.kod%type  ; Z_tag1  fx_deal.DEAL_TAG%type;  Z_dat   fx_deal.DAT%type  ;  Z_ntik  fx_deal.NTIK%type ;  Z_rnk fx_deal.RNK%type ;
   Z_dat_M fx_deal.DAT_A%type   ; Z_KVA   fx_deal.KVA%type     ;  Z_suma1 fx_deal.SUMA%type ;  Z_suma2 fx_deal.SUMA%type ;  z_ref fx_deal.Ref%type ;
-  Z_dat_X fx_deal.DAT_B%type   ; Z_KVb   fx_deal.KVB%type     ;  Z_sumb1 fx_deal.SUMB%type ;  Z_sumb2 fx_deal.SUMB%type ;  Z_noga  int ;
+   Z_dat_X fx_deal.DAT_B%type   ; Z_KVb   fx_deal.KVB%type     ;  Z_sumb1 fx_deal.SUMB%type ;  Z_sumb2 fx_deal.SUMB%type ;  --Z_noga  int ;
 
 /*
   28.09.2017 Sta добавлен бал.счет 6214
@@ -53,9 +53,51 @@ CREATE OR REPLACE PROCEDURE BARS.p_ani34 (
                         sum (decode ( nvl(x.swap_tag, x.deal_tag), x.deal_tag, 0,x. suma)   ) sumb2
                  from fx_deal x
                  where NVL(l_tag,0)  in  (0, x.swap_tag, x.deal_tag )
-                 group by nvl(x.swap_tag, x.deal_tag), x.dat,  x.ntik, x.rnk
-                 having ( FOREX.get_forextype(x.dat, max(greatest (x.dat_a, x.dat_b)) , max(greatest (x.dat_a, x.dat_b)) ) ='FORWARD'   OR    count ( * ) > 1      )
-                    and min ( least    (x.dat_a, x.dat_b)) <= l_dat2        and max( greatest (x.dat_a, x.dat_b)) >= l_dat1 ;
+                  group by forex.get_forextype3 (x.DEAL_TAG), nvl(x.swap_tag, x.deal_tag), x.dat,  x.ntik, x.rnk
+                   --having ( FOREX.get_forextype(x.dat, max(greatest (x.dat_a, x.dat_b)) , max(greatest (x.dat_a, x.dat_b)) ) ='FORWARD'   OR    count ( * ) > 1      )
+                   --having  (forex.get_forextype3 (x.DEAL_TAG) in ('D.SWAP_FORWARD','D.SWAP_SPOT','D.SWAP_TOD', 'FORWARD','V.SWAP_FORWARD','V.SWAP_SPOT','V.SWAP_TOD') OR count ( * ) > 1 )
+                   having (forex.get_forextype3(x.deal_tag) like '%.SWAP\_%' escape '\' or forex.get_forextype3(x.deal_tag) = 'FORWARD' or count(*) > 1) --COBUMMFO-8184 MDom 2018.10.18
+                   and min ( least    (x.dat_a, x.dat_b)) <= l_dat2        and max( greatest (x.dat_a, x.dat_b)) >= l_dat1 */
+                  --COBUMMFO-8184 MDom 24.10.2018 переписав попередній запит (підвищена швидкість, особливо за невеликий проміжок часу)
+                  select --якщо в типі угоди немає '_' то виводимо повністю, якщо є, обрізаємо до першого символу '_'
+                         upper(substr(case instr(t.kod, '_')
+                                        when 0 then t.kod
+                                        else substr(t.kod, 1, instr(t.kod, '_')-1)
+                                      end, 1, 9)) as kod, --т.я. Z_KOD VARCHAR2(15) і більше 9 не потрібно
+                         t.tag1,
+                         t.dat,
+                         t.ntik,
+                         t.rnk,
+                         min(t.dat_M) dat_M,
+                         max(t.dat_X) dat_X,
+                         min(t.KVA) KVA,
+                         sum(t.suma1) suma1,
+                         sum(t.suma2) suma2,
+                         min(t.KVb) KVb,
+                         sum(t.sumb1) sumb1,
+                         sum(t.sumb2) sumb2
+                    from (select forex.get_forextype3(x.DEAL_TAG) kod,
+                                 nvl(x.swap_tag, x.deal_tag) tag1,
+                                 x.dat,
+                                 x.ntik,
+                                 x.rnk,
+                                 least(x.dat_a, x.dat_b) dat_M,
+                                 greatest(x.dat_a, x.dat_b) dat_X,
+                                 decode(nvl(x.swap_tag, x.deal_tag), x.deal_tag, x.KVA, x.KVB) KVA,
+                                 decode(nvl(x.swap_tag, x.deal_tag), x.deal_tag, x.suma, 0) suma1,
+                                 decode(nvl(x.swap_tag, x.deal_tag), x.deal_tag, 0, x.sumb) suma2,
+                                 decode(nvl(x.swap_tag, x.deal_tag), x.deal_tag, x.KVB, x.KVA) KVb,
+                                 decode(nvl(x.swap_tag, x.deal_tag), x.deal_tag, x.sumb, 0) sumb1,
+                                 decode(nvl(x.swap_tag, x.deal_tag), x.deal_tag, 0, x.suma) sumb2
+                            from fx_deal x
+                           where NVL(l_tag, 0) in (0, x.swap_tag, x.deal_tag)
+                             and greatest(x.dat_a, x.dat_b) >= l_dat1
+                             and least(x.dat_a, x.dat_b) <= l_dat2) t
+                   where t.kod like '_.SWAP\_%' escape '\' or t.kod = 'FORWARD'
+                   group by upper(substr(case instr(t.kod, '_')
+                                        when 0 then t.kod
+                                        else substr(t.kod, 1, instr(t.kod, '_')-1)
+                                      end, 1, 9)), t.tag1, t.dat, t.ntik, t.rnk;
 
   -----------------------------------------------
   CURSOR C_35 IS select X.kod, x.deal_tag,  x.dat, x.ntik,  x.rnk,  x.dat_a, x.dat_b, x.KVA,  x.suma, x.kvb, x.sumb, x.ref
@@ -110,9 +152,14 @@ begin
   else  RETURN ;
   end if;
   LOOP
-     If    p_mode = 34 then FETCH C_34 into Z_KOD, Z_tag1, Z_noga, Z_dat, Z_ntik, Z_rnk, Z_dat_M, Z_dat_X, Z_KVA, Z_suma1, Z_suma2, Z_KVb, Z_sumb1, Z_sumb2;  EXIT WHEN c_34%NOTFOUND;
-     ElsIf p_mode = 35 then FETCH C_35 into Z_KOD, Z_tag1,         Z_dat, Z_ntik, Z_rnk, Z_dat_M, Z_dat_X, Z_KVA, Z_suma1,          Z_KVb, Z_sumb1, z_Ref  ;  EXIT WHEN c_35%NOTFOUND;
-     else  RETURN ;
+      If    p_mode = 34 then
+        FETCH C_34 into Z_KOD, Z_tag1, /*Z_noga, */Z_dat, Z_ntik, Z_rnk, Z_dat_M, Z_dat_X, Z_KVA, Z_suma1, Z_suma2, Z_KVb, Z_sumb1, Z_sumb2;
+        EXIT WHEN c_34%NOTFOUND;
+      ElsIf p_mode = 35 then
+        FETCH C_35 into Z_KOD, Z_tag1,             Z_dat, Z_ntik, Z_rnk, Z_dat_M, Z_dat_X, Z_KVA, Z_suma1,          Z_KVb, Z_sumb1, z_Ref  ;
+        EXIT WHEN c_35%NOTFOUND;
+      else
+        RETURN ;
      end if;
 
      fl_3800 := 0 ;   tmp.G14 := 0 ;    tmp.G15 := 0 ;  tmp.G16 := 0 ;   tmp.G17 := 0 ;   tmp.G18 := 0 ;
@@ -149,21 +196,51 @@ begin
      If fl_3800 = 1    then
         If p_mode = 34 then
 
-           If z_noga > 1 then tmp.G13 := fx_pvp ( z_tag1,  l_dat1,  l_dat2 ) ; else   tmp.G13 := 0 ; end if;
+            --COBUMMFO-8184 MDom 2018.10.26 закоментував та написав нову перевірку нижче (виправлення пункту 4)
+            --If z_noga > 1 then tmp.G13 := fx_pvp ( z_tag1,  l_dat1,  l_dat2 ) ; else   tmp.G13 := 0 ; end if;
+            if z_kod like '_.SWAP' then
+              tmp.G13 := fx_pvp(z_tag1, l_dat1, l_dat2);
+            else
+              tmp.G13 := 0;
+            end if;
 
-           insert into tmp_ani34 (B,E,G01,G02,G03,G04,G05,G06 ,G07 ,G08 ,G08a,G08b,G09,G10,G11,G12,G13,G14,G15,G16,G17,G18,G19,G20,G21,G22)
-           select l_dat1, l_dat2, decode(z_noga,1,'Форвард', 2,'Вал.своп','Депо-своп') G01, z_tag1 G02, z_ntik g03, z_rnk g04, substr(nmk,1,70) g05,
-                  z_dat g06,      decode(z_noga,1,null,z_dat_m) g07, z_dat_X g08, z_KVA g08a, z_KVb g08b,
-                  decode(z_noga,1,null   ,z_suma1)/100 g09,     decode(z_noga,1,null   ,z_sumb1)/100 g10,
-                  decode(z_noga,1,z_suma1,z_suma2)/100 g11,     decode(z_noga,1,z_sumb1,z_sumb2)/100 g12,
-                  tmp.G13/100, tmp.G14/100, tmp.G15/100, tmp.G16/100, tmp.G17/100, tmp.G18/100,
-                 (tmp.G13+tmp.G14+tmp.G16+tmp.G17) /100, (tmp.G15+tmp.G18)   /100,
-                 (tmp.G13+tmp.G14+tmp.G15+tmp.G16+tmp.G17+tmp.G18)/100,
+            insert into tmp_ani34(B, E, G01, G02, G03, G04, G05, G06, G07, G08, G08a, G08b, G09, G10, G11, G12, G13, G14, G15, G16, G17, G18, G19, G20, G21, G22)
+            select l_dat1,
+                   l_dat2,
+                   --COBUMMFO-8184 MDom 2018.10.25 закоментував та написав новий decode нижче (виправлення пункту 1)
+                   --decode(z_noga, 1, 'Форвард', 2, 'Вал.своп', 'Депо-своп') G01,
+                   decode(z_kod, 'FORWARD', 'Форвард', 'V.SWAP', 'Вал.своп', 'D.SWAP', 'Депо-своп') G01,
+                   z_tag1 G02,
+                   z_ntik g03,
+                   z_rnk g04,
+                   substr(nmk, 1, 70) g05,
+                   z_dat g06,
+                   --COBUMMFO-8184 MDom 2018.10.25 закоментував та написав новий decode нижче тому що перевірка через z_noga не точна
+                   --decode(z_noga, 1, null, z_dat_m) g07,
+                   decode(z_kod, 'FORWARD', null, z_dat_m) g07,
+                   z_dat_X g08,
+                   z_KVA g08a,
+                   z_KVb g08b,
+                   --COBUMMFO-8184 MDom 2018.10.25 закоментував та написав нові decode нижче (виправлення пунктів 2, 3)
+                   --decode(z_noga, 1, null, z_suma1)/100 g09, decode(z_noga, 1, null, z_sumb1)/100 g10,
+                   --decode(z_noga, 1, z_suma1, z_suma2)/100 g11, decode(z_noga, 1, z_sumb1, z_sumb2)/100 g12,
+                   decode(z_kod, 'FORWARD', null, z_suma1)/100 g09,
+                   decode(z_kod, 'FORWARD', null, z_sumb1)/100 g10,
+                   decode(z_kod, 'FORWARD', z_suma1, z_suma2)/100 g11,
+                   decode(z_kod, 'FORWARD', z_sumb1, z_sumb2)/100 g12,
+                   tmp.G13/100,
+                   tmp.G14/100,
+                   tmp.G15/100,
+                   tmp.G16/100,
+                   tmp.G17/100,
+                   tmp.G18/100,
+                   (tmp.G13+tmp.G14+tmp.G16+tmp.G17)/100,
+                   (tmp.G15+tmp.G18)/100,
+                   (tmp.G13+tmp.G14+tmp.G15+tmp.G16+tmp.G17+tmp.G18)/100,
                   CASE WHEN z_dat_X <= l_dat2 THEN 'Завершена' ELSE 'Відкрита' END g22
-           from customer where rnk = Z_rnk ;
+              from customer where rnk = Z_rnk ;
 
         elsIf p_mode = 35 then
-
 
            insert into tmp_ani34 (B,E,G01,G02,G03,G04,G05,G06,G07, G08,G08a,G08b, G11,G12,  G14,G15, G19, G22, g13 )
            select l_dat1, l_dat2 , z_kod G01, z_tag1 G02, z_ntik g03, z_rnk g04, substr(nmk,1,70) g05, z_dat g06,
