b21d1bb5e3040c0a4a5d1541dda38ab680336a5e
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-01 08:11:21 +0000

    #COBUMMFO-9451# Коментар:

--- sql/mmfo/bars/Table/zp_payroll.sql
+++ sql/mmfo/bars/Table/zp_payroll.sql
@@ -1,7 +1,11 @@
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /sql/bars/table/zp_payroll.sql =========*** Run *** 
+PROMPT ===================================================================================== 
+
 exec bars.bpa.alter_policy_info( 'ZP_PAYROLL', 'WHOLE' , null, null, null, null ); 
-/
+
 exec bars.bpa.alter_policy_info( 'ZP_PAYROLL', 'FILIAL', 'M', 'M', 'M', 'M' );
-/
+
 begin execute immediate'create table bars.zp_payroll  (
    id            number                  constraint cc_zp_payroll_id not null,
    rnk           number                                              not null, 
@@ -78,7 +82,7 @@ begin
 end;
 /
 begin
-    execute immediate 'alter table zp_payroll add constraint cc_zp_payroll_source   check (source in (1,2,3,4,5))';
+    execute immediate 'alter table zp_payroll add constraint cc_zp_payroll_source   check (source in (1,2,3,4,5,6))';
  exception when others then 
     if sqlcode = -2264 then null; else raise; 
     end if; 
@@ -90,7 +94,34 @@ comment on table  bars.ZP_PAYROLL is 'ЗП відомості';
 comment on column bars.ZP_PAYROLL.id is 'id відомості';
 comment on column bars.ZP_PAYROLL.source is 'Джерело відомості (1-ручне введення/2-імпорт файлу/3-клонування минулої відомості/4-Змішаний тип )';
 comment on column bars.ZP_PAYROLL.payroll_num is 'Номер відомості';
-/
-grant select,delete,update,insert on bars.ZP_PAYROLL to bars_access_defrole;
+
+PROMPT ***  Change type on column zp_payroll.sign raw(2000) -> clob ***
+declare
+  l_data_type varchar2(30);
+begin
+  select data_type 
+  into l_data_type 
+  from all_tab_columns 
+  where owner = 'BARS' and table_name = 'ZP_PAYROLL' and column_name = 'SIGN';
+  
+  if l_data_type in ('RAW') then
+    execute immediate 'alter table bars.zp_payroll add sign1 clob';
+    execute immediate 'update bars.zp_payroll set sign1 = sign';
+    execute immediate 'alter table bars.zp_payroll rename column sign to sign2';
+    execute immediate 'alter table bars.zp_payroll rename column sign1 to sign';
+    execute immediate 'alter table bars.zp_payroll drop column sign2';
+  end if;
+exception
+  when no_data_found then
+    null;
+  when others then
+    raise;
+end;
 /
 
+grant select,delete,update,insert on bars.ZP_PAYROLL to bars_access_defrole;
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /sql/bars/table/zp_payroll.sql =========*** End *** 
+PROMPT ===================================================================================== 
