5d81164dd3e612db1c23d9459d27327a88d7b16c
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-09 14:28:45 +0000

    #COBUMMFO-8332# Коментар:

--- sql/mmfo/bars/View/v_nbur_i6x.sql
+++ sql/mmfo/bars/View/v_nbur_i6x.sql
@@ -27,7 +27,7 @@ select
           , NBUR_LST_FILES v
           , table( XMLSequence( XMLType( v.FILE_BODY ).extract('/NBUSTATREPORT/DATA') ) ) t
    where  f.ID        = v.FILE_ID
-          and f.FILE_CODE = 'I6X'
+          and f.FILE_CODE = '#I6'
           and f.FILE_FMT  = 'XML'
           and v.FILE_STATUS IN ( 'FINISHED', 'BLOCKED');
            
