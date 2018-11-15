e03494e00d1c6e77496d31e9ab7bf1f649169b5c
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-15 14:22:43 +0000

    #COBUMMFO-10035# Коментар: MERGE: COBUMMFO-10035 Анна Крахмальова

--- web/mmfo/barsroot/App_Code/Areas/Payreg/Models/RegularSbonFreeOrder.cs
+++ web/mmfo/barsroot/App_Code/Areas/Payreg/Models/RegularSbonFreeOrder.cs
@@ -4,7 +4,7 @@ namespace BarsWeb.Areas.Payreg.Models
     public class RegularSbonFreeOrder
     {
         public int? Id { get; set; }
-        public int PayerAccountId { get; set; }
+        public long PayerAccountId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime? StopDate { get; set; }
         public int PaymentFrequency { get; set; }
