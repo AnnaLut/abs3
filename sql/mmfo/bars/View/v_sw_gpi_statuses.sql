f2db52acc46c95a8d75bf1787d62d28026194101
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-28 15:22:57 +0000

    #COBUMMFO-9988# Коментар:

--- sql/mmfo/bars/View/v_sw_gpi_statuses.sql
+++ sql/mmfo/bars/View/v_sw_gpi_statuses.sql
@@ -1,33 +1,65 @@
-create or replace view v_sw_gpi_statuses
-as
-select q.ref, 
-       j.mt as mt103,
-       j.io_ind as io_ind_103,
-       q.swref as swref_103,
-       j.date_in as date_input_103,
-       j.vdate as vdate_103,
-       j.sender as sender_103,
-       j.receiver as receiver_103,
-       j.payer as payer_103,
-       j.payee as payee_103,
-       j.amount/100 as amount,
-       j.currency,
-       j.sti,
-       j.uetr,
-       q.swref_199,
-       j2.mt as mt_199,
-       j2.io_ind as io_ind_199,
-       j2.date_in as date_input_199,
-       j2.sender as sender_199,
-       j2.receiver as receiver_199,
-       s.value as status_code,
-       s.description as status_description 
-        from sw_oper_queue q
-            inner join sw_journal j on j.swref=q.swref
-            inner join sw_statuses s on q.status=s.id 
-            left join oper o on o.ref=q.ref
-            left join sw_journal j2 on j2.swref=q.swref_199
-         order by q.swref desc, q.swref_199 desc;
+-- Start of DDL Script for View BARS.V_SW_GPI_STATUSES
+-- Generated 28.11.2018 16:20:58 from BARS@COBUSUPABS_DEV_MMFO_DB
+
+CREATE OR REPLACE VIEW v_sw_gpi_statuses (
+   ref,
+   mt103,
+   io_ind_103,
+   swref_103,
+   date_input_103,
+   date_output_103,
+   vdate_103,
+   sender_103,
+   receiver_103,
+   payer_103,
+   payee_103,
+   sender_account,
+   amount,
+   currency,
+   sti,
+   uetr,
+   status_code,
+   status_description )
+AS
+SELECT   q.REF,
+         j.mt AS mt103,
+         j.io_ind AS io_ind_103,
+         q.swref AS swref_103,
+         j.date_in AS date_input_103,
+         j.date_out AS date_output_103,
+         j.vdate AS vdate_103,
+         j.sender AS sender_103,
+         j.receiver AS receiver_103,
+         j.payer AS payer_103,
+         j.payee AS payee_103,
+         (SELECT REGEXP_replace (VALUE, '(/*)([^[:cntrl:]]+)(.*)' ,'\2',1,1,'n')
+          FROM   sw_operw w
+          WHERE  w.swref = q.swref AND w.tag = 50)
+             sender_account,
+         j.amount / 100 AS amount,
+         j.currency,
+         j.sti,
+         j.uetr,
+
+         s.VALUE AS status_code,
+         s.description AS status_description
+FROM     sw_oper_queue q
+         INNER JOIN sw_journal j
+             ON j.swref = q.swref
+         INNER JOIN sw_statuses s
+             ON q.status = s.id
+         LEFT JOIN oper o
+             ON o.REF = q.REF
+---  left join sw_journal j2 on j2.swref=q.swref_199
+
+WHERE    q.swref_199 = (SELECT MAX (swref_199)
+                        FROM   sw_oper_queue qq
+                        WHERE  qq.swref = q.swref)
 /
-grant select on v_sw_gpi_statuses to bars_access_defrole
-/            
+
+-- Grants for View
+GRANT SELECT ON v_sw_gpi_statuses TO bars_access_defrole
+/
+
+-- End of DDL Script for View BARS.V_SW_GPI_STATUSES
+
