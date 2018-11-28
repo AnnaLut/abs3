d5b0b984a91ac1d249473472dac901d35a1e015d
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-28 15:21:23 +0000

    #COBUMMFO-9122# Коментар:

---
+++ sql/mmfo/bars/View/v_bpk_proect_only.sql
@@ -0,0 +1,30 @@
+
+
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/BARS/View/V_BPK_PROECT_ONLY.sql ====*** Run *** =
+PROMPT ===================================================================================== 
+
+
+PROMPT *** Create  view V_BPK_PROECT_ONLY ***
+
+  CREATE OR REPLACE FORCE VIEW BARS.V_BPK_PROECT_ONLY ("ID", "NAME", "OKPO", "PRODUCT_CODE", "PRODUCT_NAME", "GRP_CODE") AS 
+/*  select -1, '*Власна картка', null, null, null, g.code
+  from w4_product_groups g
+ where code <> 'SALARY'
+ union all */
+select b.id, b.name, b.okpo, p.code, p.name, p.grp_code
+  from bpk_proect b, w4_product p
+ where b.product_code = p.code
+   and p.grp_code = 'SALARY'
+   and nvl(b.used_w4,0) = 1;
+
+PROMPT *** Create  grants  V_BPK_PROECT_ONLY ***
+grant SELECT                                                                 on V_BPK_PROECT_ONLY    to BARSREADER_ROLE;
+grant SELECT                                                                 on V_BPK_PROECT_ONLY    to BARS_ACCESS_DEFROLE;
+grant SELECT                                                                 on V_BPK_PROECT_ONLY    to UPLD;
+
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/BARS/View/V_BPK_PROECT_ONLY.sql ===*** End *** =
+PROMPT ===================================================================================== 
