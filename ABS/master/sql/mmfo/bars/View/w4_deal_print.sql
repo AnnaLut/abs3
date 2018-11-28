550f843f35be8f497793b229b131e10f1f77b4f0
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-11-28 15:21:23 +0000

    #COBUMMFO-9122# Коментар:

--- ABS/master/sql/mmfo/bars/View/w4_deal_print.sql
+++ ABS/master/sql/mmfo/bars/View/w4_deal_print.sql
@@ -8,7 +8,7 @@ PROMPT =========================================================================
 PROMPT *** Create  view W4_DEAL_PRINT ***
 
   CREATE OR REPLACE FORCE VIEW BARS.W4_DEAL_PRINT ("ND", "BRANCH", "CARD_CODE", "ACC_ACC", "ACC_NLS", "ACC_KV", "ACC_LCV", "ACC_OB22", "ACC_TIP", "ACC_TIPNAME", "ACC_OST", "ACC_DAOS", "ACC_DAZS", "CUST_RNK", "CUST_NAME", "CUST_OKPO", "CUST_TYPE", "GRP_CODE", "PROECT_OKPO", "DOC_ID") AS 
-  SELECT nd,
+SELECT nd,
           branch,
           card_code,
           acc acc_acc,
@@ -26,7 +26,8 @@ PROMPT *** Create  view W4_DEAL_PRINT ***
           okpo cust_okpo,
           custtype cust_type,
           grp_code,
-          NVL (proect_okpo, grp_code) proect_okpo,
+          --NVL (proect_okpo, grp_code) proect_okpo,
+          proect_okpo,
              'id in (select doc_id from w4_product_doc where nvl(d_close, sysdate)>=sysdate and grp_code='''
           || grp_code
           || ''' and doc_id not like ''%MIGR%'')'
@@ -37,51 +38,40 @@ PROMPT *** Create  view W4_DEAL_PRINT ***
                    ' or id in (''ACC_BPK_ANKETA_KK'',''ACC_1_BPK_ZAYAVA_NEW_DKBO_KK'',''ACC_2_BPK_ZAYAVA_NEW_DKBO_KK'')'
              END
              doc_id
-     FROM (SELECT o.nd,
-                  a.branch,
-                  o.card_code,
-                  a.acc,
-                  a.nls,
-                  a.kv,
-                  t.lcv,
-                  a.ob22,
-                  a.tip,
-                  s.name,
-                  a.ostc / POWER (10, 2) ost,
-                  a.daos,
-                  a.dazs,
-                  c.rnk,
-                  UPPER (c.nmk) nmk,
-                  c.okpo,
-                  c.custtype,
-                  wp.grp_code,
-                  (SELECT b.okpo
-                     FROM accountsw w, bpk_proect b
-                    WHERE     w.acc = a.acc
-                          AND w.tag = 'PK_PRCT'
-                          AND w.VALUE = TO_CHAR (b.id))
-                     proect_okpo
-             FROM w4_acc o,
-                  accounts a,
-                  customer c,
-                  tips s,
-                  tabval$global t,
-                  w4_card wc,
-                  w4_product wp,
-                  w4_nbs_ob22 n
-            WHERE     o.acc_pk = a.acc
-                  AND a.rnk = c.rnk
-                  AND a.tip = s.tip
-                  AND a.kv = t.kv
-                  AND o.card_code = wc.code
-                  AND wc.product_code = wp.code
-                  AND a.tip LIKE 'W4%'
-                  AND s.tip LIKE 'W4%'
-                  AND SUBSTR (a.nls, 1, 4) = n.nbs
-                  AND a.ob22 = n.ob22
-                  AND a.tip = n.tip
-                  AND a.branch LIKE
-                         SYS_CONTEXT ('bars_context', 'user_branch_mask'));
+     FROM (
+    SELECT o.nd,
+       a.branch,
+       o.card_code,
+       a.acc,
+       a.nls,
+       a.kv,
+       t.lcv,
+       a.ob22,
+       a.tip,
+       s.name,
+       a.ostc / POWER (10, 2) ost,
+       a.daos,
+       a.dazs,
+       c.rnk,
+       UPPER (c.nmk) nmk,
+       c.okpo,
+       c.custtype
+       ,wc.product_code
+       ,(select wp.grp_code from w4_product wp where wp.code=wc.product_code and wp.kf=a.kf )grp_code
+       ,b.okpo proect_okpo
+       --,null proect_okpo
+   FROM accountsw w
+     inner join bpk_proect b on to_char(b.id)=w.VALUE --and b.okpo='19121522'
+     inner join w4_acc o on o.acc_pk=w.acc
+     inner join accounts a on a.acc=w.acc  and a.tip  LIKE 'W4%' AND a.branch LIKE SYS_CONTEXT ('bars_context', 'user_branch_mask')
+     inner join tabval$global t on t.kv=a.kv
+     inner join tips s on s.tip=a.tip
+     inner join customer c on c.rnk=a.rnk
+     inner join w4_nbs_ob22 n on n.nbs=a.nbs and  n.ob22=a.ob22
+     inner join w4_card wc on wc.code=o.card_code and wc.kf=a.kf  and wc.date_close is null
+WHERE
+    w.tag = 'PK_PRCT'
+          );
 
 PROMPT *** Create  grants  W4_DEAL_PRINT ***
 grant SELECT                                                                 on W4_DEAL_PRINT   to BARSREADER_ROLE;
