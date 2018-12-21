ace79a236b1e478f1c15fd8c2bfed0c165768692
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2018-12-21 12:36:09 +0000

    #COBUMMFO-10272# Коментар: MERGE COBUMMFO-102729 to master (Микола Домалєга)

--- sql/mmfo/bars/Procedure/frx_populate_tmp.sql
+++ sql/mmfo/bars/Procedure/frx_populate_tmp.sql
@@ -10,164 +10,134 @@ PROMPT *** Create  procedure FRX_POPULATE_TMP ***
   CREATE OR REPLACE PROCEDURE BARS.FRX_POPULATE_TMP (p_dat date)
 is
 begin
-delete TMP_SWAP_ARC;
-insert into  TMP_SWAP_ARC
-select
-DAT,
-NTIK,
-RNK,
-nmk,
-KVA,
-nlsa,
-DAT_A,
-SUMA,
-KVB,
-nlsb,
-DAT_B,
-SUMB,
-PVX1,
-PVX,
-IRAE,
-IRBE,
-case when irAe>0 and irBe>0
-     then
-     (gl.p_icurval( kva, FOREX.PVXR (nvl(swap_tag,deal_tag),deal_tag,b,kva,irAe),b))/100
-     else null
-end pvx1a,
-case when irAe>0 and irBe>0
-     then
-     (gl.p_icurval( kvb, FOREX.PVXR (nvl(swap_tag,deal_tag),deal_tag,b,kvb,irbe),b))/100
-     else null
-end pvx1b,
-REF,
-NVL (SWAP_TAG, 0) SWAP_TAG, --переніс NVL на уровени више п.5
-DEAL_TAG,
-KODF,
-KOD,
-BVQ,
-BVQA,
-BVQB,
-b
-from
-(
-   SELECT
-          nlsa,
-          nlsb,
-          nmk,
-          B,
-          dat,
-          ntik,
-          swap_tag,  --переніс NVL на уровень више п.5
-          deal_tag,
-          rnk,
-          REF,
-          PVX1,
-          sos,
-          BVQA,
-          BVQB,
-          BVQA - BVQB BVQ,
-          s.KOD,
-          dat_a,
-          kva,
-          SUMA,
-          TERMA,
-          irA,
-          ROUND (irAe, 4) irAe,
-          dat_b,
-          kvb,
-          SUMB,
-          TERMB,
-          irB,
-          ROUND (irBe, 4) irBe,
-          CASE
-             WHEN s.KOD LIKE '%TOD%'
-             THEN
-                (0)
-             WHEN s.KOD LIKE '%SPOT%' AND S.swap_tag IS NULL
-             THEN
-                (BVQA - BVQB)
-             WHEN s.KOD LIKE '%SPOT%' AND S.swap_tag IS NOT NULL
-             THEN
-			 --swap_spot на дату
-                (SELECT   SUM (
-                               gl.p_icurval (kva, suma, B)
-                             - gl.p_icurval (kvb, sumb, B))
-                        / 100
-                   FROM fx_deal
-                  WHERE     swap_tag = S.swap_tag
-                        AND deal_tag <> S.swap_tag
-                        AND dat_a >= B)
-             ELSE
-                (FOREX.PVX (NVL (S.swap_tag, S.deal_tag), B) / 100)
-          END
-             PVX,
-          CASE
-             WHEN S.swap_tag IS NULL THEN ''
-             WHEN S.ira + S.irb = 0 THEN 'ВАЛ'
-             ELSE 'ДЕПО'
-          END
-             KODF
-     FROM (SELECT
-                  acca.nls nlsa,
-                  accb.nls nlsb,
-                  nmk,
-                  p_dat b,
-                  x.dat,
-                  x.ntik,
-                  x.swap_tag,
-                  x.deal_tag,
-                  x.rnk,
-                  x.REF,
-                  SUMP / 100 PVX1,
-                  NVL (x.sos, 10) sos,
-                  x.dat_a,
-                  x.kva,
-                  x.suma / 100 SUMA,
-                  ACRN.FPROCN (x.acc9a, 0, x.dat) ira,
-                  (x.dat_A - p_dat) TERMA,
-                  FOREX.IR_MB (p_dat, x.KVA, (x.dat_a - p_dat)) irAe,
-                  x.dat_b,
-                  x.kvb,
-                  x.sumb / 100 SUMB,
-                  ACRN.FPROCN (x.acc9b, 1, x.dat) irb,
-                  (x.dat_B - p_dat) TERMB,
-                  FOREX.IR_MB (p_dat, x.KVB, (x.dat_B - p_dat)) irBe,
-                  gl.p_icurval (x.kva, x.suma, p_dat) / 100 BVQA,
-                  gl.p_icurval (x.kvb, x.sumb, p_dat) / 100 BVQB,
-                  SUBSTR (FOREX.get_forextype3 (x.deal_tag), 1, 17) KOD  --функция возвращает V.SWAP__ D.SWAP__ длана на 2 знака больше ??
-             FROM
-                  fx_deal x,
-                  accounts acca,
-                  accounts accb,
-                  customer c--, V_SFDAT d
-            WHERE
-                acca.acc=x.acc9a
-            and accb.acc=x.acc9b
-            and
-            c.rnk=X.RNK
-            and
-              EXISTS
-                         (SELECT 1
-                            FROM oper
-                           WHERE REF = x.REF AND sos > 0)
-            -- убрали условие AND NVL (x.sos, 10) < 15  п.1,2
-            AND (FOREX.get_forextype3 (x.deal_tag) LIKE '%SWAP%EN'  --функция возвращает V.SWAP__ D.SWAP__ ??
-                 AND x.deal_tag <> x.swap_tag
-                  OR x.swap_tag IS NULL
-                )
-          ) S
-)
-where dat<=p_dat and dat_a>p_dat and dat_b>p_dat
-;
-commit;
+  delete TMP_SWAP_ARC;
+  insert into TMP_SWAP_ARC
+  select DAT,
+         NTIK,
+         RNK,
+         nmk,
+         KVA,
+         nlsa,
+         DAT_A,
+         SUMA,
+         KVB,
+         nlsb,
+         DAT_B,
+         SUMB,
+         PVX1,
+         CASE
+           WHEN KOD LIKE '%TOD%' THEN
+             0
+           WHEN KOD LIKE '%SPOT%' AND swap_tag IS NULL THEN
+             BVQA - BVQB
+           WHEN KOD LIKE '%SPOT%' AND swap_tag IS NOT NULL 
+             and greatest(dat_a, dat_b) < dat_next_u(dat, 2) --COBUMMFO-10272 MDom 2018.12.11 Для відсіювання овернайтів
+           THEN --swap_spot на дату
+             (SELECT SUM(gl.p_icurval(kva, suma, B) - gl.p_icurval(kvb, sumb, B))/100
+                FROM fx_deal fx
+               WHERE swap_tag = s.swap_tag
+                 AND deal_tag <> s.swap_tag
+                 AND dat_a >= B)
+           ELSE
+             FOREX.PVX(coalesce(swap_tag, deal_tag), B)/100
+         END PVX,
+         IRAE,
+         IRBE,
+         /*case
+           when irAe > 0 and irBe > 0 then
+             gl.p_icurval(kva, FOREX.PVXR(coalesce(swap_tag, deal_tag), deal_tag, b, kva, irAe), b)/100
+           else null
+         end pvx1a,
+         case
+           when irAe > 0 and irBe > 0 then
+             gl.p_icurval(kvb, FOREX.PVXR(coalesce(swap_tag, deal_tag), deal_tag, b, kvb, irbe), b)/100
+           else null
+         end pvx1b,*/
+         --COBUMMFO-10289 MDom 2018.12.11 Переробив попередній закоментований шматок для відсіювання овернайтів
+         case
+           when kod like '%SWAP%' and dat_a = dat_b and dat_a = dat_next_u(dat, 1) then BVQA
+           else
+             case
+               when irAe > 0 and irBe > 0 then
+                 gl.p_icurval(kva, FOREX.PVXR(coalesce(swap_tag, deal_tag), deal_tag, b, kva, irAe), b)/100
+               else null
+             end
+         end pvx1a,
+         case
+           when kod like '%SWAP%' and dat_a = dat_b and dat_a = dat_next_u(dat, 1) then BVQB
+           else
+             case
+               when irAe > 0 and irBe > 0 then
+                 gl.p_icurval(kvb, FOREX.PVXR(coalesce(swap_tag, deal_tag), deal_tag, b, kvb, irbe), b)/100
+               else null
+             end
+         end pvx1b,
+         ----
+         REF,
+         coalesce(SWAP_TAG, 0) SWAP_TAG, --переніс coalesce на рівень вище п.5
+         DEAL_TAG,
+         CASE
+           WHEN swap_tag IS NULL THEN ''
+           WHEN ira + irb = 0 THEN 'ВАЛ'
+           ELSE 'ДЕПО'
+         END KODF,
+         KOD,
+         BVQA - BVQB BVQ,
+         BVQA,
+         BVQB,
+         b
+    from (SELECT acca.nls nlsa,
+                 accb.nls nlsb,
+                 nmk,
+                 p_dat b,
+                 x.dat,
+                 x.ntik,
+                 x.swap_tag,
+                 x.deal_tag,
+                 x.rnk,
+                 x.REF,
+                 SUMP/100 PVX1,
+                 --coalesce(x.sos, 10) sos,
+                 x.dat_a,
+                 x.kva,
+                 x.suma/100 SUMA,
+                 ACRN.FPROCN(x.acc9a, 0, x.dat) ira,
+                 --(x.dat_A - p_dat) TERMA,
+                 ROUND(FOREX.IR_MB(p_dat, x.KVA, (x.dat_a - p_dat)), 4) irAe,
+                 x.dat_b,
+                 x.kvb,
+                 x.sumb/100 SUMB,
+                 ACRN.FPROCN(x.acc9b, 1, x.dat) irb,
+                 --(x.dat_B - p_dat) TERMB,
+                 ROUND(FOREX.IR_MB(p_dat, x.KVB, (x.dat_B - p_dat)), 4) irBe,
+                 gl.p_icurval(x.kva, x.suma, p_dat)/100 BVQA,
+                 gl.p_icurval(x.kvb, x.sumb, p_dat)/100 BVQB,
+                 SUBSTR(FOREX.get_forextype3(x.deal_tag), 1, 17) KOD  --функция возвращает V.SWAP__ D.SWAP__ длина на 2 знака больше?
+            FROM fx_deal x
+            JOIN accounts acca on acca.acc = x.acc9a
+            JOIN accounts accb on accb.acc = x.acc9b
+            JOIN customer c on c.rnk = X.RNK
+                 --, V_SFDAT d
+           WHERE EXISTS (SELECT 1
+                           FROM oper
+                          WHERE REF = x.REF
+                            AND sos > 0)
+             -- убрали условие AND coalesce (x.sos, 10) < 15 п.1,2
+             AND (FOREX.get_forextype3(x.deal_tag) LIKE '%SWAP%EN' --функция возвращает V.SWAP__ D.SWAP__?
+             AND x.deal_tag <> x.swap_tag OR x.swap_tag IS NULL)) s
+   where s.dat <= p_dat
+     and s.dat_a > p_dat
+     and s.dat_b > p_dat;
+
+  commit;
 
 end;
 /
 show err;
 
 PROMPT *** Create  grants  FRX_POPULATE_TMP ***
-grant EXECUTE                                                                on FRX_POPULATE_TMP to BARS_ACCESS_DEFROLE;
 grant EXECUTE                                                                on FRX_POPULATE_TMP to START1;
+grant EXECUTE                                                                on FRX_POPULATE_TMP to BARS_ACCESS_DEFROLE;
 
 
 
--- sql/mmfo/bars/View/v_swap.sql
+++ sql/mmfo/bars/View/v_swap.sql
@@ -8,32 +8,97 @@ PROMPT =========================================================================
 PROMPT *** Create  view V_SWAP ***
 
   CREATE OR REPLACE FORCE VIEW BARS.V_SWAP ("B", "DAT", "NTIK", "SWAP_TAG", "DEAL_TAG", "RNK", "NMK", "REF", "PVX1", "OTM", "OTM1", "DEL1", "DELX", "SOS", "BVQA", "BVQB", "BVQ", "KOD", "DAT_A", "KVA", "SUMA", "TERMA", "IRA", "IRAE", "DAT_B", "KVB", "SUMB", "TERMB", "IRB", "IRBE", "PVX", "KODF") AS 
-  SELECT 
-   B, dat, ntik, NVL(swap_tag,0) SWAP_TAG, deal_tag, rnk, NMK, REF, PVX1, 0 otm,  0 otm1, '?' DEL1, '?' DELX, sos, BVQA, BVQB, BVQA-BVQB BVQ, s.KOD,
-   dat_a, kva, SUMA, TERMA, irA, ROUND(irAe,4) irAe, dat_b, kvb, SUMB, TERMB, irB, ROUND(irBe,4) irBe,
-   CASE WHEN s.KOD LIKE '%TOD%'                             THEN (0)
-        WHEN s.KOD LIKE '%SPOT%' AND S.swap_tag IS     NULL THEN (BVQA - BVQB)
-        WHEN s.KOD LIKE '%SPOT%' AND S.swap_tag IS NOT NULL THEN (SELECT SUM(gl.p_icurval (kva,suma,B)-gl.p_icurval (kvb,sumb,B))/100 FROM fx_deal WHERE swap_tag=S.swap_tag AND deal_tag<>S.swap_tag AND dat_a>=B )
-        ELSE                                                     (FOREX.PVX (NVL (S.swap_tag,S.deal_tag),B)/100)
-   END  PVX,
-   CASE WHEN S.swap_tag IS NULL THEN ''
-        WHEN S.ira + S.irb = 0 THEN 'ÂÀË'
-        ELSE 'ÄÅÏÎ'
-    END KODF
-FROM (SELECT d.B, x.dat, x.ntik, x.swap_tag, x.deal_tag, x.rnk, c.nmk, x.REF, SUMP/100 PVX1, NVL(x.sos,10) sos, x.dat_a, x.kva, x.suma/100 SUMA, ACRN.FPROCN (x.acc9a,0,x.dat) ira, (x.dat_A-d.B) TERMA,
-             FOREX.IR_MB(d.B,x.KVA,(x.dat_a-d.B)) irAe, x.dat_b, x.kvb, x.sumb/100 SUMB, ACRN.FPROCN (x.acc9b,1,x.dat) irb, (x.dat_B-d.B) TERMB,
-             FOREX.IR_MB(d.B,x.KVB,(x.dat_B-d.B)) irBe, gl.p_icurval (x.kva,x.suma,d.B)/100 BVQA, gl.p_icurval(x.kvb,x.sumb,d.B)/100 BVQB, SUBSTR(FOREX.get_forextype3(x.deal_tag),1,15) KOD
-      FROM fx_deal x, V_SFDAT d, customer c
-      WHERE   EXISTS (SELECT 1 FROM oper  WHERE REF = x.REF AND sos > 0)    AND c.rnk = x.rnk   AND NVL (x.sos, 10) < 15
-        AND ( FOREX.get_forextype3 (x.deal_tag) LIKE    '%SWAP%EN' AND x.deal_tag <> x.swap_tag  OR x.swap_tag IS NULL)
-    ) S;
+  SELECT B,
+       dat,
+       ntik,
+       NVL(swap_tag, 0) SWAP_TAG,
+       deal_tag,
+       rnk,
+       NMK,
+       REF,
+       PVX1,
+       0 otm,
+       0 otm1,
+       '?' DEL1,
+       '?' DELX,
+       sos,
+       BVQA,
+       BVQB,
+       BVQA - BVQB BVQ,
+       s.KOD,
+       dat_a,
+       kva,
+       SUMA,
+       TERMA,
+       irA,
+       ROUND(irAe, 4) irAe,
+       dat_b,
+       kvb,
+       SUMB,
+       TERMB,
+       irB,
+       ROUND(irBe, 4) irBe,
+       CASE
+         WHEN s.KOD LIKE '%TOD%' THEN 0
+         WHEN s.KOD LIKE '%SPOT%' AND S.swap_tag IS NULL THEN BVQA - BVQB
+         WHEN s.KOD LIKE '%SPOT%' AND S.swap_tag IS NOT NULL
+           and greatest(dat_a, dat_b) < dat_next_u(dat, 2) --COBUMMFO-10272 MDom 2018.12.11 Äëÿ â³äñ³þâàííÿ îâåðíàéò³â
+         THEN (SELECT SUM(gl.p_icurval(kva, suma, B) - gl.p_icurval(kvb, sumb, B))/100
+                 FROM fx_deal
+                WHERE swap_tag = S.swap_tag
+                  AND deal_tag <> S.swap_tag
+                  AND dat_a >= B)
+         ELSE FOREX.PVX(NVL(S.swap_tag, S.deal_tag), B)/100
+       END PVX,
+       CASE
+         WHEN S.swap_tag IS NULL THEN ''
+         WHEN S.ira + S.irb = 0 THEN 'ÂÀË'
+         ELSE 'ÄÅÏÎ'
+       END KODF
+  FROM (SELECT d.B,
+               x.dat,
+               x.ntik,
+               x.swap_tag,
+               x.deal_tag,
+               x.rnk,
+               c.nmk,
+               x.REF,
+               SUMP/100 PVX1,
+               NVL(x.sos, 10) sos,
+               x.dat_a,
+               x.kva,
+               x.suma/100 SUMA,
+               ACRN.FPROCN(x.acc9a, 0, x.dat) ira,
+               (x.dat_A - d.B) TERMA,
+               FOREX.IR_MB(d.B, x.KVA, (x.dat_a - d.B)) irAe,
+               x.dat_b,
+               x.kvb,
+               x.sumb/100 SUMB,
+               ACRN.FPROCN(x.acc9b, 1, x.dat) irb,
+               (x.dat_B - d.B) TERMB,
+               FOREX.IR_MB(d.B, x.KVB, (x.dat_B - d.B)) irBe,
+               gl.p_icurval(x.kva, x.suma, d.B)/100 BVQA,
+               gl.p_icurval(x.kvb, x.sumb, d.B)/100 BVQB,
+               SUBSTR(FOREX.get_forextype3(x.deal_tag), 1, 15) KOD
+          FROM fx_deal x,
+               V_SFDAT d,
+               customer c
+         WHERE EXISTS (SELECT 1
+                         FROM oper
+                        WHERE REF = x.REF
+                          AND sos > 0)
+           AND c.rnk = x.rnk
+           AND NVL(x.sos, 10) < 15
+           AND (FOREX.get_forextype3(x.deal_tag) LIKE '%SWAP%EN'
+           AND x.deal_tag <> x.swap_tag OR x.swap_tag IS NULL)) S
+;
 
 PROMPT *** Create  grants  V_SWAP ***
-grant SELECT                                                                 on V_SWAP          to BARSREADER_ROLE;
+grant SELECT                                                                 on V_SWAP          to UPLD;
 grant SELECT                                                                 on V_SWAP          to BARS_ACCESS_DEFROLE;
 grant SELECT                                                                 on V_SWAP          to FOREX;
 grant SELECT                                                                 on V_SWAP          to START1;
-grant SELECT                                                                 on V_SWAP          to UPLD;
+grant SELECT                                                                 on V_SWAP          to BARSREADER_ROLE;
 
 
 
