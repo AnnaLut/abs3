af9be66975b7d4df6ff9a5c40642e372d18c43d3
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-01-24 08:14:49 +0000

    #COBUMMFO-10605# Коментар: merge cobummfo-10605 to master (В.Наконечный)

---
+++ sql/mmfo/bars/Data/fill_table/sb_zgod_2009.sql
@@ -0,0 +1,303 @@
+
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/Bars/Data/patch_data_SB_ZGOD_2009.sql =========**
+PROMPT ===================================================================================== 
+
+delete from SB_ZGOD_2009;
+
+
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8032','','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8061','','0415');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0166');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0164');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8020','02 ','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8063','','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0124');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0122');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0178');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0177');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','02 ','','8031','','0176');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','08 ','','8052','','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8060','','0414');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8001','','0431');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8020','01 ','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','08 ','','8051','','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8013','','0412');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','09 ','','8050','','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8030','','');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8013','','0307');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8013','','0405');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8000','','0413');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0069');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0070');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0071');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0072');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0073');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0074');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0075');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0076');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0078');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0370');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0371');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0372');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0373');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','10 ','','8010','','0374');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','08 ','','8053','06 ','0510');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','09 ','','8053','','0497');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (0,'8099','09 ','','8053','','0511');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0470');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0471');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0472');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0473');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0475');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0477');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0482');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0491');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0492');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0493');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0494');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0500');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0503');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0504');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0508');
+    exception when dup_val_on_index then null;
+end;
+/
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0513');
+    exception when dup_val_on_index then null;
+end;
+/
+
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0516');
+    exception when dup_val_on_index then null;
+end;
+/
+
+Begin
+   INSERT INTO SB_ZGOD_2009(DK,NBS_A,OB22_A,P080_A,NBS_B,OB22_B,P080_B) VALUES (1,'8099','01 ','','8062','','0517');
+    exception when dup_val_on_index then null;
+end;
+/
+
+
+COMMIT;
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/Bars/Data/patch_data_SB_ZGOD_2009.sql =========**
+PROMPT ===================================================================================== 
