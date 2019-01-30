c54e49fdf45096645796a7862adc34384dba48ad
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-01-30 06:56:19 +0000

    #COBUMMFO-10740# Коментар: merge cobuteller-239 to master (VPogoda, обновление процедуры деактивации теллера)

--- ABS/master/sql/mmfo/bars/Trigger/TBIR_OPER_TELLER.sql
+++ ABS/master/sql/mmfo/bars/Trigger/TBIR_OPER_TELLER.sql
@@ -1,25 +1,26 @@
 
+ 
  PROMPT ===================================================================================== 
  PROMPT *** Run *** ========== Scripts /Sql/BARS/trigger/tbir_oper_teller.sql =========*** Ru
  PROMPT ===================================================================================== 
  
   CREATE OR REPLACE TRIGGER BARS.TBIR_OPER_TELLER 
   before insert
-  on OPER 
+  on OPER
   for each row
  declare
      v_err varchar2(2000);
      v_ret integer;
-     v_sql varchar2(2000);
+--     v_sql varchar2(2000);
  begin
-      begin
-        v_sql := 'begin :1 := teller_tools.check_doc('''||:new.tt||''','||:new.s||','||nvl(:new.sq,0)||','''||:new.kv||''','||:new.ref||', '||:new.dk||', '''||:new.nlsa||''', '''||:new.nlsb||''', :2); end;';
+      v_ret := teller_tools.check_doc(:new.tt,:new.s,nvl(:new.sq,0),:new.kv,:new.ref,:new.dk,:new.nlsa,:new.nlsb, v_err);
+/*        v_sql := 'begin :1 := teller_tools.check_doc('''||:new.tt||''','||:new.s||','||nvl(:new.sq,0)||','''||:new.kv||''','||:new.ref||', '||:new.dk||', '''||:new.nlsa||''', '''||:new.nlsb||''', :2); end;';
         bars_audit.info('Teller: start check ('||v_sql||')');
         execute immediate v_sql using out v_ret, out v_err;
       exception when others then
         bars_audit.info('Teller: check error = '||sqlerrm);
         v_ret := 0;
-      end;
+      end;*/
       bars_audit.info('Teller: result check ('||v_ret||','||v_err||')');
       case v_ret
         when -1 then
@@ -33,7 +34,7 @@
         else
           null;
       end case;
-  
+
 end tbir_oper_teller;
 
 /
