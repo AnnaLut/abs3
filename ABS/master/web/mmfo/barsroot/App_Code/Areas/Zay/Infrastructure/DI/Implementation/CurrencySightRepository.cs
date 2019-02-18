91d3fb22d6946bbcc60051ce1e30cde55ff51548
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-02-18 16:27:47 +0000

    #COBUMMFO-10554# Коментар: MERGE: COBUMMFO-10554 Анна Крахмальова

--- ABS/master/web/mmfo/barsroot/App_Code/Areas/Zay/Infrastructure/DI/Implementation/CurrencySightRepository.cs
+++ ABS/master/web/mmfo/barsroot/App_Code/Areas/Zay/Infrastructure/DI/Implementation/CurrencySightRepository.cs
@@ -363,15 +363,8 @@ namespace BarsWeb.Areas.Zay.Infrastructure.Repository.DI.Implementation
         {
             StringBuilder queryBuilder = new StringBuilder()
                 .Append(@"
-                        SELECT id, mfo, mfo_name, req_id, dk, sos, decode(sos, 0, 0, 1) SOS_DECODED, kv2, lcv, dig, kv_conv,
-                                fdat, kurs_z, vdate, kurs_f, (nvl(s2, 0)/100) s2, viza, datz,
-                                rnk, nmk, cust_branch, priority, priorname, aim_name, comm, kurs_kl,
-                                (case when priorverify = 0 and viza >= 1 then 1
-                                      when priorverify = 1 and viza >= 2 then 1
-                                      else 0 
-                                 end) PRIORVERIFY_VIZA,
-                                close_type, close_type_name, state, start_time, req_type, vdate_plan, sq S2_EQV
-                        FROM v_zay
+                         SELECT *
+                        FROM v_zay42
                         WHERE dk = :p_dk AND s2 > 0 AND trunc(nvl(fdat,bankdate)) <= bankdate 
                             AND")
                 .AppendFormat((null == sos && null == visa) 
@@ -392,15 +385,8 @@ namespace BarsWeb.Areas.Zay.Infrastructure.Repository.DI.Implementation
         {
             StringBuilder queryBuilder = new StringBuilder()
                 .Append(@"
-                        SELECT id, mfo, mfo_name, req_id, dk, sos, decode(sos,0,0,1) SOS_DECODED, kv2, lcv, dig, kv_conv, 
-                                fdat, kurs_z, vdate, kurs_f, (nvl(s2, 0)/100) s2, viza, datz, 
-                                rnk, nmk, cust_branch, priority, priorname, aim_name, comm, kurs_kl,
-                                (case when priorverify = 0 and viza >= 1 then 1
-                                      when priorverify = 1 and viza >= 2 then 1
-                                      else 0 
-                                 end) PRIORVERIFY_VIZA,
-                                close_type, close_type_name, nvl(obz,0) obz, state, start_time, req_type, sq S2_EQV
-                        FROM v_zay
+                        SELECT *
+                        FROM v_zay42
                         WHERE dk = :p_dk AND s2 > 0  AND trunc(nvl(fdat,bankdate)) <= bankdate 
                             AND")
                 .AppendFormat((null == sos && null == visa)
