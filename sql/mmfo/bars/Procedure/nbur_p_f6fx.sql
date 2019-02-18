6b8c2573048f021d6abb2c11e1664aac27865834
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-02-18 10:41:20 +0000

    #COBUMMFO-9737# Коментар: MERGE: COBUMMFO-9737 to master

--- sql/mmfo/bars/Procedure/nbur_p_f6fx.sql
+++ sql/mmfo/bars/Procedure/nbur_p_f6fx.sql
@@ -13,11 +13,13 @@ CREATE OR REPLACE PROCEDURE BARS.NBUR_P_F6FX (p_kod_filii  varchar2
  DESCRIPTION :    Процедура формирования 6FX
  COPYRIGHT   :    Copyright UNITY-BARS Limited, 1999.  All Rights Reserved.
 
- VERSION     :    v.18.002    19.11.2018
+ VERSION     :    v.18.003  13.02.2018 (v.18.002    19.11.2018)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     параметры: p_report_date - отчетная дата
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+13.02.2018	виправлено помилки (мають попадати лише ті, що попадають в #D8)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
-  ver_              char(30)  := 'v.18.002  04.12.2018';
+  ver_              char(30)  := 'v.18.003  13.02.2018';
 
   c_title           constant varchar2(100 char) := $$PLSQL_UNIT || '.';
 
@@ -67,7 +69,7 @@ begin
   insert
     into NBUR_LOG_F6FX
        (REPORT_DATE, KF, NBUC, VERSION_ID, VERSION_D8, EKP,K020,K021,Q001,F084,K040,KU_1,
-    K110,K074,K140,Q020,Q003_1,Q029, FLAG_XML, DESCRIPTION,KV,CUST_ID,CUST_CODE,
+    K110,K074,K140,Q020,Q003_1,Q029, /*FLAG_XML,*/ DESCRIPTION,KV,CUST_ID,CUST_CODE,
     CUST_NAME,ND,AGRM_NUM,BEG_DT,END_DT,BRANCH)
   select p_report_date, p_kod_filii, nvl(trim(nbuc), l_nbuc), l_version_id, version_id
      , 'A6F001'             as  EKP
@@ -87,7 +89,7 @@ begin
      , Q020
      , (case Q003_1 when '000' then null else Q003_1 end)   as Q003_1
      , Q029
-     , flag_xml
+--     , flag_xml
      , DESCRIPTION
      , KV
      , CUST_ID
@@ -115,24 +117,18 @@ begin
              , v.BEG_DT
              , v.END_DT
              , v.BRANCH
-	     , decode(agg.FIELD_CODE, null,0,1) flag_xml
           from V_NBUR_#D8_dtl v
-               left join V_NBUR_#D8 agg -- в XML обираємо лише ті, що попадають в #D8
-                      on (v.REPORT_DATE=agg.REPORT_DATE
-                            and v.KF=agg.KF
-                            and v.VERSION_ID=agg.VERSION_ID
-                            and v.FIELD_CODE=agg.FIELD_CODE
-                          ) 
-               left join ( select distinct seg_02 okpo, -- K140 візмемо з першого з показників де він <> 0
-                                  FIRST_VALUE(seg_11) over (partition by seg_02 order by seg_01) as K140
-                            from V_NBUR_#D8_dtl
-               where report_date = p_report_date 
-                 and kf = p_kod_filii
-                             and seg_01 in ( '131','132','133','134','118','119', 
-                                           '121','122','123','124','125','126','127' )
-                             and seg_11 <> '0'
+           left join ( select distinct seg_02 okpo, -- K140 візмемо з першого з показників де він <> 0
+                              FIRST_VALUE(seg_11) over (partition by seg_02 order by seg_01) as K140
+                         from V_NBUR_#D8_dtl
+                        where report_date = p_report_date 
+                          and kf = p_kod_filii
+                          and seg_01 in ( '131','132','133','134','118','119', 
+                                          '121','122','123','124','125','126','127' )
+                          and seg_11 <> '0'
                          ) t1
                       on (v.seg_02 = t1.okpo)
+
          where v.report_date = p_report_date 
            and v.kf = p_kod_filii
            and v.seg_01 in ('085','050','025','055','021','041','019','010','060')
@@ -143,6 +139,40 @@ begin
                       '055' as KU_1, '021' as K074, '041' as Q003_1, 
                       '019' as Q029, '010' as Q001, '060' as Q020)
        );
+
+    commit;
+-- Розставимо флаг попадання в файл (FLAG_XML)
+
+      declare
+         type t_custid_tab is table of nbur_log_f6fx.cust_id%type index by pls_integer;
+         custid_tab t_custid_tab;
+      begin
+      -- зберемо в колекцію CUST_ID, які попадають в #D8
+            select distinct CUST_ID bulk collect into custid_tab
+              from (select d.CUST_ID, a.FIELD_CODE 
+                      from V_NBUR_#D8_dtl d
+                      left join V_NBUR_#D8 a
+                             on (d.REPORT_DATE=a.REPORT_DATE
+                                   and d.KF=a.KF
+                                   and d.VERSION_ID=a.VERSION_ID
+                                   and d.FIELD_CODE=a.FIELD_CODE
+                                   and d.FIELD_VALUE=a.FIELD_VALUE
+                                 ) 
+                     where d.REPORT_DATE = p_report_date 
+                       and d.KF= p_kod_filii 
+                       and d.seg_01 in ('010')
+                    ) 
+             where FIELD_CODE is not null;
+
+      -- проставимо флаг
+             forall indx in custid_tab.first..custid_tab.last
+                  update nbur_log_f6fx 
+                     set FLAG_XML = 1
+                   where REPORT_DATE = p_report_date 
+                     and KF= p_kod_filii 
+                     and version_id = l_version_id 
+                     and cust_id = custid_tab(indx);
+      end;
 
     commit;
 
