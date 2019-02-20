a8ab55d4d962f84bce4b077c1f261ead6bd8dac3
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-02-20 14:53:26 +0000

    #COBUMMFO-10635# Коментар: MERGE to master yevhen.ostapenko\COBUMMFO-10635

--- sql/mmfo/bars/View/v_stat_file_workflow.sql
+++ sql/mmfo/bars/View/v_stat_file_workflow.sql
@@ -1,17 +1,24 @@
 create or replace view v_stat_file_workflow as
-select f.id  file_id,
-       so.id ope_id,
-       so.name,
-       so.need_sign,
-       oh.user_id,
-       u.fio,
-       oh.oper_date ,
-       oh.way  ,
-       case oh.way when 1 then 'пряма' when 2 then 'відміна' when 3 then 'пряма(відмінена)' end as direction
-  from    stat_files f
-     join stat_file_types t            on f.file_type_id = t.id
-     join stat_workflow_operations wo  on wo.wf_id = t.wf_id
-     join stat_operations so           on so.id = wo.oper_id
-left join stat_file_operations_hist oh on oh.file_id  = f.id and oh.oper_id = so.id and oh.way = 1
-left join staff$base u                 on u.id = oh.user_id
-order by so.id, oh.id;
+select level a, a.*
+from (select   f.id  file_id,
+               so.id ope_id,
+               so.name,
+               so.need_sign,
+               oh.user_id,
+               u.fio,
+               oh.oper_date,
+               so.begin_status,
+               so.end_status,
+               oh.way  ,
+               case oh.way when 1 then 'пряма' when 2 then 'відміна' when 3 then 'пряма(відмінена)' end as direction
+          from    stat_files f
+             join stat_file_types t            on f.file_type_id = t.id
+             join stat_workflow_operations wo  on wo.wf_id = t.wf_id
+             join stat_operations so           on so.id = wo.oper_id
+        left join stat_file_operations_hist oh on oh.file_id  = f.id and oh.oper_id = so.id and oh.way = 1
+        left join staff$base u                 on u.id = oh.user_id) a
+start with a.begin_status=0
+connect by prior a.end_status=a.begin_status
+order  by 1;
+
+
