cb70287f5d8e1aa8e6ba235ce53f8355d83cd8aa
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-02-20 15:11:29 +0000

    #COBUMMFO-10818# Коментар:

--- ABS/master/sql/mmfo/bars/Data/tts/et_rsm.sql
+++ ABS/master/sql/mmfo/bars/Data/tts/et_rsm.sql
@@ -12,11 +12,11 @@ begin
   --------------------------------
   begin
     insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
-    values ('RSM', 'RSM ѕерерахуванн¤ кошт?в на користь  л?Їнт?в (зовн?шн?й)', 1, null, null, '#(get_proc_nls(''T00'', NVL ( #(KVA), 980 ) ) )', null, null, null, null, null, 1, 1, 0, 0, null, null, null, null, null, null, '0201100000000000000000000000000000000100000000000000000000000000', null);
+    values ('RSM', 'RSM ѕерерахуванн¤ кошт?в на користь  л?Їнт?в (зовн?шн?й)', 1, null, null, '#(get_proc_nls(''T00'', NVL ( #(KVA), 980 ) ) )', null, null, null, null, null, 1, 1, 0, 0, null, null, null, null, null, null, '0201100000000000000000000000000000000000000000000000000000000000', null);
   exception
     when dup_val_on_index then 
       update tts
-         set tt='RSM', name='RSM ѕерерахуванн¤ кошт?в на користь  л?Їнт?в (зовн?шн?й)', dk=1, nlsm=null, kv=null, nlsk='#(get_proc_nls(''T00'', NVL ( #(KVA), 980 ) ) )', kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=1, fli=1, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0201100000000000000000000000000000000100000000000000000000000000', nazn=null
+         set tt='RSM', name='RSM ѕерерахуванн¤ кошт?в на користь  л?Їнт?в (зовн?шн?й)', dk=1, nlsm=null, kv=null, nlsk='#(get_proc_nls(''T00'', NVL ( #(KVA), 980 ) ) )', kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=1, fli=1, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0201100000000000000000000000000000000000000000000000000000000000', nazn=null
        where tt='RSM';
   end;
   --------------------------------
