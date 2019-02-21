6cd9b0b19e314f7a3ac40bf195e5f553733530a2
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-02-21 07:58:30 +0000

    #COBUMMFO-10635# Коментар: MERGE to master yevhen.ostapenko\COBUMMFO-10635

--- sql/mmfo/bars/View/v_stat_file_workflow.sql
+++ sql/mmfo/bars/View/v_stat_file_workflow.sql
@@ -1,5 +1,5 @@
 create or replace view v_stat_file_workflow as
-select level a, a.*
+select level a, a."FILE_ID",a."OPE_ID",a."NAME",a."NEED_SIGN",a."USER_ID",a."FIO",a."OPER_DATE",a."BEGIN_STATUS",a."END_STATUS",a."WAY",a."DIRECTION"
 from (select   f.id  file_id,
                so.id ope_id,
                so.name,
@@ -16,9 +16,8 @@ from (select   f.id  file_id,
              join stat_workflow_operations wo  on wo.wf_id = t.wf_id
              join stat_operations so           on so.id = wo.oper_id
         left join stat_file_operations_hist oh on oh.file_id  = f.id and oh.oper_id = so.id and oh.way = 1
-        left join staff$base u                 on u.id = oh.user_id) a
+        left join staff$base u                 on u.id = oh.user_id
+      where f.id = TO_NUMBER (pul.Get_Mas_Ini_Val ('FILE_ID')) ) a
 start with a.begin_status=0
 connect by prior a.end_status=a.begin_status
 order  by 1;
-
-
