3a303c1239f0f4f7c2245479f3da147cb30ce21f
oleh.yaroshenko <oleh.yaroshenko@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-03-06 08:44:40 +0000

    #COBUMMFO-9802# Коментар: Merge cobummfo-9802 Олександр Липських

--- ABS/master/sql/mmfo/bars/View/v_finmon_public_customers.sql
+++ ABS/master/sql/mmfo/bars/View/v_finmon_public_customers.sql
@@ -15,31 +15,30 @@ select c.id,
        c.comments, 
        c.kf,
        to_char(p.bday, 'dd.mm.yyyy')  as person_birth,
-       to_char(r.birth, 'dd.mm.yyyy') as public_rels_birth
+       to_char(r.birth, 'dd.mm.yyyy') as public_rels_birth,
+       pep_code,
+       d.date_of_birth as pep_birth
 from finmon_public_customers c
      left join person p on p.rnk = c.rnk
-     left join finmon_public_rels r on r.id = c.num_reel;
+     left join finmon_public_rels r on r.id = c.num_reel
+     left join finmon_pep_dict d on c.pep_code = d.id;
 
-PROMPT *** Create comments on bars.v_finmon_public_customers ***
-
-comment on table bars.v_finmon_public_customers                    is 'Перелік виявлени клієнтів - публічних діячів';
-comment on column bars.v_finmon_public_customers.id                is 'Унікальний код';
-comment on column bars.v_finmon_public_customers.rnk               is 'Реєстраційний номер клієнта в БД';
-comment on column bars.v_finmon_public_customers.nmk               is 'Найменування клієнта в БД';
-comment on column bars.v_finmon_public_customers.crisk             is 'Категорія ризику клієнта на дату перевірки';
-comment on column bars.v_finmon_public_customers.cust_risk         is 'Критерії ризику клієнта на дату перевірки';
-comment on column bars.v_finmon_public_customers.check_date        is 'Дата перевірки клієнта на відповідність переліку публічних діячів';
-comment on column bars.v_finmon_public_customers.rnk_reel          is 'RNK пов`язаної особи';
-comment on column bars.v_finmon_public_customers.nmk_reel          is 'ПІБ/Назва пов`язаної особи';
-comment on column bars.v_finmon_public_customers.num_reel          is '№ пов`яз. особи в переліку публічних осіб';
-comment on column bars.v_finmon_public_customers.comments          is 'Коментар';
-comment on column bars.v_finmon_public_customers.kf                is 'МФО';
-comment on column bars.v_finmon_public_customers.person_birth      is 'Дата народження клієнта';
-comment on column bars.v_finmon_public_customers.public_rels_birth is 'Дата народження особи з переліку ПЕП';
-
-PROMPT *** Create  grants  v_finmon_public_customers ***
-
-grant select on bars.v_finmon_public_customers to bars_access_defrole;
+comment on table V_FINMON_PUBLIC_CUSTOMERS is 'Перелік виявлени клієнтів - публічних діячів';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.ID is 'Номер в списке КИС';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.RNK is 'Реєстраційний номер клієнта в БД';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.NMK is 'Найменування клієнта в БД';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.CRISK is 'Категорія ризику клієнта на дату перевірки';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.CUST_RISK is 'Критерії ризику клієнта на дату перевірки';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.CHECK_DATE is 'Дата перевірки клієнта на відповідність переліку публічних діячів';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.RNK_REEL is 'RNK пов`язаної особи';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.NMK_REEL is 'ПІБ/Назва пов`язаної особи';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.NUM_REEL is '№ пов`яз. особи в переліку публічних осіб (КІС)';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.COMMENTS is 'Коментар';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.KF is 'МФО';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.PERSON_BIRTH is 'Дата народження клієнта';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.PUBLIC_RELS_BIRTH is 'Дата народження особи з переліку КІС';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.PEP_CODE is 'Номер в списке ПЕП (pep.org.ua)';
+comment on column V_FINMON_PUBLIC_CUSTOMERS.PEP_BIRTH is 'Дата народження особи з переліку ПЕП';
 
 PROMPT ===================================================================================== 
 PROMPT *** End *** ========== Scripts /sql/bars/view/v_finmon_public_customers.sql =========*** End *** =
