0d8bd934723d1df5842d821db406bd98f8457261
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-03-07 19:21:52 +0000

    #COBUMON-3# Коментар:

--- sql/mmfo/bars/Script/create_subsidy_key.sql
+++ sql/mmfo/bars/Script/create_subsidy_key.sql
@@ -1,15 +1,3 @@
-declare
-  l_tmp number;
-begin
-  l_tmp := crypto_utl.create_keytype(p_name => 'Монетизація субсидій',
-                            p_code => 'SUBSIDY');
-  crypto_utl.create_key(p_key_value  => '4087F8CCCB706D49B04282A81B8570F95B53FF7DE1F228ABB3E7B0C9D4D4BA82',
-                        p_start_date => trunc(sysdate),
-                        p_end_date   => trunc(sysdate) + 365,
-                        p_code       => 'SUBSIDY');
-end;
-/
-
 update ow_keys ok
    set KEY_VALUE = '4087F8CCCB706D49B04282A81B8570F95B53FF7DE1F228ABB3E7B0C9D4D4BA82'
  where ok.type in (select kt.id from keytypes kt where kt.code = 'SUBSIDY');
