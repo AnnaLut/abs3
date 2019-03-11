304448da081d94e62a67c8e5c36970469fc3c3cf
iryna.kharina <iryna.kharina@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-03-11 18:00:30 +0000

    #COBUMMFO-10783# Коментар: исправления опер

--- sql/mmfo/bars/Data/tts/et_dp2.sql
+++ sql/mmfo/bars/Data/tts/et_dp2.sql
@@ -2,6 +2,214 @@ set lines 1000
 set trimspool on
 set serveroutput on size 1000000
 
+prompt Создание / Обновление операции !!D
+prompt Наименование операции: STOP правило на поповн деп за період
+declare
+  cnt_  number;
+begin
+  --------------------------------
+  -- Основные свойства операции --
+  --------------------------------
+  begin
+    insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
+    values ('!!D', 'STOP правило на поповн деп за період', 1, null, null, null, null, null, null, null, null, 0, 0, 0, 0, 'F_STOP(''1478'',#(KVA),#(NLSB),#(S),#(REF))', null, null, null, null, null, '0100000000000000000000000000000000000000000000000000000000000000', null);
+  exception
+    when dup_val_on_index then
+      update tts set
+        tt='!!D', name='STOP правило на поповн деп за період', dk=1, nlsm=null, kv=null, nlsk=null, kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s='F_STOP(''1478'',#(KVA),#(NLSB),#(S),#(REF))', s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0100000000000000000000000000000000000000000000000000000000000000', nazn=null
+       where tt='!!D';
+  end;
+  
+  --------------------------------
+  ---------- Реквизиты -----------
+  --------------------------------
+  delete from op_rules where tt='!!D';
+  
+  --------------------------------
+  ------ Связанные операции ------
+  --------------------------------
+  delete from ttsap where tt='!!D';
+  
+  --------------------------------
+  ------- Балансовые счета -------
+  --------------------------------
+  delete from ps_tts where tt='!!D';
+  
+  --------------------------------
+  ------- Виды документов --------
+  --------------------------------
+  delete from tts_vob where tt='!!D';
+  
+  --------------------------------
+  -------- Группы контроля -------
+  --------------------------------
+  delete from chklist_tts where tt='!!D';
+  
+  --------------------------------
+  ------------ Папки -------------
+  --------------------------------
+  delete from folders_tts where tt='!!D';
+  
+  
+end;
+/
+
+prompt Создание / Обновление операции !!P
+prompt Наименование операции: STOP правило на перерахування з деп.рахунків на акційний депозит
+declare
+  cnt_  number;
+begin
+  --------------------------------
+  -- Основные свойства операции --
+  --------------------------------
+  begin
+    insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
+    values ('!!P', 'STOP правило на перерахування з деп.рахунків на акційний депозит', 1, null, null, null, null, null, null, null, null, 0, 0, 0, 0, 'F_STOP(''9995'',#(KVA),#(NLSB),#(S),#(REF))', null, null, null, null, null, '0100000000000000000000000000000000000000000000000000000000000000', null);
+  exception
+    when dup_val_on_index then
+      update tts set
+        tt='!!P', name='STOP правило на перерахування з деп.рахунків на акційний депозит', dk=1, nlsm=null, kv=null, nlsk=null, kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s='F_STOP(''9995'',#(KVA),#(NLSB),#(S),#(REF))', s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0100000000000000000000000000000000000000000000000000000000000000', nazn=null
+       where tt='!!P';
+  end;
+  
+  --------------------------------
+  ---------- Реквизиты -----------
+  --------------------------------
+  delete from op_rules where tt='!!P';
+  
+  --------------------------------
+  ------ Связанные операции ------
+  --------------------------------
+  delete from ttsap where tt='!!P';
+  
+  --------------------------------
+  ------- Балансовые счета -------
+  --------------------------------
+  delete from ps_tts where tt='!!P';
+  
+  --------------------------------
+  ------- Виды документов --------
+  --------------------------------
+  delete from tts_vob where tt='!!P';
+  
+  --------------------------------
+  -------- Группы контроля -------
+  --------------------------------
+  delete from chklist_tts where tt='!!P';
+  
+  --------------------------------
+  ------------ Папки -------------
+  --------------------------------
+  delete from folders_tts where tt='!!P';
+  
+  
+end;
+/
+
+prompt Создание / Обновление операции KD8
+prompt Наименование операции: Комiсiя за перерахування з 2620/39 на придбання ОВДП
+declare
+  cnt_  number;
+begin
+  --------------------------------
+  -- Основные свойства операции --
+  --------------------------------
+  begin
+    insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
+    values ('KD8', 'Комiсiя за переказ з 2620/39 на придбання ОВДП', 1, null, null, '#(nbs_ob22 (''6510'',''H6''))', null, null, null, '#(nbs_ob22 (''6510'',''H6''))', null, 0, 0, 0, 0, 'KOMIS_DP2_OVDP( #(S), #(NLSA), #(NLSB), F_DOP(#(REF),''VOVDP'') )', null, null, null, null, null, '0000000000000000000000000000000000000000000000000000000000000000', null);
+  exception
+    when dup_val_on_index then
+      update tts set
+        tt='KD8', name='Комiсiя за переказ з 2620/39 на придбання ОВДП', dk=1, nlsm=null, kv=null, nlsk='#(nbs_ob22 (''6510'',''H6''))', kvk=null, nlss=null, nlsa=null, nlsb='#(nbs_ob22 (''6510'',''H6''))', mfob=null, flc=0, fli=0, flv=0, flr=0, s='KOMIS_DP2_OVDP( #(S), #(NLSA), #(NLSB), F_DOP(#(REF),''VOVDP'') )', s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0000000000000000000000000000000000000000000000000000000000000000', nazn=null
+       where tt='KD8';
+  end;
+  
+  --------------------------------
+  ---------- Реквизиты -----------
+  --------------------------------
+  delete from op_rules where tt='KD8';
+  
+  --------------------------------
+  ------ Связанные операции ------
+  --------------------------------
+  delete from ttsap where tt='KD8';
+  
+  --------------------------------
+  ------- Балансовые счета -------
+  --------------------------------
+  delete from ps_tts where tt='KD8';
+  
+  --------------------------------
+  ------- Виды документов --------
+  --------------------------------
+  delete from tts_vob where tt='KD8';
+  
+  --------------------------------
+  -------- Группы контроля -------
+  --------------------------------
+  delete from chklist_tts where tt='KD8';
+  
+  --------------------------------
+  ------------ Папки -------------
+  --------------------------------
+  delete from folders_tts where tt='KD8';
+  
+  
+end;
+/
+
+prompt Создание / Обновление операции !!V
+prompt Наименование операции: STOP правило на операції з БПК
+declare
+  cnt_  number;
+begin
+  --------------------------------
+  -- Основные свойства операции --
+  --------------------------------
+  begin
+    insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
+    values ('!!V', 'STOP правило на операції з БПК', 1, null, null, null, null, null, null, null, null, 0, 0, 0, 0, 'F_STOP(''9999'',#(KVA),#(NLSB),#(S),#(REF))', null, null, null, null, null, '0100000000000000000000000000000000000000000000000000000000000000', null);
+  exception
+    when dup_val_on_index then
+      update tts set
+        tt='!!V', name='STOP правило на операції з БПК', dk=1, nlsm=null, kv=null, nlsk=null, kvk=null, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s='F_STOP(''9999'',#(KVA),#(NLSB),#(S),#(REF))', s2=null, sk=null, proc=null, s3800=null, rang=null, flags='0100000000000000000000000000000000000000000000000000000000000000', nazn=null
+       where tt='!!V';
+  end;
+  
+  --------------------------------
+  ---------- Реквизиты -----------
+  --------------------------------
+  delete from op_rules where tt='!!V';
+  
+  --------------------------------
+  ------ Связанные операции ------
+  --------------------------------
+  delete from ttsap where tt='!!V';
+  
+  --------------------------------
+  ------- Балансовые счета -------
+  --------------------------------
+  delete from ps_tts where tt='!!V';
+  
+  --------------------------------
+  ------- Виды документов --------
+  --------------------------------
+  delete from tts_vob where tt='!!V';
+  
+  --------------------------------
+  -------- Группы контроля -------
+  --------------------------------
+  delete from chklist_tts where tt='!!V';
+  
+  --------------------------------
+  ------------ Папки -------------
+  --------------------------------
+  delete from folders_tts where tt='!!V';
+  
+  
+end;
+/
+
 prompt Создание / Обновление операции DP2
 prompt Наименование операции: DP2 +Повернення суми вкладу в нац.валюті (внутр.)
 declare
@@ -12,15 +220,23 @@ begin
   --------------------------------
   begin
     insert into tts(tt, name, dk, nlsm, kv, nlsk, kvk, nlss, nlsa, nlsb, mfob, flc, fli, flv, flr, s, s2, sk, proc, s3800, rang, flags, nazn)
-    values ('DP2', 'DP2 +Повернення суми вкладу в нац.валюті (внутр.)', 1, null, 980, null, 980, null, null, null, null, 0, 0, 0, 0, null, null, null, null, null, 0, '0000100000000000000000000011000000010000000000000000000000000000', 'Повернення коштів згідно договору № #{DPT_WEB.F_NAZN(''U'',#(ND))}');
+    values ('DP2', 'DP2 +Повернення суми вкладу в нац.валюті (внутр.)', 1, null, 980, null, 980, null, null, null, null, 1, 0, 0, 0, null, null, null, null, '0', 0, '0000100000000000000000000011000000010000000000000000000000000000', 'Повернення коштів згідно договору № #{DPT_WEB.F_NAZN(''U'',#(ND))}');
   exception
-    when dup_val_on_index then 
-      update tts
-         set tt='DP2', name='DP2 +Повернення суми вкладу в нац.валюті (внутр.)', dk=1, nlsm=null, kv=980, nlsk=null, kvk=980, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=0, fli=0, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800=null, rang=0, flags='0000100000000000000000000011000000010000000000000000000000000000', nazn='Повернення коштів згідно договору № #{DPT_WEB.F_NAZN(''U'',#(ND))}'
+    when dup_val_on_index then
+      update tts set
+        tt='DP2', name='DP2 +Повернення суми вкладу в нац.валюті (внутр.)', dk=1, nlsm=null, kv=980, nlsk=null, kvk=980, nlss=null, nlsa=null, nlsb=null, mfob=null, flc=1, fli=0, flv=0, flr=0, s=null, s2=null, sk=null, proc=null, s3800='0', rang=0, flags='0000100000000000000000000011000000010000000000000000000000000000', nazn='Повернення коштів згідно договору № #{DPT_WEB.F_NAZN(''U'',#(ND))}'
        where tt='DP2';
   end;
+  select count(*) into cnt_ from accounts where nls='0';
+  if cnt_=0 then
+  
+  
+  
+    dbms_output.put_line('Счет валютной позиции 0 заданый в операции DP2 не существует!');
+  end if;
+  
   --------------------------------
-  ----------- Реквизиты ----------
+  ---------- Реквизиты -----------
   --------------------------------
   delete from op_rules where tt='DP2';
   begin
@@ -28,7 +244,7 @@ begin
     values ('DPTOP', 'DP2', 'O', 0, 1, '23', null);
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
         dbms_output.put_line('Не удалось добавить запись (op_rules: ''DPTOP'', ''DP2'', ''O'', 0, 1, ''23'', null) - первичный ключ не найден!');
       else raise;
@@ -39,7 +255,7 @@ begin
     values ('TTOF1', 'DP2', 'O', 0, null, 'KBL', null);
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
         dbms_output.put_line('Не удалось добавить запись (op_rules: ''TTOF1'', ''DP2'', ''O'', 0, null, ''KBL'', null) - первичный ключ не найден!');
       else raise;
@@ -61,36 +277,83 @@ begin
   ------ Связанные операции ------
   --------------------------------
   delete from ttsap where tt='DP2';
+  begin
+    insert into ttsap(ttap, tt, dk)
+    values ('!!D', 'DP2', 0);
+  exception
+    when dup_val_on_index then null;
+    when others then 
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ttsap: ''!!D'', ''DP2'', 0) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ttsap(ttap, tt, dk)
+    values ('!!P', 'DP2', 0);
+  exception
+    when dup_val_on_index then null;
+    when others then 
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ttsap: ''!!P'', ''DP2'', 0) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ttsap(ttap, tt, dk)
+    values ('KD8', 'DP2', 0);
+  exception
+    when dup_val_on_index then null;
+    when others then 
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ttsap: ''KD8'', ''DP2'', 0) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  begin
+    insert into ttsap(ttap, tt, dk)
+    values ('!!V', 'DP2', 0);
+  exception
+    when dup_val_on_index then null;
+    when others then 
+      if ( sqlcode = -02291 ) then
+        dbms_output.put_line('Не удалось добавить запись (ttsap: ''!!V'', ''DP2'', 0) - первичный ключ не найден!');
+      else raise;
+      end if;
+  end;
+  
   --------------------------------
   ------- Балансовые счета -------
   --------------------------------
   delete from ps_tts where tt='DP2';
+  
   --------------------------------
-  -------- Виды документов -------
+  ------- Виды документов --------
   --------------------------------
   delete from tts_vob where tt='DP2';
   begin
-    insert into tts_vob(vob, tt, ord)
-    values (6, 'DP2', null);
+    insert into tts_vob(vob, tt)
+    values (6, 'DP2');
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
-        dbms_output.put_line('Не удалось добавить запись (tts_vob: 6, ''DP2'', null) - первичный ключ не найден!');
+        dbms_output.put_line('Не удалось добавить запись (tts_vob: 6, ''DP2'') - первичный ключ не найден!');
       else raise;
       end if;
   end;
   begin
-    insert into tts_vob(vob, tt, ord)
-    values (102, 'DP2', null);
+    insert into tts_vob(vob, tt)
+    values (102, 'DP2');
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
-        dbms_output.put_line('Не удалось добавить запись (tts_vob: 102, ''DP2'', null) - первичный ключ не найден!');
+        dbms_output.put_line('Не удалось добавить запись (tts_vob: 102, ''DP2'') - первичный ключ не найден!');
       else raise;
       end if;
   end;
+  
   --------------------------------
   -------- Группы контроля -------
   --------------------------------
@@ -100,7 +363,7 @@ begin
     values (2, 'DP2', 2, null, 'branch_edit.get_branch_parameter_ex(branch, ''NOT2VISA'', ''0'') = ''0''', null);
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
         dbms_output.put_line('Не удалось добавить запись (chklist_tts: 2, ''DP2'', 2, null, ''branch_edit.get_branch_parameter_ex(branch, ''''NOT2VISA'''', ''''0'''') = ''''0'''''', null) - первичный ключ не найден!');
       else raise;
@@ -111,14 +374,15 @@ begin
     values (5, 'DP2', 1, null, '(not exists (Select id from staff$base where  clsid > 0 and branch = ''/'' and id = userid))', null);
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
         dbms_output.put_line('Не удалось добавить запись (chklist_tts: 5, ''DP2'', 1, null, ''(not exists (Select id from staff$base where  clsid > 0 and branch = ''''/'''' and id = userid))'', null) - первичный ключ не найден!');
       else raise;
       end if;
   end;
+  
   --------------------------------
-  ------------- Папки ------------
+  ------------ Папки -------------
   --------------------------------
   delete from folders_tts where tt='DP2';
   begin
@@ -126,12 +390,16 @@ begin
     values (1, 'DP2');
   exception
     when dup_val_on_index then null;
-    when others then
+    when others then 
       if ( sqlcode = -02291 ) then
         dbms_output.put_line('Не удалось добавить запись (folders_tts: 1, ''DP2'') - первичный ключ не найден!');
       else raise;
       end if;
   end;
+  
+  
 end;
 /
+
+
 commit;
