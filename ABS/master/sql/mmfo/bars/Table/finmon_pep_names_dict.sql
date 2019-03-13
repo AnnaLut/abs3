29622b657281f0e41920a742f0203fa4d08d7367
rostyslav.kuzmovych <rostyslav.kuzmovych@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-03-13 07:31:21 +0000

    #COBUMMFO-9802# Коментар: MERGE: COBUMMFO-9802 Олександр Липських

--- ABS/master/sql/mmfo/bars/Table/finmon_pep_names_dict.sql
+++ ABS/master/sql/mmfo/bars/Table/finmon_pep_names_dict.sql
@@ -10,8 +10,7 @@ create table finmon_pep_names_dict
 (
 pep_id number,
 search_name varchar2(150),
-constraint XPK_FINMON_PEP_NAMES_DICT primary key (search_name, pep_id),
-constraint R_FINMON_PEP_NAMES_ID foreign key (pep_id) references finmon_pep_dict (id)
+constraint XPK_FINMON_PEP_NAMES_DICT primary key (search_name, pep_id)
 )
 organization index
 tablespace BRSDYNI
@@ -24,3 +23,11 @@ end;
 comment on table finmon_pep_names_dict is 'Справочник альтернативных имен/названий публ. деятелей (pep.org.ua)';
 comment on column finmon_pep_names_dict.pep_id is 'Искуственный номер для идентификации в справочнике';
 comment on column finmon_pep_names_dict.search_name is 'Искуственное поле для поиска';
+
+begin
+    execute immediate 'alter table finmon_pep_names_dict drop constraint R_FINMON_PEP_NAMES_ID';
+exception
+    when others then
+        if sqlcode = -2443 then null; else raise; end if;
+end;
+/
