d09b0492d63f7736a8dfd4029a3c12990e1a5f34
oleh.yaroshenko <oleh.yaroshenko@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-03-18 13:24:29 +0000

    #COBUMMFO-11154# Коментар: Merge COBUMMFO-11154 Олександр Іваненко

---
+++ ABS/master/sql/mmfo/bars/Script/add_rec_to_ow_inst_totals_hist.sql
@@ -0,0 +1,17 @@
+begin
+    for j in (select kf from mv_kf) loop
+        bc.go(j.kf);
+        for i in (select chain_idt from (
+                         select chain_idt,  
+                                max(plan_num) KEEP (DENSE_RANK LAST ORDER BY plan_num) as by_pl_num, 
+                                max(plan_num) KEEP (DENSE_RANK LAST ORDER BY ins_bd) as by_ins_bd 
+                           from ow_inst_totals_hist group by chain_idt)
+                          where by_pl_num <> by_ins_bd) loop
+        bars_ow.int_move_to_hist(i.chain_idt);
+        end loop;
+    end loop;
+bc.go('/');
+exception when others then 
+bc.go('/');
+raise;
+end;
