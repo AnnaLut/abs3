611ec56eacc4ffed575ee33c59576403fd342f8a
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-03-20 16:43:31 +0000

    #COBUSAGO-29# Коментар:

---
+++ sql/mmfo/bars/Script/alter_sago_documents.sql
@@ -0,0 +1,19 @@
+begin 
+    execute immediate 'create table tmp_sago( id number(38),
+                                              ref varchar2(100))';
+                           
+    execute immediate 'insert into tmp_sago 
+    select sd.id, sd.ref_our from sago_documents sd where sd.ref_our is not null';
+
+    update sago_documents sd 
+       set sd.ref_our = null
+     where sd.ref_our is not null;
+     
+    execute immediate 'alter table SAGO_DOCUMENTS modify ref_our varchar2(100)';
+
+    execute immediate 'update sago_documents sd 
+       set sd.ref_our = (select ref from tmp_sago where id = sd.id)';
+       
+    execute immediate 'drop table tmp_sago';
+    commit;
+end;
