c4d2542cf7dc8f5bbd143223f96fadb44f2136a2
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-04-17 07:17:07 +0000

    #COBUMMFO-10212# Коментар: MERGE COBUMMFO-10212 to master

---
+++ sql/mmfo/bars/View/v_nbur_#X2.sql
@@ -0,0 +1,40 @@
+
+
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/BARS/View/V_NBUR_#X2.sql =========*** Run *** ===
+PROMPT ===================================================================================== 
+
+
+PROMPT *** Create  view V_NBUR_#X2 ***
+
+  CREATE OR REPLACE FORCE VIEW BARS.V_NBUR_#X2 ("REPORT_DATE", "KF", "VERSION_ID", "NBUC", "FIELD_CODE", "SEG_01", "SEG_02", "SEG_03", "FIELD_VALUE") AS 
+  select p.REPORT_DATE
+     , p.KF
+     , p.VERSION_ID
+     , p.NBUC
+     , p.FIELD_CODE
+     , SUBSTR(p.FIELD_CODE,1,2) as SEG_01
+     , SUBSTR(p.FIELD_CODE,3,4) as SEG_02
+     , SUBSTR(p.FIELD_CODE,7,3) as SEG_03
+     , p.FIELD_VALUE
+  from NBUR_AGG_PROTOCOLS_ARCH p
+  join NBUR_REF_FILES f
+    on ( f.FILE_CODE = p.REPORT_CODE )
+  join NBUR_LST_FILES v
+    on ( v.REPORT_DATE = p.REPORT_DATE and
+         v.KF          = p.KF          and
+         v.VERSION_ID  = p.VERSION_ID  and
+         v.FILE_ID     = f.ID )           
+ where p.REPORT_CODE = '#X2'
+   and v.FILE_STATUS IN ( 'FINISHED', 'BLOCKED' );
+
+PROMPT *** Create  grants  V_NBUR_#X2 ***
+grant SELECT                                                                 on V_NBUR_#X2      to BARSREADER_ROLE;
+grant SELECT                                                                 on V_NBUR_#X2      to BARS_ACCESS_DEFROLE;
+grant SELECT                                                                 on V_NBUR_#X2      to UPLD;
+
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/BARS/View/V_NBUR_#X2.sql =========*** End *** ===
+PROMPT ===================================================================================== 
