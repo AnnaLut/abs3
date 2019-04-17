69084b5de3ec3cde64e31cad6463ea1a6b17c46d
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-04-17 08:05:44 +0000

    #COBUMMFO-8116# Коментар: MERGE

--- sql/mmfo/bars/Grant/grants_pfu.sql
+++ sql/mmfo/bars/Grant/grants_pfu.sql
@@ -3,4 +3,5 @@ grant execute on  bars.number_list to pfu;
 
 grant select on  bars.staff$base to pfu;
 
-/
+grant select on bars.oper_visa to pfu;
+grant execute on bars.getglobaloption to pfu with grant option;
---
+++ sql/mmfo/pfu/View/v_pfu_payment_for_sign.sql
@@ -0,0 +1,17 @@
+create or replace view pfu.v_pfu_payment_for_sign as
+select o.ref
+  from bars.oper o,
+       bars.oper_visa ov
+ where o.tt in ('PKX ', '024')
+   and o.pdat > sysdate - 5
+   and o.sos not in (5, -1, -2)
+   and o.nextvisagrp is null
+   and o.ref = ov.ref
+   and ov.groupid =  to_number(bars.getglobaloption('FM_GRP2'));
+/   
+ 
+  grant select on pfu.v_pfu_payment_for_sign to bars_access_defrole;
+   
+   
+   
+  
