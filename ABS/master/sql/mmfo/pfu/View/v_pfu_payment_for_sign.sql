109f572c107f9287cbbf9e9b471d82e2615bcb2a
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-04-26 11:33:54 +0000

    #COBUMMFO-8116# Коментар: MERGE

--- ABS/master/sql/mmfo/pfu/View/v_pfu_payment_for_sign.sql
+++ ABS/master/sql/mmfo/pfu/View/v_pfu_payment_for_sign.sql
@@ -2,7 +2,7 @@ create or replace view pfu.v_pfu_payment_for_sign as
 select o.ref
   from bars.oper o,
        bars.oper_visa ov
- where o.tt in ('PKX ', '024')
+ where o.tt in ('PKX ', '024', '024')
    and o.pdat > sysdate - 5
    and o.sos not in (5, -1, -2)
    and o.nextvisagrp is null
