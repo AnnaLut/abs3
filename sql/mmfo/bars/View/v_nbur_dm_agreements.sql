14f85eca4b40f0759c1d74bf39775a6a02a5267c
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-05-29 07:28:46 +0000

    #COBUMMFO-10883# Коментар: merge COBUMMFO-10883 to master (Vyacheslav Kovalchuk)

--- sql/mmfo/bars/View/v_nbur_dm_agreements.sql
+++ sql/mmfo/bars/View/v_nbur_dm_agreements.sql
@@ -1,41 +1,39 @@
 
-
-PROMPT ===================================================================================== 
-PROMPT *** Run *** ========== Scripts /Sql/BARS/View/V_NBUR_DM_AGREEMENTS.sql =========*** R
-PROMPT ===================================================================================== 
-
-
-PROMPT *** Create  view V_NBUR_DM_AGREEMENTS ***
-
+ 
+ PROMPT ===================================================================================== 
+ PROMPT *** Run *** ========== Scripts /Sql/BARS/view/v_nbur_dm_agreements.sql =========*** R
+ PROMPT ===================================================================================== 
+ 
   CREATE OR REPLACE FORCE VIEW BARS.V_NBUR_DM_AGREEMENTS ("REPORT_DATE", "KF", "AGRM_ID", "AGRM_NUM", "AGRM_TP", "AGRM_STE", "BEG_DT", "END_DT", "PRTFL_TP", "CCY_ID") AS 
-  SELECT a.REPORT_DATE
-     , a.KF
-     , a.AGRM_ID
-     , a.AGRM_NUM
-     , a.AGRM_TP
-     , a.AGRM_STE
-     , a.BEG_DT
-     , a.END_DT
-     , a.PRTFL_TP
-     , a.CCY_ID
-  from NBUR_REF_OBJECTS o
-  join NBUR_LST_OBJECTS v
-    on ( v.OBJECT_ID = o.ID )
-  join NBUR_DM_AGREEMENTS_ARCH a
-    on ( a.REPORT_DATE = v.REPORT_DATE and
-         a.KF          = v.KF          and
-         a.VERSION_ID  = v.VERSION_ID  )
- where o.OBJECT_NAME = 'NBUR_DM_AGREEMENTS'
-   and v.OBJECT_STATUS IN ( 'FINISHED', 'BLOCKED' )
+  SELECT /*+ leading(o v) full(a) no_push_pred */
+         v.REPORT_DATE,
+          v.KF,
+          a.AGRM_ID,
+          a.AGRM_NUM,
+          a.AGRM_TP,
+          a.AGRM_STE,
+          a.BEG_DT,
+          a.END_DT,
+          a.PRTFL_TP,
+          a.CCY_ID
+     FROM NBUR_REF_OBJECTS o
+          JOIN NBUR_LST_OBJECTS v ON (v.OBJECT_ID = o.ID)
+          JOIN NBUR_DM_AGREEMENTS_ARCH a
+             ON (    a.REPORT_DATE = v.REPORT_DATE
+                 AND a.KF = v.KF
+                 AND a.VERSION_ID = v.VERSION_ID)
+    WHERE o.OBJECT_NAME = 'NBUR_DM_AGREEMENTS' AND V.VLD = 0
 ;
-
+ show err;
+ 
 PROMPT *** Create  grants  V_NBUR_DM_AGREEMENTS ***
 grant SELECT                                                                 on V_NBUR_DM_AGREEMENTS to BARSREADER_ROLE;
 grant SELECT                                                                 on V_NBUR_DM_AGREEMENTS to BARS_ACCESS_DEFROLE;
 grant SELECT                                                                 on V_NBUR_DM_AGREEMENTS to UPLD;
 
-
-
-PROMPT ===================================================================================== 
-PROMPT *** End *** ========== Scripts /Sql/BARS/View/V_NBUR_DM_AGREEMENTS.sql =========*** E
-PROMPT ===================================================================================== 
+ 
+ 
+ PROMPT ===================================================================================== 
+ PROMPT *** End *** ========== Scripts /Sql/BARS/view/v_nbur_dm_agreements.sql =========*** E
+ PROMPT ===================================================================================== 
+ 
