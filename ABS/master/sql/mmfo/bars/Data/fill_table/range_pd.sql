707647e0152f0afb2c820483cc7f4ed4a899c706
Volodymyr.Pohoda <Volodymyr.Pohoda@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-05-29 07:19:02 +0000

    #COBUMMFO-11722# Коментар: MERGE COBUMMFO-11722 to master liudmyla.martseniuk + БАГФИКС

---
+++ ABS/master/sql/mmfo/bars/Data/fill_table/range_pd.sql
@@ -0,0 +1,61 @@
+delete from RANGE_PD;
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 1, 1, 1, 0.0015, 0.0024);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 2, 1, 1, 0.0025, 0.049);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 3, 1, 1, 0.05, 0.79);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 4, 1, 1, 0.8, 0.99);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 5, 1, 1, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 1, 1, 1, 0.0015, 0.0024);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 2, 1, 1, 0.0025, 0.049);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 3, 1, 1, 0.05, 0.79);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 4, 1, 1, 0.8, 0.99);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 5, 1, 1, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 1, 1, 2, 0.005, 0.012);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 2, 1, 2, 0.13, 0.21);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 3, 1, 2, 0.22, 0.36);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 4, 1, 2, 0.37, 0.99);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (5, 5, 1, 2, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 1, 1, 2, 0.005, 0.012);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 2, 1, 2, 0.13, 0.21);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 3, 1, 2, 0.22, 0.36);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 4, 1, 2, 0.37, 0.99);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (6, 5, 1, 2, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 1, 1, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 2, 1, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 3, 1, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 4, 1, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 5, 1, 2, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 1, 1, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 2, 1, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 3, 1, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 4, 1, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 5, 1, 1, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  1, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  2, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  3, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  4, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  5, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  6, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  7, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  8, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  9, 2, 1, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 10, 2, 1, 1, 1);
+
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  1, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  2, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  3, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  4, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  5, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  6, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  7, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  8, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3,  9, 2, 2, 0, 1);
+Insert into BARS.RANGE_PD (TIP, FIN, TIP_FIN, RZ, MIN, MAX) Values  (3, 10, 2, 2, 1, 1);
+
+
+COMMIT;                                                                  
---
+++ ABS/master/sql/mmfo/bars/Table/range_pd.sql
@@ -0,0 +1,84 @@
+PROMPT ===================================================================================== 
+PROMPT *** Run *** ========== Scripts /Sql/BARS/Table/RANGE_PD.sql =========*** Run *** 
+PROMPT ===================================================================================== 
+
+
+PROMPT *** ALTER_POLICY_INFO to RANGE_PD ***
+
+
+BEGIN 
+        execute immediate  
+          'begin  
+               bpa.alter_policy_info(''RANGE_PD'', ''FILIAL'' , null, null, null, null);
+               bpa.alter_policy_info(''RANGE_PD'', ''WHOLE'' , null, null, null, null);
+               null;
+           end; 
+          '; 
+END; 
+/
+
+PROMPT *** Create  table RANGE_PD ***                    
+begin 
+  execute immediate '
+  CREATE TABLE BARS.RANGE_PD 
+   (    tip     NUMBER,
+   	fin     NUMBER, 
+        tip_fin NUMBER, 
+	rz      NUMBER, 
+	min     NUMBER,
+        max     NUMBER
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  TABLESPACE BRSDYND ';
+exception when others then       
+  if sqlcode=-955 then null; else raise; end if; 
+end; 
+/
+
+PROMPT *** ALTER_POLICIES to RANGE_PD ***
+ exec bpa.alter_policies('RANGE_PD');
+
+COMMENT ON TABLE BARS.RANGE_PD IS 'Таблица диапазонов PD';
+COMMENT ON COLUMN BARS.RANGE_PD.TIP IS 'Тип актива';
+COMMENT ON COLUMN BARS.RANGE_PD.FIN IS 'Фин.клас';
+COMMENT ON COLUMN BARS.RANGE_PD.TIP_FIN IS 'Тип фин.стана 0-(1,2); 1-(1-5); 2-(1-10)';
+COMMENT ON COLUMN BARS.RANGE_PD.RZ IS 'Резидентность';
+COMMENT ON COLUMN BARS.RANGE_PD.MIN IS 'Минимальное значение';
+COMMENT ON COLUMN BARS.RANGE_PD.MAX IS 'Максимальное значение';
+
+PROMPT *** Create  constraint PK_RANGE_PD ***
+begin   
+ execute immediate '
+  ALTER TABLE BARS.RANGE_PD ADD CONSTRAINT PK_RANGE_PD PRIMARY KEY (TIP, FIN, tip_fin, RZ)
+  USING INDEX PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE BRSDYND  ENABLE';
+exception when others then
+  if  sqlcode=-2260 or sqlcode=-2261 or sqlcode=-2264 or sqlcode=-2275 or sqlcode=-1442 then null; else raise; end if;
+ end;
+/
+
+PROMPT *** Create  index PK_RANGE_PD ***
+begin   
+ execute immediate '
+  CREATE UNIQUE INDEX BARS.PK_RANGE_PD ON BARS.RANGE_PD (TIP, FIN, tip_fin, RZ) 
+  PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS 
+  TABLESPACE BRSDYND ';
+exception when others then
+  if  sqlcode=-955  then null; else raise; end if;
+ end;
+/
+
+
+
+PROMPT *** Create  grants  RANGE_PD ***
+grant SELECT                                                                 on RANGE_PD    to BARSREADER_ROLE;
+grant DELETE,FLASHBACK,INSERT,SELECT,UPDATE                                  on RANGE_PD    to BARS_ACCESS_DEFROLE;
+grant DELETE,INSERT,SELECT,UPDATE                                            on RANGE_PD    to RCC_DEAL;
+grant DELETE,INSERT,SELECT,UPDATE                                            on RANGE_PD    to START1;
+
+
+
+PROMPT ===================================================================================== 
+PROMPT *** End *** ========== Scripts /Sql/BARS/Table/RANGE_PD.sql =========*** End *** 
+PROMPT ===================================================================================== 
