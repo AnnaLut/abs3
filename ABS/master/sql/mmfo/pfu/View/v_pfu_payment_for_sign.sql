01650cb08e22831ab424b597ae3c8b4791e378c8
yaroslav.kurtov <yaroslav.kurtov@04814963-7c8d-8047-b203-fa5b01f58d43>
2019-05-29 14:30:19 +0000

    #COBUMMFO-8116# Коментар: MERGE COBUMMFO-8116_master to master

--- ABS/master/sql/mmfo/pfu/View/v_pfu_payment_for_sign.sql
+++ ABS/master/sql/mmfo/pfu/View/v_pfu_payment_for_sign.sql
@@ -1,17 +1,16 @@
 create or replace view pfu.v_pfu_payment_for_sign as
 select o.ref
-  from bars.oper o,
-       bars.oper_visa ov
- where o.tt in ('PKX ', '024', '024')
-   and o.pdat > sysdate - 5
-   and o.sos not in (5, -1, -2)
-   and o.nextvisagrp is null
-   and o.ref = ov.ref
-   and ov.groupid =  to_number(bars.getglobaloption('FM_GRP2'));
+  from bars.oper o
+       --bars.oper_visa ov
+ where o.tt in ('PKX ', '024', 'PFR')
+   and o.pdat between sysdate-1 and sysdate - 1/24
+   and o.sos =1
+   and o.nextvisagrp is null;
+   --and o.ref = ov.ref
+   --and ov.groupid =  to_number(bars.getglobaloption('FM_GRP2'));
 /   
  
-  grant select on pfu.v_pfu_payment_for_sign to bars_access_defrole;
-   
-   
+grant select on pfu.v_pfu_payment_for_sign to bars_access_defrole;
+
    
   
